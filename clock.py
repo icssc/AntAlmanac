c0c0841f9f567f06bbdb111bacf16cdb038a81b4
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-06 19:31:49 -0800

    added clock/py hopefully it schedules

---
+++ clock.py
@@ -0,0 +1,10 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from fData_Updater import update
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=4)
+def scheduled_job():
+    update()
+
+sched.start()
