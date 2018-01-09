8c9def715e04d83fddf555dfda850954ff588fe6
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-08 22:14:22 -0800

    changed time

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from fData_Updater import update
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=4)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=8)
 def scheduled_job():
     update()
 
