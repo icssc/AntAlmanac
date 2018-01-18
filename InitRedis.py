82620f9a6939b5669787e494bd9f37db2c99eb1f
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-17 22:04:00 -0800

    Update and rename InitRedis to InitRedis.py

--- InitRedis.py
+++ InitRedis.py
@@ -4,3 +4,5 @@ import redis
 
 url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+
+with open('corrected.txt') as data:
