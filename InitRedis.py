e9ddaa2404c831905f064ff36196261a3ed2df0d
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-17 22:21:03 -0800

    fixed urlparse 2->3

--- InitRedis.py
+++ InitRedis.py
@@ -2,7 +2,7 @@ import os
 from urllib.parse import urlparse
 import redis
 
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+url = urlparse(os.environ.get('REDISCLOUD_URL'))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
 with open('corrected.txt') as data:
