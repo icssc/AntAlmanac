cfadadf905bf7016b31b12aa325fdfbca5df3520
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-17 22:14:59 -0800

    Update InitRedis.py

--- InitRedis.py
+++ InitRedis.py
@@ -6,3 +6,13 @@ url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
 with open('corrected.txt') as data:
+  code = data.readline().strip()
+  while code != '=====':
+    
+    cap = data.readline().split()
+    enr = data.readline().split()
+    wl = data.readline().split()
+    
+    r.set(code, (cap,enr,wl))
+    
+    code = data.readline().strip()
