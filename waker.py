a241088994f892c591295cbbfbf61684e9e46f3e
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-01-29 10:39:32 -0800

    Update waker.py

--- waker.py
+++ waker.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 now_time = datetime.now().time()
 print(now_time)
-if now_time > datetime.strptime('15:00', '%H:%M').time() and now_time < datetime.strptime('23:59', '%H:%M').time():
+if (now_time > datetime.strptime('15:00', '%H:%M').time() and now_time < datetime.strptime('23:59', '%H:%M').time()) or now_time < datetime.strptime('5:00', '%H:%M').time():
     try:
         data = urllib.request.urlopen('https://antalmanac.herokuapp.com/')
         data.read()
