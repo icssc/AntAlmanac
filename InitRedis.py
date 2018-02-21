237e59196b8583a010424f36e4df4ab2ad9d42ff
andrewwself <awself@uci.edu>
2018-02-21 14:11:10 -0800

    fixed capitalization

--- InitRedis.py
+++ InitRedis.py
@@ -6,25 +6,25 @@ import redis
 url = urlparse(os.environ.get('REDISCLOUD_URL'))
 r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 
-r.flushall()
+# r.flushall()
 
-with open('corrected.txt') as data:
-  code = data.readline().strip()
-  while code != '=====':
+# with open('corrected.txt') as data:
+#   code = data.readline().strip()
+#   while code != '=====':
     
-    print(code,end=' ')
+#     print(code,end=' ')
     
-    cap = data.readline().split()
-    enr = data.readline().split()
-    req = data.readline().split()
-    wl = data.readline().split()
+#     cap = data.readline().split()
+#     enr = data.readline().split()
+#     req = data.readline().split()
+#     wl = data.readline().split()
     
-    r.set('W'+code, (cap,enr,req,wl))
+#     r.set('W'+code, (cap,enr,req,wl))
     
-    code = data.readline().strip()
+#     code = data.readline().strip()
     
 #input new
-master_dict = SOCSpider.getAllInfo(SOCSPider.getURL(SOCSpider.getDepts()))
+master_dict = SOCSpider.getAllInfo(SOCSpider.getURL(SOCSpider.getDepts()))
 for code, data in master_dict.items():
     print(code,end=' ')
     r.set('S'+code, ([data[0]], [data[1]], [data[2]], [data[3]]))
