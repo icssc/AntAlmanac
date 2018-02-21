1dc59ac5a719787d34c73667d850900b3a953edf
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-02-21 13:58:30 -0800

    Init for S setup.py

--- InitRedis.py
+++ InitRedis.py
@@ -1,5 +1,6 @@
 import os
 from urllib.parse import urlparse
+import SOCSpider
 import redis
 
 url = urlparse(os.environ.get('REDISCLOUD_URL'))
@@ -22,5 +23,10 @@ with open('corrected.txt') as data:
     
     code = data.readline().strip()
     
+#input new
+master_dict = SOCSpider.getAllInfo(SOCSPider.getURL(SOCSpider.getDepts()))
+for code, data in master_dict.items():
+    print(code,end=' ')
+    r.set('S'+code, ([data[0]], [data[1]], [data[2]], [data[3]]))
     
 print('done')    
