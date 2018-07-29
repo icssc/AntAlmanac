2950ac2df211d292562620d4890d655e1ba17899
Ran Gu <earlguwang@gmail.com>
2018-07-28 17:28:59 -0700

    Getting multiple select

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component }, { Fragment} from 'react';
 import DeptSearchBar from "../SearchForm/DeptSearchBar/deptsearchbar";
 import GESelector from "../SearchForm/GESelector/geselector";
+import TermSelector from "../SearchForm/termselector"
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <DeptSearchBar />
         <GESelector />
-      </div>
+        <TermSelector />
+      </Fragment >
     );
   }
 }
