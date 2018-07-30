3e4629ffacdeae5b3db8b66a18187bfc45d84ead
Nathan Abebayehu <nabebaye@uci.edu>
2018-07-29 21:42:43 -0700

    Fix comment rendering in JSX

--- src/components/SearchForm/GESelector/geselector.js
+++ src/components/SearchForm/GESelector/geselector.js
@@ -1,36 +1,47 @@
 import React, {Component} from 'react';
-import ge from './ge.json';
-import MuiDownshift from 'mui-downshift';
-
-
-const items = ge.map((label, value) => ({label, value}));
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 class GESelector extends Component {
     constructor(props) {
         super(props);
-        this.state = {filteredItems: items}; // Inital state is the whole list of GEs
-        this.handleStateChange = this.handleStateChange.bind(this);
+
+        this.state = {
+            ge: 'ANY',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleStateChange(changes) {
-        if (typeof changes.inputValue === 'string') {
-            // Match depts by label (ignoring case) and filter out the non matching depts
-            const filteredItems = items.filter(item => item.label.toLowerCase().includes(changes.inputValue.toLowerCase()));
-            this.setState({filteredItems});
-        }
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
     };
 
     render() {
         return (
-            <MuiDownshift
-                items={this.state.filteredItems}
-                onStateChange={this.handleStateChange}
-                getInputProps={() => ({     // Downshift requires this syntax to pass down these props to the text field
-                    label: 'GE',
-                    required: true,
-                })}
-                {...this.props} //Pass down other props to the Downshift layer
-            />
+            //Temporary CSS
+            <FormControl style={{minWidth: 300}}>
+                <InputLabel htmlFor='ge-select'>General Education</InputLabel>
+                <Select
+                    value={this.state.ge}
+                    onChange={this.handleChange}
+                    inputProps={{name:'ge', id:'ge-select'}}
+                >
+                    <MenuItem value={'ANY'}>All: Do not filter for GE</MenuItem>
+                    <MenuItem value={'GE-1A'}>GE Ia (1a): Lower Division Writing</MenuItem>
+                    <MenuItem value={'GE-1B'}>GE Ib (1b): Upper Division Writing</MenuItem>
+                    <MenuItem value={'GE-2'}>GE II (2): Science and Technology</MenuItem>
+                    <MenuItem value={'GE-3'}>GE III (3): Social and Behavioral Sciences</MenuItem>
+                    <MenuItem value={'GE-4'}>GE IV (4): Arts and Humanities</MenuItem>
+                    <MenuItem value={'GE-5A'}>GE Va (5a): Quantitative Literacy</MenuItem>
+                    <MenuItem value={'GE-5B'}>GE Vb (5b): Formal Reasoning</MenuItem>
+                    <MenuItem value={'GE-6'}>GE VI (6): Language other than English</MenuItem>
+                    <MenuItem value={'GE-7'}>GE VII (7): Multicultural Studies</MenuItem>
+                    <MenuItem value={'GE-8'}>GE VIII (8): International/Global Issues</MenuItem>
+                </Select>
+            </FormControl>
         );
     }
 }
