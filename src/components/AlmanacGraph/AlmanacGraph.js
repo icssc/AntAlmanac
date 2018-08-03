de454f2f7a7f6d8a52a74acf9a91014516fac666
Ran Gu <earlguwang@gmail.com>
2018-08-02 22:09:31 -0700

    trying things

--- src/components/AlmanacGraph/AlmanacGraph.js
+++ src/components/AlmanacGraph/AlmanacGraph.js
@@ -7,6 +7,29 @@ import Button from '@material-ui/core/Button';
 import availableQuarters from './availableQuarters.json'
 import FetchGraph from './FetchGraph'
 
+const https = require('https');
+var url_base = "https://www.ics.uci.edu/~rang1/";
+
+function FetchGraph(quarter, year, code){
+  var graph_url = url_base + quarter + year + '/' + code + '.txt';
+  https.get(graph_url, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      return JSON.parse(data).explanation;
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
+
 function getModalStyle() {
   return {
     width: 800,
