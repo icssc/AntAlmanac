2979d33771e517f0a7d90de3a14a16e2118c9083
Rango (Ran Gu) <34990962+the-rango@users.noreply.github.com>
2018-08-13 11:55:06 -0700

    Moved max's stuff here

--- src/components/AlmanacGraph/AlmanacGraph.js
+++ src/components/AlmanacGraph/AlmanacGraph.js
@@ -5,47 +5,19 @@ import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import availableQuarters from './availableQuarters.json'
-
-const https = require('https');
-var url_base = "https://www.ics.uci.edu/~rang1/";
-
-function FetchGraph(quarter, year, code){
-  var graph_url = url_base + quarter + year + '/' + code + '.txt';
-  https.get(graph_url, (resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      return JSON.parse(data).explanation;
-    });
-
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
-}
-
-function getModalStyle() {
-  return {
-    width: 800,
-    height: 600,
-    top: 50,
-    left: 50,
-    transform: `translate(-50, -50)`,
-  };
-}
+//----------------------------------
+import SimpleTab from './UITabs'
+import {getGraph} from './FetchGraph'
+//---------------------------------
 
 const styles = theme => ({
   paper: {
-    position: 'absolute',
+    position: 'relative',
     width: theme.spacing.unit * 50,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
+
   },
 });
 
@@ -61,28 +33,30 @@ class AlmanacGraph extends React.Component {
   handleClose = () => {
     this.setState({ open: false });
   };
+  //UnComment to display graph here then add an img tag inside the Model component
+/*
+   // will the graph from the call back function in Fetch Graph.js
+   componentDidMount(){
+    // create an object x and add it to the state
+    getGraph( (x) => {this.setState({x});} )
+  }
+*/
 
   render() {
     const { classes } = this.props;
-
     return (
-      <div>
+      <React.Fragment>
         <Typography gutterBottom>Click to see graph in a modal!</Typography>
         <Button onClick={this.handleOpen}>Open Modal</Button>
         <Modal
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
-          open={this.state.open}
-          onClose={this.handleClose}
-        >
-          <div style={getModalStyle()} className={classes.paper}>
-            <Typography variant="title" id="modal-title">
-              This should be the graph for ICS 31
-            </Typography>
-            <img src= {FetchGraph('w','18','36050')}/>
-          </div>
-        </Modal>
-      </div>
+          open={this.state.open} onClose={this.handleClose}>
+                 
+          <SimpleTab/>
+   
+          </Modal>
+      </React.Fragment>
     );
   }
 }
@@ -93,5 +67,24 @@ AlmanacGraph.propTypes = {
 
 // We need an intermediary variable for handling the recursive nesting.
 const AlmanacGraphWrapped = withStyles(styles)(AlmanacGraph);
-
 export default AlmanacGraphWrapped;
+
+/**
+ * manuel tabs 
+ *  <Tabs defaultIndex={1}>
+            <TabList>
+                <Tab>2018</Tab>
+                <Tab>2017</Tab>
+            </TabList>
+            <TabPanel>
+              <div style={getModalStyle()} className={classes.paper}>
+               <img src = {this.state.x}/>
+             </div> 
+          </TabPanel>
+
+            <TabPanel> <div style={getModalStyle()} className={classes.paper}>
+               <img src = {this.state.x}/>
+             </div> 
+             </TabPanel>
+          </Tabs>
+ */
