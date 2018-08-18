41f2b41bb46e1b573e9edfb47fa851d79f7f4482
pistachio05 <malaya@uci.edu>
2018-08-17 22:17:08 -0700

    passing responseText to a callback function

--- src/components/AlmanacGraph/FetchGraph.js
+++ src/components/AlmanacGraph/FetchGraph.js
@@ -14,59 +14,17 @@ let url_base = "https://summer18.herokuapp.com/";
       if (xmlhttp.readyState==4 && xmlhttp.status==200)
       {
           let imgSrc = xmlhttp.responseText;
+        /* This code to pasre the text response and extract the src from it 
+        currently we are passing the the text to  UITab
           let regex = /<embed*(.*?)>/g;
           let arr, outp = [];
           while ((arr = regex.exec(imgSrc))) 
               outp.push(arr[1]);
           let str = outp.toString();
           str = str.split(" ");
-          callback(str[4]);
+         */
+          callback(imgSrc);
       }
   };
   xmlhttp.send();    
 }
-
-
-
-/*
-export function getGraph(callBack)
-{
- //let graphUrl = url_base + q + "/" + y + "/" + code
-  request('https://summer18.herokuapp.com/w/18/36050', function(err, resp, html) 
-  {
-        if (!err)
-        {
-          const $ = cheerio.load(html);
-          let graph = $('#cheerio');
-          console.log("The Graph from CHerrio",graph.html());
-          callBack(graph);
-        }
-  });
-}
-*/
-//var url_base = "https://www.ics.uci.edu/~rang1/";
-/* OLD CODE
- export function getGraph(quarter, year, code)
-{
-  var graph_url = url_base + quarter+'/' + year + '/' + code;
-  console.log(graph_url);
-  https.get('',(resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
-      console.log("Chunk: " + data);
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(data);
-      return JSON.parse(data).explanation;
-    });
-
-  }).on("error", (err) => {
-    console.log("Error: " + err.name +"THE MESSAGE "+ err.message);
-  });
-}
- */
