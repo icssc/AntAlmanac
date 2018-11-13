2961042cf14a90e8262121e98ae88649047b0f05
Rango <34990962+the-rango@users.noreply.github.com>
2018-11-13 13:01:19 -0800

    Mobile threshold now at 1000 px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -72,7 +72,7 @@ class App extends Component {
   }
   
   resizeLogo() {
-    this.setState({ isDesktop: window.innerWidth > 1450 });
+    this.setState({ isDesktop: window.innerWidth > 1000 });
   }
 
   setView = viewNum => {
