e90927ad45128447f5791b3a45cda3581e0b05f7
Tianjiao Huang <maitian519@gmail.com>
2019-04-10 12:24:25 -0700

    use react-snap for better support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { render } from 'react-snapshot';
+import { hydrate, render } from "react-dom";
 import App from "./components/App/App";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import{unregister} from "./registerServiceWorker";
@@ -26,11 +25,15 @@ const theme = createMuiTheme({
   }
 });
 
-render(
-  <MuiThemeProvider theme={theme}>
-    <App style ={{height:"100%"}}/>
-  </MuiThemeProvider>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<MuiThemeProvider theme={theme}>
+    <App style={{ height: "100%" }} />
+  </MuiThemeProvider>, rootElement);
+} else {
+  render(<MuiThemeProvider theme={theme}>
+    <App style={{ height: "100%" }} />
+  </MuiThemeProvider>, rootElement);
+}
 
 unregister();
