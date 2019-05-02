5396258db68675a3a4b283a4adaba4fd3d39cd9c
lavanyak <33532127+lavanyak@users.noreply.github.com>
2019-05-01 18:15:44 -0700

    added ad for ads banner

--- src/components/CoursePane/CourseRenderPane.js
+++ src/components/CoursePane/CourseRenderPane.js
@@ -5,6 +5,7 @@ import CourseDetailPane from "./CourseDetailPane";
 import SchoolDeptCard from "./SchoolDeptCard";
 import MiniSectionTable from "./MiniSectionTable";
 import NoNothing from "./no_results.png";
+import AdAd from "./ad_ad.png";
 
 const styles = theme => ({
   course: {
@@ -18,6 +19,11 @@ const styles = theme => ({
     cursor: "pointer"
   },
   text: {
+    flexGrow: 1,
+    display: "inline",
+    width: "100%"
+  },
+  ad: {
     flexGrow: 1,
     display: "inline",
     width: "100%"
@@ -151,6 +157,9 @@ class CourseRenderPane extends Component {
           </div>
         ) : (
           <Grid container spacing={16}>
+            <Grid item md={12} xs={12}>
+                <a href="https://forms.gle/irQBrBkqHYYxcEU39" target="_blank" rel="noopener noreferrer" ><img src={AdAd} alt="" className={this.props.classes.ad}/></a>
+            </ Grid>
             {this.props.courseData.map(item => this.getGrid(item))}
           </Grid>
         )}
