f34741966b28f7fe0e006b4e584b3917e41a8b90
Ran Gu <earlguwang@gmail.com>
2019-07-13 22:21:09 -0700

    Minor dept changes

--- src/components/CoursePane/Ad.js
+++ src/components/CoursePane/Ad.js
@@ -67,7 +67,7 @@ export default class Advert extends Component {
   render() {
     return (
       <div>
-        <Typography fontSize="1">
+        <Typography fontSize="1" align="right">
           AntAlmanac is not affiliated with the following club/activity
         </Typography>
         <a
