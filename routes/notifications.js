e94b5d4a27b15ae875a60d80164974d6c8c9844f
Nathan <devsdevsdevs@github.com>
2020-11-18 23:53:20 -0800

    Fix notifications endpoint

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const Notification = require('../models/Notification')
+const connectToDb = require('../db')
 
 router.post('/registerNotifications', async (req, res) => {
+  await connectToDb();
+
   const {phoneNumber, sectionCode, courseTitle} = req.body;
 
   try {
@@ -18,11 +21,17 @@ router.post('/registerNotifications', async (req, res) => {
 });
 
 router.post('/lookupNotifications', async (req, res) => {
+  await connectToDb();
+
   try {
     const phoneNumber = req.body.phoneNumber
     const data = await Notification.find({ phoneNumbers: phoneNumber })
 
-    res.status(200).send({ smsNotificationList: data })
+    const smsNotificationsList = data.map((section) => {
+      return {sectionCode: section.sectionCode, courseTitle: section.courseTitle}
+    })
+
+    res.status(200).send({ smsNotificationList: smsNotificationsList })
   } catch (err) {
     res.status(500).json({error: err.message})
   }
