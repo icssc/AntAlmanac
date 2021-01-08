36e0eb9cef7cb8cf82a5965affdfc9272497177b
Nathan <nathan.abebayehu@gmail.com>
2021-01-07 21:50:02 -0800

    Add backend deployment config to API domain. Enable cors interaction domain.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const serverless = require('serverless-http');
 require('dotenv').config({path:__dirname + '/.env'})
 const bodyParser = require('body-parser');
 const routes = require('./routes')
-
+const cors = require('cors')
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: ['https://antalmanacdev.de', 'https://www.antalmanacdev.de']}))
 app.use('/api', routes)
 
 app.listen(8080, () => console.log('Server is running'))
