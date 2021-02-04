824834b66cfdc257453edf00431c17b942ade090
Nathan <nathan.abebayehu@gmail.com>
2021-02-04 00:04:04 -0800

    Modify peterportal API proxy to take API key

--- routes/peterportalapi.js
+++ routes/peterportalapi.js
@@ -4,7 +4,16 @@ const https = require('https');
 
 router.get('/courses/:deptCode/:courseNumber', async (req, res) => {
   const courseID = `${req.params.deptCode.replace(/\s/g, '')}${req.params.courseNumber.replace(/\s/g, '')}`
-  https.get(`https://api.peterportal.org/rest/v0/courses/${courseID}`, (resp) => {
+
+  const options = {
+    hostname: 'api.peterportal.org',
+    path: `/rest/v0/courses/${courseID}`,
+    headers: {
+      'x-api-key': process.env.PETERPORTAL_API_KEY
+    }
+  }
+
+  https.get(options, (resp) => {
     let data = '';
 
     resp.on('data', (chunk) => {data += chunk;});
