7efad39121241972daf4ea394dccaf975128cb00
Nathan <nathan.abebayehu@gmail.com>
2021-02-09 23:34:10 -0800

    Update index.html, endpoints for ads/domain name, remove react-snap, update serverless domain

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -1,13 +1,13 @@
 function endpointTransform(path) {
     if (process.env.NODE_ENV === 'development') return path;
-    else return `https://api.antalmanacdev.de${path}`;
+    else return `https://api.antalmanac.com${path}`;
 }
 
 export const WEBSOC_ENDPOINT = endpointTransform('/api/websocapi');
 export const LOOKUP_NOTIFICATIONS_ENDPOINT = endpointTransform('/api/notifications/lookupNotifications');
 export const REGISTER_NOTIFICATIONS_ENDPOINT = endpointTransform('/api/notifications/registerNotifications');
-export const RANDOM_AD_ENDPOINT = endpointTransform('/api/ads/getRandomAd');
-export const AD_IMAGE_ENDPOINT = endpointTransform('/api/ads/getAdImage');
+export const RANDOM_AD_ENDPOINT = endpointTransform('/api/banners/getRandomAd');
+export const AD_IMAGE_ENDPOINT = endpointTransform('/api/banners/getAdImage');
 export const LOAD_DATA_ENDPOINT = endpointTransform('/api/users/loadUserData');
 export const SAVE_DATA_ENDPOINT = endpointTransform('api/users/saveUserData');
 export const ENROLLMENT_DATA_ENDPOINT = endpointTransform('/api/enrollmentData');
