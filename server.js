0dfb37b1e3c8d2b27574e0249b723ed1f14cc243
Nathan <nathan.abebayehu@gmail.com>
2021-02-09 23:27:18 -0800

    Change CORS origins to match domain, image sending support

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const routes = require('./routes')
 const cors = require('cors')
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: ['https://antalmanacdev.de', 'https://www.antalmanacdev.de']}))
+app.use(cors({ origin: ['https://antalmanac.com', 'https://www.antalmanac.com']}))
 app.use('/api', routes)
 
 app.listen(8080, () => console.log('Server is running'))
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {binary: ['image/*']});
