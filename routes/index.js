67e29cec8adf92cb4d157e9a56ba53d973262cc9
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-08-16 02:43:01 -0700

    Remove PeterPortal api endpoint
    The frontend can query this directly, it doesn't need to go through the backend.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const enrollmentData = require('./enrollmentData')
 const notificationsRoute = require('./notifications')
 const usersRoute = require('./users')
 const websocapiRoute = require('./websocapi')
-const peterportalapiRoute = require('./peterportalapi')
 const newsRoute = require('./news')
 
 router.use('/banners', adsRoute)
@@ -14,7 +13,6 @@ router.use('/enrollmentData', enrollmentData)
 router.use('/notifications', notificationsRoute)
 router.use('/users', usersRoute)
 router.use('/websocapi', websocapiRoute)
-router.use('/peterportalapi', peterportalapiRoute)
 router.use('/news', newsRoute)
 
 module.exports = router
