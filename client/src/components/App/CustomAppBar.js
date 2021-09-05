52846b4abbbc8255606eeb89226fc63a8543ef40
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-09-04 23:18:38 -0700

    Mobile navbars (#186)
    * Create mobile navbar
    
    Navbar is now mobile friendly!
    On smaller screens, it will now only show the antalmanac logo (without the words) and save/load icons.
    Other navbar buttons will be behind a vertical dot dropdown
    
    Also converted navbar component to use react hooks
    
    * Create mobile calendar toolbar
    
    Toolbar now looks good on mobile!
    Moved Finals button next to schedule
    Hide the other buttons behind a horizontal triple dot menu
    
    Also converted component to react hooks
    
    * Make AppBar menu a column instead of row
    
    * remove unused import

--- client/src/components/App/CustomAppBar.js
+++ client/src/components/App/CustomAppBar.js
@@ -1,11 +1,14 @@
-import React, { PureComponent } from 'react';
-import { AppBar, Button, Toolbar, Tooltip } from '@material-ui/core';
+import React from 'react';
+import { AppBar, Button, Toolbar, Tooltip, Menu, useMediaQuery } from '@material-ui/core';
 import LoadSaveScheduleFunctionality from './LoadSaveFunctionality';
 import { Assignment } from '@material-ui/icons';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
 import NotificationHub from './NotificationHub';
 import SettingsMenu from './SettingsMenu';
 import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as MobileLogo } from './mobile-logo.svg';
 import News from './News';
 
 const styles = {
@@ -19,52 +22,72 @@ const styles = {
     },
 };
 
-class CustomAppBar extends PureComponent {
-    render() {
-        const { classes } = this.props;
+const ConditionalWrapper = ({ condition, wrapper, children }) => {
+    return condition ? wrapper(children) : children;
+};
 
-        return (
-            <AppBar position="static" className={classes.appBar}>
-                <Toolbar variant="dense">
-                    <Logo height={32} />
+const CustomAppBar = (props) => {
+    const { classes } = props;
 
-                    <div style={{ flexGrow: '1' }} />
+    const isMobileScreen = useMediaQuery('(max-width:750px)');
 
-                    <LoadSaveScheduleFunctionality />
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-                    <SettingsMenu />
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-                    <NotificationHub />
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-                    <Tooltip title="Give Us Feedback!">
-                        <Button
-                            onClick={() => {
-                                window.open('https://goo.gl/forms/eIHy4kp56pZKP9fK2', '_blank');
-                            }}
-                            color="inherit"
-                            startIcon={<Assignment />}
+    return (
+        <AppBar position="static" className={classes.appBar}>
+            <Toolbar variant="dense">
+                {isMobileScreen ? <MobileLogo height={32} /> : <Logo height={32} />}
+
+                <div style={{ flexGrow: '1' }} />
+
+                <LoadSaveScheduleFunctionality />
+
+                <ConditionalWrapper
+                    condition={isMobileScreen}
+                    wrapper={(children) => (
+                        <div>
+                            <MenuIcon onClick={handleClick} />
+                            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                                {children}
+                            </Menu>
+                        </div>
+                    )}
+                >
+                    {[
+                        <SettingsMenu />,
+                        <NotificationHub />,
+                        <Tooltip title="Give Us Feedback!">
+                            <Button
+                                onClick={() => {
+                                    window.open('https://goo.gl/forms/eIHy4kp56pZKP9fK2', '_blank');
+                                }}
+                                color="inherit"
+                                startIcon={<Assignment />}
+                            >
+                                Feedback
+                            </Button>
+                        </Tooltip>,
+                        <News />,
+                    ].map((element) => (
+                        <ConditionalWrapper
+                            condition={isMobileScreen}
+                            wrapper={(children) => <MenuItem>{children}</MenuItem>}
                         >
-                            Feedback
-                        </Button>
-                    </Tooltip>
-
-                    <News />
-
-                    {/*<Tooltip title="Info Page">*/}
-                    {/*<Button*/}
-                    {/*    onClick={() => {*/}
-                    {/*        window.open('https://www.ics.uci.edu/~rang1/AntAlmanac/index.html', '_blank');*/}
-                    {/*    }}*/}
-                    {/*    color="inherit"*/}
-                    {/*    startIcon={<Info />}*/}
-                    {/*>*/}
-                    {/*    About*/}
-                    {/*</Button>*/}
-                    {/*</Tooltip>*/}
-                </Toolbar>
-            </AppBar>
-        );
-    }
-}
+                            {element}
+                        </ConditionalWrapper>
+                    ))}
+                </ConditionalWrapper>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default withStyles(styles)(CustomAppBar);
