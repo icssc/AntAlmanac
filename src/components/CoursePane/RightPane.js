8982a53bb0a9f0c5655cc2ddfda364edd46796cc
Esteban Aldana <estebanaaldana@gmail.com>
2022-03-21 23:45:19 -0700

    Search Requirements
    this.props.toggleSearch() checks if GE, Department, Course Code/Range, or Instructor are still set to their default values. If none of them have been changed, the search is not performed and an error message is displayed. If one of them has been changed, the search is performed as normal.

--- src/components/CoursePane/RightPane.js
+++ src/components/CoursePane/RightPane.js
@@ -5,6 +5,8 @@ import CourseRenderPane from './CourseRenderPane';
 import { withStyles } from '@material-ui/core/styles';
 import RightPaneStore from '../../stores/RightPaneStore';
 import dispatcher from '../../dispatcher';
+import SectionCodeSearchBar from '../SearchForm/SectionCodeSearchBar';
+import { openSnackbar } from '../../actions/AppStoreActions';
 
 const styles = {
     container: {
@@ -14,10 +16,19 @@ const styles = {
 
 class RightPane extends PureComponent {
     toggleSearch = () => {
-        dispatcher.dispatch({
-            type: 'TOGGLE_SEARCH',
-        });
-        this.forceUpdate();
+        if(RightPaneStore.getFormData().ge != 'ANY' || RightPaneStore.getFormData().deptValue != 'ALL' || 
+            RightPaneStore.getFormData().sectionCode != "" || RightPaneStore.getFormData().instructor != ""){
+            dispatcher.dispatch({
+                type: 'TOGGLE_SEARCH',
+            });
+            this.forceUpdate();
+        }
+        else{
+            openSnackbar(
+                'error',
+                `Please provide one of the following: GE, Department, Course Code or Range, or Instructor`
+            );
+        }
     };
 
     render() {
