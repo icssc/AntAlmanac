a08fe497ac7d3e8574350ba48354010e318b85ed
Chase Carnaroli <chasecarnaroli@gmail.com>
2022-04-14 19:49:10 -0700

    Stick Privacy Banner on the bottom

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -16,6 +16,10 @@ const styles = {
         borderTop: 'solid 8px transparent',
         display: 'inline-flex',
     },
+    form: {
+        minHeight: 'calc(100% - 120px)',
+        marginBottom: '20px',
+    },
 };
 
 class SearchForm extends PureComponent {
@@ -28,21 +32,22 @@ class SearchForm extends PureComponent {
         const { classes } = this.props;
 
         return (
-            <form onSubmit={this.onFormSubmit}>
-                <div className={classes.container}>
-                    <div className={classes.margin}>
-                        <TermSelector changeState={updateFormValue} fieldName={'term'} />
-                    </div>
-
+            <>
+                <form onSubmit={this.onFormSubmit} className={classes.form}>
                     <div className={classes.container}>
-                        <FuzzySearch toggleSearch={this.props.toggleSearch} />
+                        <div className={classes.margin}>
+                            <TermSelector changeState={updateFormValue} fieldName={'term'} />
+                        </div>
+
+                        <div className={classes.container}>
+                            <FuzzySearch toggleSearch={this.props.toggleSearch} />
+                        </div>
+
+                        <LegacySearch onSubmit={() => this.props.toggleSearch()} onReset={resetFormValues} />
                     </div>
-
-                    <LegacySearch onSubmit={() => this.props.toggleSearch()} onReset={resetFormValues} />
-
-                    <PrivacyPolicyBanner />
-                </div>
-            </form>
+                </form>
+                <PrivacyPolicyBanner />
+            </>
         );
     }
 }
