298f85ca68cba6eb57fe6aa93818cdb8dde080bd
Weiguang Wu <wwg12121@gmail.com>
2022-06-10 14:32:37 -0700

    Add getDefaultTerm function to termData.js, updated the imports of it in other files, and changed the usage of termData[defaultTerm] to getDefaultTerm()

--- src/components/EnrollmentGraph/GraphModalContent.js
+++ src/components/EnrollmentGraph/GraphModalContent.js
@@ -3,7 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import { FormControl, InputLabel, MenuItem, Paper, Select, Typography } from '@material-ui/core';
 import GraphRenderPane from './GraphRenderPane';
 import { queryWebsoc } from '../../helpers';
-import { termData, defaultTerm } from '../../termData';
+import { termData, defaultTerm, getDefaultTerm } from '../../termData';
 
 const styles = {
     paper: {
