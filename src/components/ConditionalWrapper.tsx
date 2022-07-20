4b41d691d13d69621fd84f1ba090d08a2e8476a6
Eric Pedley <ericpedley@gmail.com>
2022-07-19 19:19:04 -0500

    convert ConditionalWrapper

---
+++ src/components/ConditionalWrapper.tsx
@@ -0,0 +1,15 @@
+import {forwardRef, ReactElement} from 'react';
+
+interface ConditionalWrapperProps {
+    condition: boolean,
+    wrapper: (children: ReactElement) => ReactElement
+    children: ReactElement
+}
+
+// This uses forwardRef because at some point we got a console warning
+// about "functional components cannot be given refs" (https://github.com/icssc/AntAlmanac/pull/231/commits/bd80dd085e4f502292b4e1002fdc8fa398f375ab)
+const ConditionalWrapper = forwardRef<Element, ConditionalWrapperProps>(({condition, wrapper, children}, ref) => {
+    return condition ? wrapper(children) : children;
+});
+
+export default ConditionalWrapper;
