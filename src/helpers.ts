76ad65422efe11142cd009588bc7d5175d892f1f
Eric Pedley <ericpedley@gmail.com>
2022-11-08 12:49:33 -0800

    add grades type

--- src/helpers.ts
+++ src/helpers.ts
@@ -156,7 +156,18 @@ export async function queryWebsoc(params: Record<string,string>): Promise<Websoc
     }
 }
 
-const gradesCache: {[key: string]: any} = {};// TODO replace this any
+interface Grades {
+    average_gpa: number;
+    sum_grade_a_count: number;
+    sum_grade_b_count: number;
+    sum_grade_c_count: number;
+    sum_grade_d_count: number;
+    sum_grade_f_count: number;
+    sum_grade_np_count: number;
+    sum_grade_p_count: number;
+}
+
+const gradesCache: {[key: string]: Grades} = {};
 
 export async function queryGrades(deptCode: string, courseNumber: string) {
     if (gradesCache[deptCode + courseNumber]) {
