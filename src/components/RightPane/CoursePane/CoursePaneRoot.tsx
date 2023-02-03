b18e7173faee26912ed82b4cdb04e7df58f00a65
senti <brianmv@uci.edu>
2023-02-02 20:49:58 -0800

    updated imports for CoursePaneRoot

--- src/components/RightPane/CoursePane/CoursePaneRoot.tsx
+++ src/components/RightPane/CoursePane/CoursePaneRoot.tsx
@@ -1,4 +1,4 @@
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@mui/styles';
 import React, { PureComponent } from 'react';
 
 import { openSnackbar } from '../../../actions/AppStoreActions';
