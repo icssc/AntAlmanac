718bbe89b4d1448071332d1a4e052a37e2bd2a14
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-02-05 21:11:44 -0800

    Duplicate section meetings from different locations (#465)

--- src/components/RightPane/SectionTable/SectionTableLazyWrapper.tsx
+++ src/components/RightPane/SectionTable/SectionTableLazyWrapper.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 
 import { SectionTableProps } from './SectionTable.types';
 
+// This should be in SectionTable.tsx, IMO
 const SectionTable = React.lazy(() => import('./SectionTable'));
 
 export default function SectionTableLazyWrapper(props: SectionTableProps) {
