c64e3f04d50d9e3d7de24bbcd179efea8b99dfd5
senti <brianmv@uci.edu>
2023-02-07 12:21:50 -0800

    configured vite for building

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), visualizer()],
   resolve: {
     alias: {
-      $lib: 'src/lib',
-      $components: 'src/components',
+      $lib: resolve(__dirname, 'src/lib'),
+      $components: resolve(__dirname, 'src/components'),
     },
   },
 });
