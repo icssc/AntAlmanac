6380212c9ced7ba78e21cd610735d6a597251d6c
senti <brianmv@uci.edu>
2023-02-07 18:15:45 -0800

    done refactoring the massive store

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,36 +1,62 @@
+import { AppStoreCourse, CourseEvent, RepeatingCustomEvent, ZotCustomEvent } from '../types';
+
 /**
  * @param courseNumber represents the course number of a course (eg. '122A', '121')
  * @returns an int or number with a decimal representation of the passed in string
  * @example courseNumAsDecimal('122A') returns 122.1, courseNumAsDecimal('121') returns 121
  */
 export function courseNumAsDecimal(courseNumber: string) {
-  // I wanted to split the course detail number into letters and digits
+  /**
+   * split the course detail number into letters and digits
+   */
   const courseNumArr = courseNumber.split(/(\d+)/);
-  // Gets rid of empty strings in courseNumArr
+
+  /**
+   * get rid of empty strings in courseNumArr
+   */
   const filtered = courseNumArr.filter((value) => value !== '');
 
-  // Return 0 if array is empty
+  /**
+   * Return 0 if array is empty
+   */
   if (filtered.length === 0) {
     console.error(`No characters were found, returning 0, Input: ${courseNumber}`);
     return 0;
   }
 
-  const lastElement = filtered[filtered.length - 1].toUpperCase(); // .toUpperCase() won't affect numeric characters
-  const lastElementCharCode = lastElement.charCodeAt(0); // Just checks the first character of the last element in the array
-  // Return the last element of the filtered array as an integer if it represents an integer
+  /**
+   * won't affect numeric characters
+   */
+  const lastElement = filtered[filtered.length - 1].toUpperCase();
+
+  /**
+   * checks first character of the last element
+   */
+  const lastElementCharCode = lastElement.charCodeAt(0);
+
+  /**
+   * return last element of the filtered array if it's an integer
+   */
   if ('0'.charCodeAt(0) <= lastElementCharCode && lastElementCharCode <= '9'.charCodeAt(0)) {
     return parseInt(lastElement);
   }
 
-  // If the string does not have any numeric characters
+  /**
+   * If the string does not have any numeric characters
+   */
   if (filtered.length === 1) {
     console.error(`The string did not have numbers, returning 0, Input: ${courseNumber}`);
     return 0;
   }
 
-  // This element is the second to last element of the array, supposedly a string of numeric characters
+  /**
+   * string of numeric characters
+   */
   const secondToLastElement = filtered[filtered.length - 2];
-  // The characters within [A-I] or [a-i] will be converted to 1-9, respectively
+
+  /**
+   * characters within [A-I] or [a-i] will be converted to 1-9, respectively
+   */
   const letterAsNumber = lastElement.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0) + 1;
   if (1 <= letterAsNumber && letterAsNumber <= 9) {
     return parseFloat(`${secondToLastElement}.${letterAsNumber}`);
@@ -40,8 +66,182 @@ export function courseNumAsDecimal(courseNumber: string) {
         filtered[filtered.length - 1][0]
       }, Input: ${courseNumber}`
     );
-    // This will represent an integer at this point because the split in the beginning split the array into strings of digits and strings of other characters
-    // If the last element in the array does not represent an integer, then the second to last element must represent an integer
+
+    /**
+     * should be an integer because string was initially split into strings of digits
+     * if last element in the array is not integer, then second to last should be
+     */
     return parseInt(secondToLastElement);
   }
+}
+
+/**
+ * @param addedCourses courses that have been added to the schedule
+ * @param scheduleNames names of the schedules
+ * @returns schedule names mapped to the section codes and course term
+ */
+export function getSectionCodes(addedCourses: AppStoreCourse[], scheduleNames: string[]) {
+  const addedSectionCodes = {};
+
+  for (let i = 0; i < scheduleNames.length; i++) {
+    addedSectionCodes[i] = new Set();
+  }
+
+  for (const course of addedCourses) {
+    for (const scheduleIndex of course.scheduleIndices) {
+      addedSectionCodes[scheduleIndex].add(`${course.section.sectionCode} ${course.term}`);
+    }
+  }
+  return addedSectionCodes;
+}
+
+/**
+ * @param addedCourses courses that have been added to the schedule
+ * @returns an array of course events that can be added to the calendar
+ */
+export function calendarizeCourseEvents(addedCourses: AppStoreCourse[]) {
+  const courseEventsInCalendar: CourseEvent[] = [];
+
+  for (const course of addedCourses) {
+    for (const meeting of course.section.meetings) {
+      const timeString = meeting.time.replace(/\s/g, '');
+
+      if (timeString !== 'TBA') {
+        const [, startHrStr, startMinStr, endHrStr, endMinStr, ampm] = timeString.match(
+          /(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(p?)/
+        ) as RegExpMatchArray;
+
+        let startHr = parseInt(startHrStr, 10);
+        const startMin = parseInt(startMinStr, 10);
+        let endHr = parseInt(endHrStr, 10);
+        const endMin = parseInt(endMinStr, 10);
+
+        const dates = [
+          meeting.days.includes('Su'),
+          meeting.days.includes('M'),
+          meeting.days.includes('Tu'),
+          meeting.days.includes('W'),
+          meeting.days.includes('Th'),
+          meeting.days.includes('F'),
+          meeting.days.includes('Sa'),
+        ];
+
+        if (ampm === 'p' && endHr !== 12) {
+          startHr += 12;
+          endHr += 12;
+          if (startHr > endHr) startHr -= 12;
+        }
+
+        dates.forEach((shouldBeInCal, index) => {
+          if (shouldBeInCal) {
+            const newEvent = {
+              color: course.color,
+              term: course.term,
+              title: course.deptCode + ' ' + course.courseNumber,
+              courseTitle: course.courseTitle,
+              bldg: meeting.bldg,
+              instructors: course.section.instructors,
+              sectionCode: course.section.sectionCode,
+              sectionType: course.section.sectionType,
+              start: new Date(2018, 0, index, startHr, startMin),
+              finalExam: course.section.finalExam,
+              end: new Date(2018, 0, index, endHr, endMin),
+              isCustomEvent: false as const,
+              scheduleIndices: course.scheduleIndices,
+            };
+
+            courseEventsInCalendar.push(newEvent as any);
+          }
+        });
+      }
+    }
+  }
+
+  return courseEventsInCalendar;
+}
+
+/**
+ * @param customEvents custom events that have been added to the schedule
+ * @returns an array of calendar events
+ */
+export function calendarizeCustomEvents(customEvents: RepeatingCustomEvent[]) {
+  const customEventsInCalendar: ZotCustomEvent[] = [];
+
+  for (const customEvent of customEvents) {
+    for (let dayIndex = 0; dayIndex < customEvent.days.length; dayIndex++) {
+      if (customEvent.days[dayIndex]) {
+        const startHour = parseInt(customEvent.start.slice(0, 2), 10);
+        const startMin = parseInt(customEvent.start.slice(3, 5), 10);
+        const endHour = parseInt(customEvent.end.slice(0, 2), 10);
+        const endMin = parseInt(customEvent.end.slice(3, 5), 10);
+
+        customEventsInCalendar.push({
+          customEventID: customEvent.customEventID,
+          color: customEvent.color || '',
+          start: new Date(2018, 0, dayIndex, startHour, startMin),
+          isCustomEvent: true,
+          end: new Date(2018, 0, dayIndex, endHour, endMin),
+          scheduleIndices: customEvent.scheduleIndices,
+          title: customEvent.title,
+        });
+      }
+    }
+  }
+
+  return customEventsInCalendar as ZotCustomEvent[];
+}
+
+/**
+ * @param addedCourses courses that have been added to the schedule
+ * @returns an array of calendar events
+ */
+export function calendarizeFinals(addedCourses: AppStoreCourse[]) {
+  const finalsEventsInCalendar = [] as CourseEvent[];
+
+  for (const course of addedCourses) {
+    const finalExam = course.section.finalExam;
+    if (finalExam.length > 5) {
+      const [, date, , , startStr, startMinStr, endStr, endMinStr, ampm] = finalExam.match(
+        /([A-za-z]+) ([A-Za-z]+) *(\d{1,2}) *(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(am|pm)/
+      ) as RegExpMatchArray;
+      // TODO: this block is almost the same as in calenarizeCourseEvents. we should refactor to remove the duplicate code.
+      let startHour = parseInt(startStr, 10);
+      const startMin = parseInt(startMinStr, 10);
+      let endHour = parseInt(endStr, 10);
+      const endMin = parseInt(endMinStr, 10);
+      const weekdayInclusion: boolean[] = [
+        date.includes('Sat'),
+        date.includes('Sun'),
+        date.includes('Mon'),
+        date.includes('Tue'),
+        date.includes('Wed'),
+        date.includes('Thu'),
+        date.includes('Fri'),
+      ];
+      if (ampm === 'pm' && endHour !== 12) {
+        startHour += 12;
+        endHour += 12;
+        if (startHour > endHour) startHour -= 12;
+      }
+
+      weekdayInclusion.forEach((shouldBeInCal, index) => {
+        if (shouldBeInCal)
+          finalsEventsInCalendar.push({
+            title: course.deptCode + ' ' + course.courseNumber,
+            sectionCode: course.section.sectionCode,
+            sectionType: 'Fin',
+            bldg: course.section.meetings[0].bldg,
+            color: course.color,
+            scheduleIndices: course.scheduleIndices,
+            start: new Date(2018, 0, index - 1, startHour, startMin),
+            end: new Date(2018, 0, index - 1, endHour, endMin),
+            finalExam: course.section.finalExam,
+            instructors: course.section.instructors,
+            term: course.term,
+            isCustomEvent: false,
+          } as any);
+      });
+    }
+  }
+  return finalsEventsInCalendar;
 }
--- src/lib/stores/schedule.ts
+++ src/lib/stores/schedule.ts
@@ -1,255 +1,13 @@
 import { create } from 'zustand';
-import { AppStoreCourse, CourseEvent, RepeatingCustomEvent, ZotCustomEvent } from '../../types';
-
-/**
- */
-export function getSectionCodes(addedCourses: AppStoreCourse[], scheduleNames: string[]) {
-  const addedSectionCodes = {};
-
-  for (let i = 0; i < scheduleNames.length; i++) {
-    addedSectionCodes[i] = new Set();
-  }
-
-  for (const course of addedCourses) {
-    for (const scheduleIndex of course.scheduleIndices) {
-      addedSectionCodes[scheduleIndex].add(`${course.section.sectionCode} ${course.term}`);
-    }
-  }
-  return addedSectionCodes;
-}
-
-/**
- */
-export function calendarizeCourseEvents(addedCourses: AppStoreCourse[]) {
-  const courseEventsInCalendar: CourseEvent[] = [];
-
-  for (const course of addedCourses) {
-    for (const meeting of course.section.meetings) {
-      const timeString = meeting.time.replace(/\s/g, '');
-
-      if (timeString !== 'TBA') {
-        const [, startHrStr, startMinStr, endHrStr, endMinStr, ampm] = timeString.match(
-          /(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(p?)/
-        ) as RegExpMatchArray;
-
-        let startHr = parseInt(startHrStr, 10);
-        const startMin = parseInt(startMinStr, 10);
-        let endHr = parseInt(endHrStr, 10);
-        const endMin = parseInt(endMinStr, 10);
-
-        const dates = [
-          meeting.days.includes('Su'),
-          meeting.days.includes('M'),
-          meeting.days.includes('Tu'),
-          meeting.days.includes('W'),
-          meeting.days.includes('Th'),
-          meeting.days.includes('F'),
-          meeting.days.includes('Sa'),
-        ];
-
-        if (ampm === 'p' && endHr !== 12) {
-          startHr += 12;
-          endHr += 12;
-          if (startHr > endHr) startHr -= 12;
-        }
-
-        dates.forEach((shouldBeInCal, index) => {
-          if (shouldBeInCal) {
-            const newEvent = {
-              color: course.color,
-              term: course.term,
-              title: course.deptCode + ' ' + course.courseNumber,
-              courseTitle: course.courseTitle,
-              bldg: meeting.bldg,
-              instructors: course.section.instructors,
-              sectionCode: course.section.sectionCode,
-              sectionType: course.section.sectionType,
-              start: new Date(2018, 0, index, startHr, startMin),
-              finalExam: course.section.finalExam,
-              end: new Date(2018, 0, index, endHr, endMin),
-              isCustomEvent: false as const,
-              scheduleIndices: course.scheduleIndices,
-            };
-
-            courseEventsInCalendar.push(newEvent as any);
-          }
-        });
-      }
-    }
-  }
-
-  return courseEventsInCalendar;
-}
-
-export function calendarizeCustomEvents(customEvents: RepeatingCustomEvent[]) {
-  const customEventsInCalendar: ZotCustomEvent[] = [];
-
-  for (const customEvent of customEvents) {
-    for (let dayIndex = 0; dayIndex < customEvent.days.length; dayIndex++) {
-      if (customEvent.days[dayIndex]) {
-        const startHour = parseInt(customEvent.start.slice(0, 2), 10);
-        const startMin = parseInt(customEvent.start.slice(3, 5), 10);
-        const endHour = parseInt(customEvent.end.slice(0, 2), 10);
-        const endMin = parseInt(customEvent.end.slice(3, 5), 10);
-
-        customEventsInCalendar.push({
-          customEventID: customEvent.customEventID,
-          color: customEvent.color || '',
-          start: new Date(2018, 0, dayIndex, startHour, startMin),
-          isCustomEvent: true,
-          end: new Date(2018, 0, dayIndex, endHour, endMin),
-          scheduleIndices: customEvent.scheduleIndices,
-          title: customEvent.title,
-        });
-      }
-    }
-  }
-
-  return customEventsInCalendar as ZotCustomEvent[];
-}
-
-export function calendarizeFinals(addedCourses: AppStoreCourse[]) {
-  const finalsEventsInCalendar = [] as CourseEvent[];
-
-  for (const course of addedCourses) {
-    const finalExam = course.section.finalExam;
-    if (finalExam.length > 5) {
-      const [, date, , , startStr, startMinStr, endStr, endMinStr, ampm] = finalExam.match(
-        /([A-za-z]+) ([A-Za-z]+) *(\d{1,2}) *(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(am|pm)/
-      ) as RegExpMatchArray;
-      // TODO: this block is almost the same as in calenarizeCourseEvents. we should refactor to remove the duplicate code.
-      let startHour = parseInt(startStr, 10);
-      const startMin = parseInt(startMinStr, 10);
-      let endHour = parseInt(endStr, 10);
-      const endMin = parseInt(endMinStr, 10);
-      const weekdayInclusion: boolean[] = [
-        date.includes('Sat'),
-        date.includes('Sun'),
-        date.includes('Mon'),
-        date.includes('Tue'),
-        date.includes('Wed'),
-        date.includes('Thu'),
-        date.includes('Fri'),
-      ];
-      if (ampm === 'pm' && endHour !== 12) {
-        startHour += 12;
-        endHour += 12;
-        if (startHour > endHour) startHour -= 12;
-      }
-
-      weekdayInclusion.forEach((shouldBeInCal, index) => {
-        if (shouldBeInCal)
-          finalsEventsInCalendar.push({
-            title: course.deptCode + ' ' + course.courseNumber,
-            sectionCode: course.section.sectionCode,
-            sectionType: 'Fin',
-            bldg: course.section.meetings[0].bldg,
-            color: course.color,
-            scheduleIndices: course.scheduleIndices,
-            start: new Date(2018, 0, index - 1, startHour, startMin),
-            end: new Date(2018, 0, index - 1, endHour, endMin),
-            finalExam: course.section.finalExam,
-            instructors: course.section.instructors,
-            term: course.term,
-            isCustomEvent: false,
-          } as any);
-      });
-    }
-  }
-  return finalsEventsInCalendar;
-}
-
-// import { AASection } from '$lib/peterportal.types';
-//
-// export interface AppStoreCourse {
-//   color: string;
-//   courseComment: string;
-//   courseNumber: string; //i.e. 122a
-//   courseTitle: string;
-//   deptCode: string;
-//   prerequisiteLink: string;
-//   scheduleIndices: number[];
-//   section: AASection;
-//   term: string;
-// }
-//
-// export interface CommonCalendarEvent extends Partial<Event> {
-//   color: string;
-//   start: Date;
-//   end: Date;
-//   scheduleIndices: number[];
-//   title: string;
-// }
-//
-// export interface CourseEvent extends CommonCalendarEvent {
-//   bldg: string;
-//   finalExam: string;
-//   instructors: string[];
-//   isCustomEvent: false;
-//   sectionCode: string;
-//   sectionType: string;
-//   term: string;
-// }
-//
-// /**
-//  * There is another CustomEvent interface in CourseCalendarEvent and they are slightly different.
-//  * This one encapsulates the occurences of an event on multiple days,
-//  * like Monday Tuesday Wednesday all in the same object as specified by the days array.
-//  * The other one, `CustomEventDialog`'s CustomEvent, represents only one day,
-//  * like the event on Monday, and needs to be duplicated to be repeated across multiple days.
-//  * @see {@link https://github.com/icssc/AntAlmanac/wiki/The-Great-AntAlmanac-TypeScript-Rewritening%E2%84%A2#duplicate-interface-names-%EF%B8%8F}
-//  */
-// export interface RepeatingCustomEvent {
-//   title: string;
-//   start: string;
-//   end: string;
-//   days: boolean[];
-//   scheduleIndices: number[];
-//   customEventID: number;
-//   color?: string;
-// }
-//
-// /**
-//  * There is another CustomEvent interface in CourseCalendarEvent and they are slightly different.
-//  * The this one represents only one day, like the event on Monday, and needs to be duplicated to be repeated across multiple days.
-//  * The other one, `CustomEventDialog`'s `RepeatingCustomEvent`,
-//  * encapsulates the occurences of an event on multiple days,
-//  * like Monday Tuesday Wednesday all in the same object as specified by the `days` array.
-//  * @see {@link https://github.com/icssc/AntAlmanac/wiki/The-Great-AntAlmanac-TypeScript-Rewritening%E2%84%A2#duplicate-interface-names-%EF%B8%8F}
-//  */
-// export interface ZotCustomEvent extends CommonCalendarEvent {
-//   customEventID: number;
-//   isCustomEvent: true;
-// }
-//
-// export type CalendarEvent = CourseEvent | ZotCustomEvent;
-//
-// export interface CourseData {
-//   addedCourses: AppStoreCourse[];
-//   scheduleNames: string[];
-//   customEvents: RepeatingCustomEvent[];
-// }
-//
-// export interface SnackbarPosition {
-//   horizontal: 'left' | 'right';
-//   vertical: 'bottom' | 'top';
-// }
-//
-// export interface ShortCourseInfo {
-//   color: string;
-//   term: string;
-//   sectionCode: string;
-//   scheduleIndices: number[];
-// }
-// export interface UserData {
-//   addedCourses: ShortCourseInfo[];
-//   scheduleNames: string[];
-//   customEvents: RepeatingCustomEvent[];
-// }
-//
-// export interface AppStoreDeletedCourse extends AppStoreCourse {
-//   scheduleIndex: number;
-// }
+import { calendarizeCourseEvents, calendarizeCustomEvents, calendarizeFinals, getSectionCodes } from '$lib/helpers';
+import {
+  AppStoreCourse,
+  AppStoreDeletedCourse,
+  CalendarEvent,
+  CourseData,
+  CourseEvent,
+  RepeatingCustomEvent,
+} from '../../types';
 
 /**
  * shared schedule store
@@ -285,7 +43,10 @@ export interface ScheduleStore {
   changeCourseColor: (addedCourses: AppStoreCourse[], sectionCode: string, color: string) => void;
 }
 
-export const useAppStore = create<ScheduleStore>((set, get) => ({
+/**
+ * hook to use the shared schedule store
+ */
+export const useScheduleStore = create<ScheduleStore>((set, get) => ({
   currentScheduleIndex: 0,
   customEvents: [],
   addedCourses: [],
@@ -510,6 +271,7 @@ export const useAppStore = create<ScheduleStore>((set, get) => ({
       unsavedChanges,
     }));
   },
+
   clearSchedule(addedCourses: AppStoreCourse[], customEvents: RepeatingCustomEvent[]) {
     const currentStoreValues = get();
     const currentScheduleNames = currentStoreValues.scheduleNames;
