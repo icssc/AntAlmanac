4fb9c92c47ada14a28eaee8e2cc876ea942fd92a
senti <brianmv@uci.edu>
2023-02-09 23:58:24 -0800

    included demo of new state architecture

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -1,7 +1,9 @@
 import { AppBar, Button, Toolbar } from '@mui/material';
 import { Assignment as AssignmentIcon } from '@mui/icons-material';
+import { useScheduleStore } from '$stores/schedule';
 
 export default function ActionsBar() {
+  const { scheduleIndex } = useScheduleStore()
   return (
     <AppBar position="static" sx={{ marginBottom: '4px' }}>
       <Toolbar variant="dense" sx={{ justifyContent: 'space-evenly' }}>
@@ -14,6 +16,7 @@ export default function ActionsBar() {
         <Button color="inherit" startIcon={<AssignmentIcon />}>
           Import
         </Button>
+        schedule index: {scheduleIndex}
       </Toolbar>
     </AppBar>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,24 @@
+import { Button } from '@mui/material'
 import Header from '$components/Header';
 import Actions from '$components/Actions';
+import { useScheduleStore } from '$stores/schedule';
+import { setScheduleIndex } from '$stores/schedule/schedule'
 
 /**
  * home page
  */
 export default function Home() {
+  const { scheduleIndex } = useScheduleStore()
+
+  function handleClick() {
+    setScheduleIndex(scheduleIndex + 1)
+  }
+
   return (
     <>
       <Header />
       <Actions />
+      <Button onClick={handleClick} variant="contained">Increment Schedule Index</Button>
     </>
   );
 }
