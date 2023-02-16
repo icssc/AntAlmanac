906c4997ce4f7419e3cca56cbb69d891705be3ab
senti <brianmv@uci.edu>
2023-02-15 20:30:36 -0800

    added settings store

---
+++ src/stores/settings.ts
@@ -0,0 +1,68 @@
+/**
+ * shared state for website settings, e.g. color scheme, finals mode, etc.
+ */
+
+import { create } from 'zustand'
+
+/**
+ * retrive the color scheme from the browser
+ */
+function getColorScheme() {
+  return typeof Storage === 'undefined' ? 'auto' : window.localStorage.getItem('colorScheme') || 'auto'
+}
+
+interface SettingsStore {
+  /**
+   * whether the website is currently displaying finals
+   */
+  showFinals: boolean
+
+  /**
+   * control whether the website is currently displaying finals
+   */
+  setShowFinals: (showFinals: boolean) => void
+
+  /**
+   * current color scheme
+   */
+  colorScheme: string
+
+  /**
+   * set the new color schema and save it to local storage
+   */
+  setColorScheme: (scheme: string) => void
+
+  /**
+   * function to determine if the current color scheme is dark
+   */
+  isDarkMode: () => boolean
+}
+
+/**
+ * hook for accessing the shared settings store
+ */
+export const useSettingsStore = create<SettingsStore>((set, get) => ({
+  showFinals: false,
+
+  setShowFinals(showFinals: boolean) {
+    set({ showFinals })
+  },
+
+  colorScheme: getColorScheme(),
+
+  setColorScheme(colorScheme: string) {
+    window.localStorage.setItem('colorScheme', colorScheme)
+    set({ colorScheme })
+  },
+
+  isDarkMode() {
+    switch (get().colorScheme) {
+      case 'light':
+        return false
+      case 'dark':
+        return true
+      default:
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+    }
+  },
+}))
