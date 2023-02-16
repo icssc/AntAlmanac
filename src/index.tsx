274e7a6a6712c5caef446d53a2243cfa11d6d163
senti <brianmv@uci.edu>
2023-02-16 06:19:44 -0800

    moved index.jsx to index.tsx
    - now it's typesafe :sunglasses:
    - renamed the script import in index.html to match

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
-import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import { SnackbarProvider } from 'notistack';
-import React from 'react';
 import { render } from 'react-dom';
-
+import { SnackbarProvider } from 'notistack';
+import { createTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import App from './components/App';
 import { unregister } from './registerServiceWorker';
-// import whyDidYouRender from '@welldone-software/why-did-you-render';
-// if (process.env.NODE_ENV === 'development') {
-// const whyDidYouRender = require('@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js');
-//     whyDidYouRender(React, {
-//         trackAllPureComponents: true,
-//     });
-// // }
+
 const theme = createTheme({
     typography: {
         htmlFontSize: parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('font-size'), 10),
         fontSize: parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('font-size'), 10) * 0.9,
     },
     palette: {
-        // type: 'dark',
         primary: {
             light: '#5191d6',
             main: '#0064a4',
@@ -35,17 +26,12 @@ const theme = createTheme({
     spacing: 4,
 });
 
-// if (process.env.NODE_ENV === 'development') {
-//     whyDidYouRender(React, {
-//         trackAllPureComponents: true,
-//     });
-// }
-
 const rootElement = document.getElementById('root');
+
 render(
     <MuiThemeProvider theme={theme}>
         <SnackbarProvider>
-            <App style={{ height: '100%' }} />
+            <App />
         </SnackbarProvider>
     </MuiThemeProvider>,
     rootElement
