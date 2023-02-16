15e88853f423fbb3d9f818ddc8763ff759ef4cce
senti <brianmv@uci.edu>
2023-02-16 12:11:12 -0800

    added store for keeping track of search form

---
+++ src/stores/search.ts
@@ -0,0 +1,154 @@
+/**
+ * shared state for the course search
+ */
+
+import { create } from 'zustand'
+import { getDefaultTerm } from '$lib/termData'
+
+/**
+ * all form values
+ */
+export interface FormValues {
+  deptValue: string
+  deptLabel: string
+  ge: string
+  term: string
+  courseNumber: string
+  sectionCode: string
+  instructor: string
+  units: string
+  endTime: string
+  startTime: string
+  coursesFull: string
+  building: string
+  room: string
+}
+
+/**
+ * params used for the websoc query
+ */
+interface WebsocParams {
+  department: FormValues['deptValue']
+  term: FormValues['term']
+  ge: FormValues['ge']
+  courseNumber: FormValues['courseNumber']
+  sectionCode: FormValues['sectionCode']
+  instructorName: FormValues['instructor']
+  units: FormValues['units']
+  endTime: FormValues['endTime']
+  startTime: FormValues['startTime']
+  fullCourses: FormValues['coursesFull']
+  building: FormValues['building']
+  room: FormValues['room']
+}
+
+/**
+ * empty form
+ */
+const defaultFormValues: FormValues = {
+  deptValue: 'ALL',
+  deptLabel: 'ALL: Include All Departments',
+  ge: 'ANY',
+  term: getDefaultTerm().shortName,
+  courseNumber: '',
+  sectionCode: '',
+  instructor: '',
+  units: '',
+  endTime: '',
+  startTime: '',
+  coursesFull: 'ANY',
+  building: '',
+  room: '',
+}
+
+/**
+ * interface for the search store
+ */
+interface SearchStore {
+  /**
+   * current form values
+   */
+  form: FormValues
+
+  /**
+   * set a field in the form
+   */
+  setField: (field: keyof FormValues, value: string) => void
+
+  /**
+   * whether the website should render the search results
+   */
+  showResults: boolean
+
+  /**
+   * set whether the website should render the search results
+   */
+  setShowResults: (showResults: boolean) => void
+
+  /**
+   * convert the form values to appropriate websoc params
+   */
+  getParams: () => WebsocParams
+
+  /**
+   * reset only form fields specified in the array
+   */
+  resetFields: (fields: (keyof FormValues)[]) => void
+
+  /**
+   * reset the entire form
+   */
+  reset: () => void
+}
+
+/**
+ * hook to access the shared search store
+ */
+export const useSearchStore = create<SearchStore>((set, get) => ({
+  form: structuredClone(defaultFormValues),
+
+  showResults: false,
+
+  setShowResults(showResults) {
+    set({ showResults })
+  },
+
+  setField(field, value) {
+    const currentState = get()
+    const currentFormValues = currentState.form
+    set({ form: { ...currentFormValues, [field]: value } })
+  },
+
+  reset() {
+    set({ form: structuredClone(defaultFormValues) })
+  },
+
+  resetFields(fields) {
+    const currentState = get()
+    const { form } = currentState
+    fields.forEach((field) => {
+      form[field] = defaultFormValues[field]
+    })
+    set({ form: form })
+  },
+
+  getParams() {
+    const currentState = get()
+    const { form } = currentState
+    const params = {
+      department: form.deptValue,
+      term: form.term,
+      ge: form.ge,
+      courseNumber: form.courseNumber,
+      sectionCode: form.sectionCode,
+      instructorName: form.instructor,
+      units: form.units,
+      endTime: form.endTime,
+      startTime: form.startTime,
+      fullCourses: form.coursesFull,
+      building: form.building,
+      room: form.room,
+    }
+    return params
+  },
+}))
