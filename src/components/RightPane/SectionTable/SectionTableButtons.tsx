24cef228124cd33500bdf3377d85aad0d85d5785
Eric Pedley <ericpedley@gmail.com>
2023-02-23 15:59:38 -0800

    add temp variable for clarity

--- src/components/RightPane/SectionTable/SectionTableButtons.tsx
+++ src/components/RightPane/SectionTable/SectionTableButtons.tsx
@@ -85,7 +85,8 @@ export const ScheduleAddCell = withStyles(styles)((props: ScheduleAddCellProps) 
                     action: analyticsEnum.classSearch.actions.ADD_SPECIFIC,
                 });
             }
-            section.color = addCourse(section, courseDetails, term, scheduleIndex).section.color;
+            const newCourse = addCourse(section, courseDetails, term, scheduleIndex);
+            section.color = newCourse.section.color;
         }
     };
 
