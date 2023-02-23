31b3e72e2f1e0ab98c89c7e7b677d4c219736203
Frederick Dong <53128285+Voark@users.noreply.github.com>
2023-02-22 21:45:59 -0800

    Fix staging (#504)
    * Fix staging API url
    
    * Add package lock and vite config to eslint ignore
    
    Co-authored-by: senti <brianmv@uci.edu>
    
    * Restore accidentally deleted file
    
    * Clean up vite-env
    
    ---------
    
    Co-authored-by: senti <brianmv@uci.edu>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    outDir: 'build'
-  }
-})
+    plugins: [react(), svgr()],
+    build: {
+        outDir: 'build',
+    },
+    server: {
+        host: 'localhost',
+    },
+});
