f68c021ed2d3d733a21fef135607a8a1dc0a2e94
senti <brianmv@uci.edu>
2023-02-23 11:36:52 -0800

    updated stores with new abilities

--- src/stores/schedule/calendar.ts
+++ src/stores/schedule/calendar.ts
@@ -1,17 +1,18 @@
 /**
  * helpers to convert courses or events to calendar events
  *
- * "satisfies" is used to ensure that the internal CalendarEvent types 
- * we define are compatible with the types that FullCalendar expects, 
- * but without unsafe type assertions of generalizing of the type itself
+ * "satisfies" is used to ensure that the internal CalendarEvent types
+ * we define are compatible with the types that FullCalendar expects,
+ * but without unsafe type assertions or coerced generalizing of the type itself
  * @see {@link https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html#the-satisfies-operator}
  */
 
-import type { Course, RepeatingCustomEvent } from '.'
-import type { Event } from 'react-big-calendar'
+import type { Event } from 'react-big-calendar';
+
+import type { Course, RepeatingCustomEvent } from '.';
 
 /**
- * common properties for the internal calendar types 
+ * common properties for the internal calendar types
  */
 interface CommonCalendarEvent {
     color: string;
@@ -50,142 +51,145 @@ export type CalendarEvent = CourseCalendarEvent | CustomCalendarEvent;
 /**
  * type guard that asserts the returned value isn't null or undefined
  * @see {@link https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-guards}
- * @see {@link https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates}
  */
 function notNull<T>(x: T): x is NonNullable<T> {
-  return x != null
-}
-
-/**
- * regex parse a meeting time string 
- * @returns the parsed + transformed info
- */
-function parseMeetingTime(meetingTime: string) {
-  const [, date, , , startHrStr, startMinStr, endHrStr, endMinStr, ampm] = meetingTime.match(
-    /([A-za-z]+) ([A-Za-z]+) *(\d{1,2}) *(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(am|pm)/
-  ) as RegExpMatchArray
-
-  let startHr = parseInt(startHrStr, 10)
-  const startMin = parseInt(startMinStr, 10)
-  let endHr = parseInt(endHrStr, 10)
-  const endMin = parseInt(endMinStr, 10)
-
-  if (ampm === 'p' && endHr !== 12) {
-    startHr += 12
-    endHr += 12
-    if (startHr > endHr) startHr -= 12
-  }
-
-  return { date, startHr, startMin, endHr, endMin }
+    return x != null;
 }
 
 /**
  * converts courses to calendar events
  */
-export function getCourseCalendarEvents(courses: Course[]): CourseCalendarEvent[] {
-  const calendarEventsForAllCourses = courses.map((course) => {
-    const calendarEventsForCourse = course.section.meetings
-      .map((meeting) => ({ ...meeting, time: meeting.time.replace(/\s/g, '') }))
-      .filter((meeting) => meeting.time !== 'TBA')
-      .map((meeting) => {
-        const { startHr, startMin, endHr, endMin } = parseMeetingTime(meeting.time)
-        const calendarEventsMeeting = ['Su', 'M', 'Tu', 'W', 'Th', 'F', 'Sa'].map((day, index) => {
-          if (meeting.days.includes(day)) {
-            const newCalendarEvent = {
-              color: course.section.color,
-              term: course.term,
-              title: `${course.deptCode} ${course.courseNumber}`,
-              courseTitle: course.courseTitle,
-              bldg: meeting.bldg,
-              instructors: course.section.instructors,
-              sectionCode: course.section.sectionCode,
-              sectionType: course.section.sectionType,
-              start: new Date(2018, 0, index, startHr, startMin),
-              finalExam: course.section.finalExam,
-              end: new Date(2018, 0, index, endHr, endMin),
-              isCustomEvent: false as const,
-            }
-            return newCalendarEvent
-          }
-        })
+export function getCourseCalendarEvents(courses: Course[] = []): CourseCalendarEvent[] {
+    const calendarEventsForAllCourses = courses.map((course) => {
+        const calendarEventsForCourse = course.section.meetings
+            .map((meeting) => ({ ...meeting, time: meeting.time.replace(/\s/g, '') }))
+            .filter((meeting) => meeting.time !== 'TBA')
+            .map((meeting) => {
+                const [, startHrStr, startMinStr, endHrStr, endMinStr, ampm] = meeting.time.match(
+                    /(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(p?)/
+                ) as RegExpMatchArray;
 
-        const definedCalendarEventsMeetings = calendarEventsMeeting.filter(notNull)
-        return definedCalendarEventsMeetings
-      })
+                let startHr = parseInt(startHrStr, 10);
+                const startMin = parseInt(startMinStr, 10);
+                let endHr = parseInt(endHrStr, 10);
+                const endMin = parseInt(endMinStr, 10);
 
-    const flatCalendarEventsForCourse = calendarEventsForCourse.flat()
-    return flatCalendarEventsForCourse
-  })
+                if (ampm === 'p' && endHr !== 12) {
+                    startHr += 12;
+                    endHr += 12;
+                    if (startHr > endHr) startHr -= 12;
+                }
 
-  const flatCalendarEventsForAllCourses = calendarEventsForAllCourses.flat()
-  return flatCalendarEventsForAllCourses satisfies Event[]
+                const calendarEventsMeeting = ['Su', 'M', 'Tu', 'W', 'Th', 'F', 'Sa'].map((day, index) => {
+                    if (meeting.days.includes(day)) {
+                        const newCalendarEvent = {
+                            color: course.section.color,
+                            term: course.term,
+                            title: `${course.deptCode} ${course.courseNumber}`,
+                            courseTitle: course.courseTitle,
+                            bldg: meeting.bldg,
+                            instructors: course.section.instructors,
+                            sectionCode: course.section.sectionCode,
+                            sectionType: course.section.sectionType,
+                            start: new Date(2018, 0, index, startHr, startMin),
+                            finalExam: course.section.finalExam,
+                            end: new Date(2018, 0, index, endHr, endMin),
+                            isCustomEvent: false as const,
+                        };
+                        return newCalendarEvent;
+                    }
+                });
+
+                const definedCalendarEventsMeetings = calendarEventsMeeting.filter(notNull);
+                return definedCalendarEventsMeetings;
+            });
+
+        const flatCalendarEventsForCourse = calendarEventsForCourse.flat();
+        return flatCalendarEventsForCourse;
+    });
+
+    const flatCalendarEventsForAllCourses = calendarEventsForAllCourses.flat();
+    return flatCalendarEventsForAllCourses satisfies Event[];
 }
 
 /**
  * converts course finals to calendar events
  */
-export function getFinalsCalendarEvents(courses: Course[]): CourseCalendarEvent[] {
-  const finalsForAllCourses = courses
-    .filter((course) => course.section.finalExam.length > 5)
-    .map((course) => {
-      const { date, startHr, startMin, endHr, endMin } = parseMeetingTime(course.section.finalExam)
-      const finalsForCourse = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'].map((day, index) => {
-        if (date.includes(day)) {
-          const newCalendarEvent = {
-            title: `${course.deptCode} ${course.courseNumber}`,
-            sectionCode: course.section.sectionCode,
-            sectionType: 'Fin',
-            bldg: course.section.meetings[0].bldg,
-            color: course.section.color,
-            start: new Date(2018, 0, index - 1, startHr, startMin),
-            end: new Date(2018, 0, index - 1, endHr, endMin),
-            finalExam: course.section.finalExam,
-            instructors: course.section.instructors,
-            term: course.term,
-            isCustomEvent: false as const,
-            customEventID: 0,
-          }
-          return newCalendarEvent
-        }
-      })
+export function getFinalsCalendarEvents(courses: Course[] = []): CourseCalendarEvent[] {
+    const finalsForAllCourses = courses
+        .filter((course) => course.section.finalExam.length > 5)
+        .map((course) => {
+            const [, date, , , startStr, startMinStr, endStr, endMinStr, ampm] = course.section.finalExam.match(
+                /([A-za-z]+) ([A-Za-z]+) *(\d{1,2}) *(\d{1,2}):(\d{2})-(\d{1,2}):(\d{2})(am|pm)/
+            ) as RegExpMatchArray;
 
-      const definedFinalsForCourse = finalsForCourse.filter(notNull)
-      return definedFinalsForCourse
-    })
+            let startHour = parseInt(startStr, 10);
+            const startMin = parseInt(startMinStr, 10);
+            let endHour = parseInt(endStr, 10);
+            const endMin = parseInt(endMinStr, 10);
 
-  const flatFinalsForAllCourses = finalsForAllCourses.flat()
-  return flatFinalsForAllCourses satisfies Event[]
+            if (ampm === 'pm' && endHour !== 12) {
+                startHour += 12;
+                endHour += 12;
+                if (startHour > endHour) startHour -= 12;
+            }
+
+            const finalsForCourse = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'].map((day, index) => {
+                if (date.includes(day)) {
+                    const newCalendarEvent = {
+                        title: `${course.deptCode} ${course.courseNumber}`,
+                        sectionCode: course.section.sectionCode,
+                        sectionType: 'Fin',
+                        bldg: course.section.meetings[0].bldg,
+                        color: course.section.color,
+                        start: new Date(2018, 0, index - 1, startHour, startMin),
+                        end: new Date(2018, 0, index - 1, endHour, endMin),
+                        finalExam: course.section.finalExam,
+                        instructors: course.section.instructors,
+                        term: course.term,
+                        isCustomEvent: false as const,
+                        customEventID: 0,
+                    };
+                    return newCalendarEvent;
+                }
+            });
+
+            const definedFinalsForCourse = finalsForCourse.filter(notNull);
+            return definedFinalsForCourse;
+        });
+
+    const flatFinalsForAllCourses = finalsForAllCourses.flat();
+    return flatFinalsForAllCourses satisfies Event[];
 }
 
 /**
  * converts custom events to calendar events
  */
-export function getCustomCalendarEvents(customEvents: RepeatingCustomEvent[]): CustomCalendarEvent[] {
-  const allCustomCalendarEvents = customEvents.map((customEvent) => {
-    const calendarEventsSingleCustom = customEvent.days
-      .map((day, dayIndex) => {
-        if (day) {
-          const startHour = parseInt(customEvent.start.slice(0, 2), 10)
-          const startMin = parseInt(customEvent.start.slice(3, 5), 10)
-          const endHour = parseInt(customEvent.end.slice(0, 2), 10)
-          const endMin = parseInt(customEvent.end.slice(3, 5), 10)
+export function getCustomCalendarEvents(customEvents: RepeatingCustomEvent[] = []): CustomCalendarEvent[] {
+    const allCustomCalendarEvents = customEvents.map((customEvent) => {
+        const calendarEventsSingleCustom = customEvent.days.map((day, dayIndex) => {
+            if (day) {
+                const startHour = parseInt(customEvent.start.slice(0, 2), 10);
+                const startMin = parseInt(customEvent.start.slice(3, 5), 10);
+                const endHour = parseInt(customEvent.end.slice(0, 2), 10);
+                const endMin = parseInt(customEvent.end.slice(3, 5), 10);
 
-          const newCalendarEvent = {
-            customEventID: customEvent.customEventID,
-            color: customEvent.color || '',
-            start: new Date(2018, 0, dayIndex, startHour, startMin),
-            isCustomEvent: true as const,
-            end: new Date(2018, 0, dayIndex, endHour, endMin),
-            title: customEvent.title,
-          }
-          return newCalendarEvent
-        }
-      })
+                const newCalendarEvent = {
+                    customEventID: customEvent.customEventID,
+                    color: customEvent.color || '',
+                    start: new Date(2018, 0, dayIndex, startHour, startMin),
+                    isCustomEvent: true as const,
+                    end: new Date(2018, 0, dayIndex, endHour, endMin),
+                    title: customEvent.title,
+                };
+                return newCalendarEvent;
+            }
+        });
 
-    const definedCalendarEventsSingleCustom = calendarEventsSingleCustom.filter(notNull)
-    return definedCalendarEventsSingleCustom
-  })
+        const definedCalendarEventsSingleCustom = calendarEventsSingleCustom.filter(notNull);
+        return definedCalendarEventsSingleCustom;
+    });
 
-  const flatCustomCalendarEvents = allCustomCalendarEvents.flat()
-  return flatCustomCalendarEvents satisfies Event[]
+    const flatCustomCalendarEvents = allCustomCalendarEvents.flat();
+    return flatCustomCalendarEvents satisfies Event[];
 }
--- src/stores/schedule/index.ts
+++ src/stores/schedule/index.ts
@@ -1,104 +1,111 @@
 /**
  * basic schedule store with primitive accessors
  *
- * all other files in this folder are de-coupled from one another, 
+ * all other files in this folder are de-coupled from one another,
  * and only extend this store's functionality
  *
  * move common/shared interfaces here
  */
 
-import { create } from 'zustand'
-import type { AASection, Section } from '$lib/peterportal.types'
+import { create } from 'zustand';
+
+import type { AASection, Section } from '$lib/peterportal.types';
 
 export interface RepeatingCustomEvent {
-  title: string
-  start: string
-  end: string
-  days: boolean[]
-  customEventID: number
-  color?: string
+    title: string;
+    start: string;
+    end: string;
+    days: boolean[];
+    customEventID: number;
+    color?: string;
 }
 
 export interface CourseDetails {
-  courseComment: string
-  courseNumber: string
-  courseTitle: string
-  deptCode: string
-  prerequisiteLink: string
+    courseComment: string;
+    courseNumber: string;
+    courseTitle: string;
+    deptCode: string;
+    prerequisiteLink: string;
 }
 
 export interface CourseInfo {
-  courseDetails: CourseDetails
-  section: Section
+    courseDetails: CourseDetails;
+    section: Section;
 }
 
 export interface Course extends CourseDetails {
-  section: AASection
-  term: string
+    section: AASection;
+    term: string;
 }
 
 export interface Schedule {
-  scheduleName: string
-  courses: Course[]
-  customEvents: RepeatingCustomEvent[]
+    scheduleName: string;
+    courses: Course[];
+    customEvents: RepeatingCustomEvent[];
 }
 
 export interface ShortCourse {
-  color: string
-  term: string
-  sectionCode: string
+    color: string;
+    term: string;
+    sectionCode: string;
 }
 
 export interface ShortCourseSchedule {
-  scheduleName: string
-  courses: ShortCourse[]
-  customEvents: RepeatingCustomEvent[]
+    scheduleName: string;
+    courses: ShortCourse[];
+    customEvents: RepeatingCustomEvent[];
 }
 
 /**
  * saved schedule in the database (similar to the undo state, but with less data)
  */
 export interface ScheduleSaveState {
-  schedules: ShortCourseSchedule[]
-  scheduleIndex: number
+    schedules: ShortCourseSchedule[];
+    scheduleIndex: number;
 }
 
 /**
  * saved schedule in memory
  */
 export interface ScheduleUndoState {
-  schedules: Schedule[]
-  scheduleIndex: number
+    schedules: Schedule[];
+    scheduleIndex: number;
 }
 
 export interface ScheduleStore {
-  /**
-   * all schedules
-   */
-  schedules: Schedule[]
+    /**
+     * all schedules
+     */
+    schedules: Schedule[];
 
-  /**
-   * index of currently selected schedule
-   */
-  scheduleIndex: number
+    /**
+     * index of currently selected schedule
+     */
+    scheduleIndex: number;
 
-  /**
-   * undo tree
-   */
-  previousStates: ScheduleUndoState[]
+    /**
+     * undo tree
+     */
+    previousStates: ScheduleUndoState[];
 
-  /**
-   * whether the schedule's been saved
-   */
-  saved: boolean
+    /**
+     * redo tree
+     */
+    nextStates: ScheduleUndoState[];
+
+    /**
+     * whether the schedule's been saved
+     */
+    saved: boolean;
 }
 
 /**
  * hook to access the store (shared state) with info about the current schedules
  */
-export const useScheduleStore = create<ScheduleStore>((_set, _get) => ({
-  schedules: [{ scheduleName: 'Schedule 1', courses: [], customEvents: [] }],
-  scheduleIndex: 0,
-  previousStates: [],
-  saved: false,
-}))
+export const useScheduleStore = create<ScheduleStore>(() => ({
+    schedules: [{ scheduleName: 'Schedule 1', courses: [], customEvents: [] }],
+    scheduleIndex: 0,
+    previousStates: [],
+    nextStates: [],
+    saved: true,
+}));
