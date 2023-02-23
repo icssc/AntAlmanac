253f95db7dcdd02c1cd88658082e9789719eea8e
senti <brianmv@uci.edu>
2023-02-23 09:28:16 -0800

    added the ellipsis
    - the strat is very dumb, so I wrote down a lot of explanations

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
-import Split from 'react-split';
 import DateFnsUtils from '@date-io/date-fns';
 import { Box, CssBaseline, useMediaQuery, useTheme } from '@material-ui/core';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { MoreVert as MoreVertIcon } from '@mui/icons-material';
+import { useEffect, useRef } from 'react';
+import Split from 'react-split';
+
 import Bar from './AppBar/CustomAppBar';
 import NotificationSnackbar from './AppBar/NotificationSnackbar';
 import Calendar from './Calendar/CalendarRoot';
@@ -11,6 +14,21 @@ import DesktopTabs from './RightPane/RightPaneRoot';
 const Home = () => {
     const isMobileScreen = useMediaQuery('(max-width: 750px)');
     const theme = useTheme();
+
+    /**
+     * ref to the DOM node that has the ellipsis MUI icon
+     */
+    const ellipsisRef = useRef<SVGSVGElement>(null);
+
+    useEffect(() => {
+        /**
+         * the gutter is the middle bar used to resize the split pane
+         * grab the ellipsis DOM node and relocate it under the gutter
+         */
+        if (ellipsisRef.current) {
+            document.querySelector('.gutter')?.appendChild(ellipsisRef.current);
+        }
+    }, []);
 
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -44,6 +62,9 @@ const Home = () => {
                 </Split>
             )}
             <NotificationSnackbar />
+
+            {/* render an ellipsis icon and manually teleport it to the gutter */}
+            <MoreVertIcon ref={ellipsisRef} sx={{ color: 'white' }} />
         </MuiPickersUtilsProvider>
     );
 };
