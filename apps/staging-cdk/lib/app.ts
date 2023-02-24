c93e887dc07e43729c8d1b12f7b981c74330893a
Frederick Dong <fredisawesome147@gmail.com>
2023-02-24 02:24:32 -0800

    Minor action fixes

--- apps/staging-cdk/lib/app.ts
+++ apps/staging-cdk/lib/app.ts
@@ -6,15 +6,13 @@ import * as process from "process";
 
 const app = new App({ autoSynth: true });
 
-if (!process.env.PR_NUM || !process.env.CERTIFICATE_ARN || !process.env.HOSTED_ZONE_ID || !process.env.ACCOUNT_ID){
+if (!process.env.PR_NUM || !process.env.CERTIFICATE_ARN || !process.env.HOSTED_ZONE_ID ){
     throw new Error("Environmental variables weren't provided and/or this file shouldn't have been run");
 }
 
-const account = process.env['ACCOUNT_ID'];
-
-new ActionsStack(app, `github-actions-stack-${process.env.PR_NUM}`, {
+new ActionsStack(app, `antalmanac-staging-stack-${process.env.PR_NUM}`, {
     pr_num: process.env.PR_NUM,
     certificateArn: process.env.CERTIFICATE_ARN,
     hostedZoneId: process.env.HOSTED_ZONE_ID,
-    env: { region: 'us-east-1', account: account },
+    env: { region: 'us-east-1' },
 })
