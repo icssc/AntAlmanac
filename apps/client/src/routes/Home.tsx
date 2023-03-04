8dc6a15fbf4879e1cdc8169fa2a4b4dd57644d0e
senti <brianmv@uci.edu>
2023-03-04 14:38:34 -0800

    updated home

--- apps/client/src/routes/Home.tsx
+++ apps/client/src/routes/Home.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material'
 import trpc from '$lib/trpc'
 export default function Home() {
   const query = trpc.schedule.find.useQuery('rem')
+  trpc.user.findAll.useQuery()
   return (
     <Box sx={{ whiteSpace: 'pre' }}>
       {JSON.stringify(query.data, null, 2)}
--- apps/server/src/routes/user.ts
+++ apps/server/src/routes/user.ts
@@ -2,10 +2,14 @@ import { procedure, router } from '../trpc'
 import UserModel from '$models/User'
 
 const userRouter = router({
-  find: procedure.query(async () => {
-    const users = UserModel.scan().exec()
+  findAll: procedure.query(async () => {
+    const users = await UserModel.scan().exec()
     return users
-  })
+  }),
+  create: procedure.mutation(async () => {
+    const users = await UserModel.get('1')
+    return users
+  }),
 })
 
 export default userRouter
