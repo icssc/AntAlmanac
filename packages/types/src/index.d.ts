1bcc334e10c554a1f53898e7658571fa8df3fade
senti <brianmv@uci.edu>
2023-03-05 18:58:41 -0800

    cleaned up typescript

--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -1,9 +1,13 @@
-import type { WebsocCourse, WebsocSection } from 'peterportal-api-next-types'
+import type { WebsocSection, WebsocAPIResponse } from 'peterportal-api-next-types'
+
+//-----------------------------------------------------------------------------------
+// main data entities
+//-----------------------------------------------------------------------------------
 
 /**
  * modified section from peterportal websoc
  */
-export interface AASection extends WebsocSection {
+export interface Section extends WebsocSection {
   /**
    * color for the calendar
    */
@@ -11,25 +15,7 @@ export interface AASection extends WebsocSection {
 }
 
 /**
- * transformed course from peterportal websoc
- * describes a course with sections that can be individually added to a schedule
- */
-export interface AACourse extends WebsocCourse {
-  /**
-   * a course can have multiple sections that each can be added to the schedcule
-   */
-  sections: AASection[]
-
-  /**
-   * a course is associated with a department
-   */
-  deptCode: string
-}
-
-/**
- * modified course from peterportal
- * describes a single section associated with a course in a schedule
- * @remarks essentially a section that's been appended with course info
+ * modified section from the AACourse
  */
 export interface Course extends WebsocCourse {
   /**
@@ -40,12 +26,8 @@ export interface Course extends WebsocCourse {
   /**
    * a course in the schedule is associated with a single section after it's added
    */
-  section: AASection
+  section: Section
 }
-
-//-----------------------------------------------------------------------------------
-// main data entities
-//-----------------------------------------------------------------------------------
 
 /**
  * custom calendar event, similar to react-big-calendar's events
