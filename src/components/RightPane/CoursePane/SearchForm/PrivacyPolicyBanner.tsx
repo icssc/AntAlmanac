009f53a73101c48d71fd0636ce0a0e1870012a10
Minh Nguyen <minhnx.vn@gmail.com>
2023-03-21 20:08:47 -0700

    Apply suggestions from review

--- src/components/RightPane/CoursePane/SearchForm/PrivacyPolicyBanner.tsx
+++ src/components/RightPane/CoursePane/SearchForm/PrivacyPolicyBanner.tsx
@@ -21,9 +21,7 @@ class PrivacyPolicyBanner extends PureComponent<PrivacyPolicyBannerProps> {
                 <Typography variant="body2">
                     We use cookies to analyze website traffic and track usage, with the aim of improving your experience
                     on AntAlmanac. By continuing to use this website, consent to our{' '}
-                    <a href={'https://github.com/icssc/AntAlmanac/blob/privacy-policy/PRIVACY-POLICY.md'}>
-                        privacy policy
-                    </a>
+                    <a href={'https://github.com/icssc/AntAlmanac/blob/main/PRIVACY-POLICY.md'}>privacy policy</a>
                 </Typography>
             </Paper>
         );
