c0be4653caddbf7ee63efb8fabaf4b6124f9a738
senti <brianmv@uci.edu>
2023-05-06 00:58:32 -0700

    obliterated the map

--- apps/antalmanac/src/components/Home.tsx
+++ apps/antalmanac/src/components/Home.tsx
@@ -1,8 +1,6 @@
 import DateFnsUtils from '@date-io/date-fns';
 import { Box, CssBaseline, useMediaQuery, useTheme } from '@material-ui/core';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { MoreVert as MoreVertIcon } from '@mui/icons-material';
-import { useEffect, useRef } from 'react';
 import Split from 'react-split';
 
 import Bar from './AppBar/CustomAppBar';
@@ -15,21 +13,6 @@ import DesktopTabs from './RightPane/RightPaneRoot';
 const Home = () => {
     const isMobileScreen = useMediaQuery('(max-width: 750px)');
     const theme = useTheme();
-
-    /**
-     * ref to the DOM node that has the ellipsis MUI icon
-     */
-    const ellipsisRef = useRef<SVGSVGElement>(null);
-
-    useEffect(() => {
-        /**
-         * the gutter is the middle bar used to resize the split pane
-         * grab the ellipsis DOM node and relocate it under the gutter
-         */
-        if (!isMobileScreen && ellipsisRef.current) {
-            document.querySelector('.gutter')?.appendChild(ellipsisRef.current);
-        }
-    }, [isMobileScreen]);
 
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -61,9 +44,6 @@ const Home = () => {
 
                     <Box>
                         <DesktopTabs style={{ height: 'calc(100vh - 58px)' }} />
-
-                        {/* render an ellipsis icon and manually teleport it to the gutter */}
-                        <MoreVertIcon ref={ellipsisRef} sx={{ color: 'white' }} />
                     </Box>
                 </Split>
             )}
