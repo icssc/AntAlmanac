23df616f773cfdebed4807a937762b3f262dd0b5
senti <brianmv@uci.edu>
2023-05-06 01:07:03 -0700

    added everything except the map

---
+++ apps/antalmanac/src/components/Map/UserLocator.tsx
@@ -0,0 +1,43 @@
+import L from 'leaflet'
+import 'leaflet.locatecontrol'
+import { useEffect } from 'react'
+import { createElementHook, createElementObject, useLeafletContext } from '@react-leaflet/core'
+import type { LeafletContextInterface } from '@react-leaflet/core'
+
+function createUserLocator(_props: any, context: LeafletContextInterface) {
+  const userLocator = createElementObject(
+    L.control.locate({
+      position: 'topleft',
+      flyTo: true,
+      strings: {
+        title: 'Look for your lost soul',
+      },
+    }),
+    context
+  )
+
+  return userLocator
+}
+
+/**
+ * Use react-leaflet's core API to manage lifecycle of leaflet elements properly.
+ * @see {@link https://react-leaflet.js.org/docs/core-architecture/#element-hook-factory}
+ */
+const useUserLocator = createElementHook(createUserLocator)
+
+/**
+ * Initializes a leaflet locator to locate the user.
+ */
+export default function UserLocator() {
+  const context = useLeafletContext()
+  const elementRef = useUserLocator(null, context)
+
+  useEffect(() => {
+    elementRef.current.instance.addTo(context.map)
+    return () => {
+      elementRef.current.instance.remove()
+    }
+  }, [])
+
+  return null
+}
