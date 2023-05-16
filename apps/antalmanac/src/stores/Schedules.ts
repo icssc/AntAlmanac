64680d4ee07fc6e2b1a9c463f3bd5b842d455f93
Frederick Dong <53128285+Voark@users.noreply.github.com>
2023-05-15 18:22:49 -0700

    Fix adding classes that already exist in other schedules (#573)

--- apps/antalmanac/src/stores/Schedules.ts
+++ apps/antalmanac/src/stores/Schedules.ts
@@ -346,6 +346,7 @@ export class Schedules {
 
         const existingSection = this.getExistingCourse(newCourse.section.sectionCode, newCourse.term);
         if (existingSection) {
+            this.schedules[scheduleIndex].courses.push(existingSection);
             return existingSection;
         }
 
