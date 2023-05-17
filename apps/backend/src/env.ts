73a76e2a85d29c58034a5228fb0e83872e04c46a
Frederick Dong <fredisawesome147@gmail.com>
2023-05-17 12:54:44 -0700

    Attempt to fix environmental variable issues

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -6,6 +6,7 @@ const Environment = type([
         'STAGE?': "'dev' | 'prod' | 'staging'",
         USERDATA_TABLE_NAME: 'string',
         AA_MONGODB_URI: 'string',
+        'PR_NUM?': 'number',
     },
     '|>',
     (s) => ({ STAGE: 'dev', ...s }),
