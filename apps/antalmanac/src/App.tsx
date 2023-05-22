03508026cf7b8c225015dcc120e6439add199d30
senti <brianmv@uci.edu>
2023-05-22 12:50:14 -0700

    bump app and map

--- apps/antalmanac/src/App.tsx
+++ apps/antalmanac/src/App.tsx
@@ -10,43 +10,43 @@ import AppQueryProvider from './providers/Query';
 import AppThemeProvider from './providers/Theme';
 
 import Home from './routes/Home';
-import Feedback from './routes/Feedback'
+import Feedback from './routes/Feedback';
 
 const BrowserRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/:tab',
-    element: <Home />
-  },
-  {
-    path: '/feedback',
-    element: <Feedback />
-  }
-])
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/:tab',
+        element: <Home />,
+    },
+    {
+        path: '/feedback',
+        element: <Feedback />,
+    },
+]);
 
 /**
- * renders the single page application
+ * Renders the single page application.
  */
 export default function App() {
-  useEffect(() => {
-    document.addEventListener('keydown', undoDelete, false);
-    ReactGA4.initialize('G-30HVJXC2Y4');
-    ReactGA4.send('pageview');
-    return () => {
-      document.removeEventListener('keydown', undoDelete, false);
-    };
-  }, []);
+    useEffect(() => {
+        document.addEventListener('keydown', undoDelete, false);
+        ReactGA4.initialize('G-30HVJXC2Y4');
+        ReactGA4.send('pageview');
+        return () => {
+            document.removeEventListener('keydown', undoDelete, false);
+        };
+    }, []);
 
-  return (
-    <AppQueryProvider>
-      <AppThemeProvider>
-        <SnackbarProvider>
-          <RouterProvider router={BrowserRouter} />
-        </SnackbarProvider>
-      </AppThemeProvider>
-    </AppQueryProvider>
-  );
+    return (
+        <AppQueryProvider>
+            <AppThemeProvider>
+                <SnackbarProvider>
+                    <RouterProvider router={BrowserRouter} />
+                </SnackbarProvider>
+            </AppThemeProvider>
+        </AppQueryProvider>
+    );
 }
