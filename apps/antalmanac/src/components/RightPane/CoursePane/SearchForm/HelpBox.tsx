061abd35d1ae2a2b60165759c66c5defc22924ae
Kevin Wu <kevinwu098@gmail.com>
2023-06-01 01:54:43 -0700

    Rewrote to MUI, fixed bug

--- apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/HelpBox.tsx
+++ apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/HelpBox.tsx
@@ -1,5 +1,6 @@
 import { Paper, withStyles } from '@material-ui/core';
 import { ClassNameMap } from '@material-ui/core/styles/withStyles';
+import { ImageList, ImageListItem, Typography } from '@mui/material';
 
 const styles = {
     container: {
@@ -7,21 +8,8 @@ const styles = {
         marginBottom: '10px',
         marginRight: '5px',
     },
-    heading: {
-        marginTop: 2,
-    },
-    list: {
-        paddingLeft: '1.5em',
-    },
-    images: {
-        display: 'flex',
-        gap: 10,
-        overflow: 'auto',
-    },
-    image: {
-        height: 250,
-    },
 };
+
 interface HelpBoxProps {
     classes: ClassNameMap;
 }
@@ -29,34 +17,44 @@ interface HelpBoxProps {
 const HelpBox = ({ classes }: HelpBoxProps) => {
     return (
         <Paper variant="outlined" className={classes.container}>
-            <h2 className={classes.heading}>Need help planning your schedule?</h2>
-            <ol className={classes.list}>
-                <li>
-                    Browse undergraduate majors on the{' '}
-                    <a href="https://catalogue.uci.edu/undergraduatedegrees/" target="_blank" rel="noopener noreferrer">
-                        UCI Catalogue
-                    </a>
-                    .
-                </li>
-                <li>Select your major.</li>
-                <li>
-                    View the &quot;REQUIREMENTS&quot; and &quot;SAMPLE PROGRAM&quot; tabs to see what classes you should
-                    take.
-                </li>
-            </ol>
-            <div className={classes.images}>
-                <img
-                    className={classes.image}
-                    src="/helpbox1.png"
-                    alt='UCI General Catalogue with "Explore Undergraduate Programs" button highlighted'
-                />
-                <img className={classes.image} src="/helpbox2.png" alt="Undergraduate Majors and Minors page" />
-                <img
-                    className={classes.image}
-                    src="/helpbox3.png"
-                    alt='Electrical Engineering page with "REQUIREMENTS" and "SAMPLE PROGRAM" tabs highlighted'
-                />
-            </div>
+            <Typography variant="h4">Need help planning your schedule?</Typography>
+            <Typography>
+                <ol>
+                    <li>
+                        Browse undergraduate majors on the{' '}
+                        <a
+                            href="https://catalogue.uci.edu/undergraduatedegrees/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            UCI Catalogue
+                        </a>
+                        .
+                    </li>
+                    <li>Select your major.</li>
+                    <li>
+                        View the &quot;REQUIREMENTS&quot; and &quot;SAMPLE PROGRAM&quot; tabs to see what classes you
+                        should take.
+                    </li>
+                </ol>
+            </Typography>
+            <ImageList cols={3}>
+                <ImageListItem>
+                    <img
+                        src="/helpbox1.png"
+                        alt='UCI General Catalogue with "Explore Undergraduate Programs" button highlighted'
+                    />
+                </ImageListItem>
+                <ImageListItem>
+                    <img src="/helpbox2.png" alt="Undergraduate Majors and Minors page" />
+                </ImageListItem>
+                <ImageListItem>
+                    <img
+                        src="/helpbox3.png"
+                        alt='Electrical Engineering page with "REQUIREMENTS" and "SAMPLE PROGRAM" tabs highlighted'
+                    />
+                </ImageListItem>
+            </ImageList>
         </Paper>
     );
 };
