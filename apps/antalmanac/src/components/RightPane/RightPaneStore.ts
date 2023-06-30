449fc6154ac6e4e061486ad4accb32ce36869d7c
Kevin Wu <kevinwu098@gmail.com>
2023-06-30 00:52:19 -0700

    Cleaned up RightPaneStore as part of resolving merge conflicts

--- apps/antalmanac/src/components/RightPane/RightPaneStore.ts
+++ apps/antalmanac/src/components/RightPane/RightPaneStore.ts
@@ -1,6 +1,5 @@
 import { EventEmitter } from 'events';
 
-import { FAKE_LOCATIONS } from '$lib/helpers';
 import { getDefaultTerm } from '$lib/termData';
 
 const defaultFormValues: Record<string, string> = {
@@ -96,27 +95,6 @@ class RightPaneStore extends EventEmitter {
 
     toggleOpenSpotAlert = () => {
         this.openSpotAlertPopoverActive = !this.openSpotAlertPopoverActive;
-    };
-
-    focusOnBuilding = (buildingFocusInfo: BuildingFocusInfo) => {
-        // Filter out fake locations
-        if (!FAKE_LOCATIONS.includes(buildingFocusInfo.location)) this.emit('focusOnBuilding', buildingFocusInfo);
-        /** Explanation of what happens when 'focusOnBuilding' is emitted:
-         *
-         *  If desktop:
-         *  1) RightPaneRoot recieves 'focusOnBuilding'.
-         *  2a) If the Map tab is selected already, it passes the args down to UCIMap with 'selectBuilding'.
-         *  2b) If the Map tab is not selected, it switches to the Map tab, waits for it to load, and does 2a.
-         *  3) UCIMap recieves 'selectBuilding' and focuses on that building.
-         *
-         *  If mobile (MobileHome.js is being displayed):
-         *  1a) If the "SEARCH" tab is selected, (and, therefore, RighPaneRoot is loaded), it can listen to 'focusOnBuilding' itself.
-         *  1b) If the "SEARCH" tab is not selected (and, therefore, RighPaneRoot is unloaded), switch to it,
-         *      wait for it to load and emit 'RightPaneRootLoaded', and re-emit 'focusOnBuilding'
-         *
-         *  The choice was between prop-drilling from Home and having cascading listeners, and
-         *  I think the latter is reasonable.
-         */
     };
 
     setActiveColumns = (columns: string[]) => {
