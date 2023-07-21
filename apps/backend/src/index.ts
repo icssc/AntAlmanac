fda960066bdf8e76d24a68ddfa067d7f4ce349dc
Frederick Dong <53128285+Voark@users.noreply.github.com>
2023-07-21 12:58:47 -0700

    Fix dev server (#642)
    * fix dev server
    
    * increase timeout
    
    * add endpoint comment
    
    * change port back to 3000

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -11,7 +11,7 @@ const corsOptions: CorsOptions = {
     origin: ['https://antalmanac.com', 'https://www.antalmanac.com', 'https://icssc-projects.github.io/AntAlmanac'],
 };
 
-const PORT = 8080;
+const PORT = 3000;
 
 export async function start(corsEnabled = false) {
     await connectToMongoDB();
@@ -28,7 +28,7 @@ export async function start(corsEnabled = false) {
         })
     );
 
-    if (env.STAGE === 'dev') {
+    if (env.STAGE === 'local') {
         app.listen(PORT, async () => {
             console.log(`🚀 Server listening at http://localhost:${PORT}`);
         });
