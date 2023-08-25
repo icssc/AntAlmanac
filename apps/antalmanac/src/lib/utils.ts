79edd5668920d50afaccad1c785dae98573dac07
Aponia <brianmv@uci.edu>
2023-08-25 08:22:59 -0700

    doc: added more documentation to reference util

--- apps/antalmanac/src/lib/utils.ts
+++ apps/antalmanac/src/lib/utils.ts
@@ -5,6 +5,25 @@ export function notNull<T>(value: T): value is NonNullable<T> {
 /**
  * Given a reference array and an input, generate an array of booleans for each position in the
  * reference array, indicating whether the value at that position is in the input.
+ *
+ * @example
+ * reference = ['a', 'b', 'c', 'd', 'e']
+ * input = 'ace'
+ * result = [true, false, true, false, true]
+ *
+ * Can be used in conjunection with {@link notNull} to get only indices.
+ *
+ * @example
+ *
+ * ```ts
+ *
+ * const reference = ['a', 'b', 'c', 'd', 'e'];
+ * const input = 'ace';
+ *
+ * const occurringReferences = getReferencesOccurring(reference, input)
+ * const indicesOrNull = occurringReferences.map((occurring, index) => occurring ? index : null)
+ * const indices = indicesOrNull.filter(notNull)
+ * ```
  */
 export function getReferencesOccurring(reference: string[], input?: string | string[] | null): boolean[] {
     return input ? reference.map((reference) => input.includes(reference)) : reference.map(() => false);
