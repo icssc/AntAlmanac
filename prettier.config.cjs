98a03a6112d71b989de50d54782ad79ac1a37f09
Aponia <brianmv@uci.edu>
2023-09-04 18:21:49 -0700

    chore: clean up cdk

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,13 @@
+// @ts-check
+
 /**
  * `cjs` format is required for plugins to work in VSCode.
  * @see {@link https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/113}
  */
 
-/** @type {import('prettier').Config} */
+/** 
+ * @type {import('prettier').Config}
+ */
 const config = {
   printWidth: 120,
   tabWidth: 4,
