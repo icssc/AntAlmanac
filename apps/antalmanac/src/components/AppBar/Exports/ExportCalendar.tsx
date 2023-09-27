9b1484028679bce86de1b8132de69dfc82a0eb01
Eric Pedley <ericpedley@gmail.com>
2023-09-26 21:44:08 -0400

    Make it clear you import a .ics file to other calendars. (#706)
    Co-authored-by: Kevin Wu <kevinwu098@gmail.com>

--- apps/antalmanac/src/components/AppBar/Exports/ExportCalendar.tsx
+++ apps/antalmanac/src/components/AppBar/Exports/ExportCalendar.tsx
@@ -4,7 +4,7 @@ import { Download } from '@mui/icons-material';
 import { exportCalendar } from '$lib/download';
 
 const ExportCalendarButton = () => (
-    <Tooltip title="Download Calendar as an .ics file">
+    <Tooltip title="Download Calendar as a .ics file. You can import this file to Google or Apple calendars.">
         <Button onClick={exportCalendar} variant="outlined" size="small" startIcon={<Download fontSize="small" />}>
             Download
         </Button>
