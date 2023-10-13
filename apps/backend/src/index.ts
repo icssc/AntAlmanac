0c03bec99b5dbd241ba3beab7b907a84974a5f1e
Eric Pedley <ericpedley@gmail.com>
2023-10-13 11:05:35 -0700

    comment-out all CORS bullshit

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -19,7 +19,7 @@ export async function start(corsEnabled = false) {
     await connectToMongoDB();
 
     const app = express();
-    app.use(cors(corsEnabled ? corsOptions : undefined));
+    // app.use(cors(corsEnabled ? corsOptions : undefined));
     app.use(express.json());
 
     app.use('/mapbox/directions/*', async (req, res) => {
@@ -35,8 +35,8 @@ export async function start(corsEnabled = false) {
         searchParams.set('access_token', env.MAPBOX_ACCESS_TOKEN);
         const url = `${MAPBOX_API_URL}/styles/v1/mapbox/streets-v11/tiles/${req.params[0]}?${searchParams.toString()}`;
         const result = await fetch(url).then((res) => res.blob());
-        res.header('Content-Security-Policy', "img-src 'self'"); // https://stackoverflow.com/questions/56386307/loading-of-a-resource-blocked-by-content-security-policy
-        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        // res.header('Content-Security-Policy', "img-src 'self'"); // https://stackoverflow.com/questions/56386307/loading-of-a-resource-blocked-by-content-security-policy
+        // res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         res.type(result.type)
         result.arrayBuffer().then((buf) => {
             res.send(Buffer.from(buf))
