52400bc671baf57cb137bbe7a02252efa5f7422d
Kevin Wu <kevinwu098@gmail.com>
2023-11-09 00:32:54 -0800

    Refactor settings menu

--- apps/antalmanac/src/components/Header/SettingsMenu.tsx
+++ apps/antalmanac/src/components/Header/SettingsMenu.tsx
@@ -1,79 +1,64 @@
 import { Button, FormControl, FormControlLabel, Paper, Popover, Radio, RadioGroup } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { ClassNameMap } from '@material-ui/core/styles/withStyles';
-import Brightness4Icon from '@material-ui/icons/Brightness4';
-import { PureComponent } from 'react';
+import { Settings } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
 
 import { toggleTheme } from '$actions/AppStoreActions';
 import AppStore from '$stores/AppStore';
 
-const styles = {
-    container: {
-        padding: '0.5rem',
-        minWidth: '12.25rem',
-    },
-    betaBadge: { transform: 'scale(1) translate(95%, -50%)' },
-};
+function SettingsMenu() {
+    const [anchorEl, setAnchorEl] = useState<HTMLElement>();
+    const [theme, setTheme] = useState(AppStore.getTheme());
 
-interface SettingsState {
-    anchorEl?: HTMLElement;
-    theme: string;
-}
-
-class SettingsMenu extends PureComponent<{ classes: ClassNameMap }, SettingsState> {
-    state: SettingsState = {
-        anchorEl: undefined,
-        theme: AppStore.getTheme(),
+    const handleThemeChange = () => {
+        setTheme(AppStore.getTheme());
     };
 
-    componentDidMount = () => {
-        AppStore.on('themeToggle', () => {
-            this.setState({ theme: AppStore.getTheme() });
-        });
-    };
+    useEffect(() => {
+        AppStore.on('themeToggle', handleThemeChange);
 
-    render() {
-        const { classes } = this.props;
+        return () => {
+            AppStore.off('themeToggle', handleThemeChange);
+        };
+    }, []);
 
-        return (
-            <>
-                <Button
-                    onClick={(event) => {
-                        this.setState({ anchorEl: event.currentTarget });
-                    }}
-                    color="inherit"
-                    startIcon={<Brightness4Icon />}
-                >
-                    Theme
-                </Button>
-                <Popover
-                    open={Boolean(this.state.anchorEl)}
-                    anchorEl={this.state.anchorEl}
-                    onClose={() => {
-                        this.setState({ anchorEl: undefined });
-                    }}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'center',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'center',
-                    }}
-                >
-                    <Paper className={classes.container}>
-                        <FormControl>
-                            <RadioGroup aria-label="theme" name="theme" value={this.state.theme} onChange={toggleTheme}>
-                                <FormControlLabel value="light" control={<Radio color="primary" />} label="Light" />
-                                <FormControlLabel value="dark" control={<Radio color="primary" />} label="Dark" />
-                                <FormControlLabel value="auto" control={<Radio color="primary" />} label="Automatic" />
-                            </RadioGroup>
-                        </FormControl>
-                    </Paper>
-                </Popover>
-            </>
-        );
-    }
+    return (
+        <>
+            <Button
+                onClick={(event) => {
+                    setAnchorEl(event.currentTarget);
+                }}
+                color="inherit"
+                startIcon={<Settings />}
+            >
+                Settings
+            </Button>
+            <Popover
+                open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                onClose={() => {
+                    setAnchorEl(undefined);
+                }}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <Paper style={{ padding: '0.5rem', minWidth: '12.25rem' }}>
+                    <FormControl>
+                        <RadioGroup aria-label="theme" name="theme" value={theme} onChange={toggleTheme}>
+                            <FormControlLabel value="light" control={<Radio color="primary" />} label="Light" />
+                            <FormControlLabel value="dark" control={<Radio color="primary" />} label="Dark" />
+                            <FormControlLabel value="auto" control={<Radio color="primary" />} label="Automatic" />
+                        </RadioGroup>
+                    </FormControl>
+                </Paper>
+            </Popover>
+        </>
+    );
 }
 
-export default withStyles(styles)(SettingsMenu);
+export default SettingsMenu;
