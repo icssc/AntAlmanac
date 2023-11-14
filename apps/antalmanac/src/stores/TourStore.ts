666b8fe37719b2a857a6f608db320812e50d82db
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-11-14 07:25:16 +0000

    Add Calendar step

--- apps/antalmanac/src/stores/TourStore.ts
+++ apps/antalmanac/src/stores/TourStore.ts
@@ -6,6 +6,10 @@ export const tourSteps: Array<ReactourStep> = [
         selector: '#searchBar',
         content: 'Search for your classes here!',
     },
+    {
+        selector: '.rbc-time-view', // Calendar.
+        content: 'See the classes in your schedule!',
+    },
 ];
 
 interface TourStore {
