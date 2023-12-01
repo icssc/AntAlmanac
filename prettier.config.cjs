c7a2f386374b23fb273916d1bfd0a502c7f2921e
Aponia <brianmv@uci.edu>
2023-12-01 09:08:31 -0800

    chore: update prettier and eslint configs at project root

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,23 +1,20 @@
 //@ts-check
 
 /**
- * `cjs` format is required for plugins to work in VSCode.
- * @see {@link https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/113}
+ * @type {import('prettier').Config}
  */
-
-/** @type {import('prettier').Config} */
 const config = {
-  printWidth: 120,
-  tabWidth: 4,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  arrowParens: 'always',
-  endOfLine: 'lf',
-}
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
 
-module.exports = config
+module.exports = config;
