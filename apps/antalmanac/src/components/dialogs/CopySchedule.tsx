ee4ed36cbd7e353aac8770d1ab0758687756ad41
Jun Xia <xiaj8@uci.edu>
2023-12-03 16:27:45 -0800

    Added copy button to schedule dropdown

---
+++ apps/antalmanac/src/components/dialogs/CopySchedule.tsx
@@ -0,0 +1,83 @@
+import { useState, useEffect, useCallback } from 'react';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    MenuItem,
+    Select,
+    type DialogProps,
+} from '@mui/material';
+import AppStore from '$stores/AppStore';
+import { copySchedule } from '$actions/AppStoreActions';
+
+interface CopyScheduleDialogProps extends DialogProps {
+    index: number;
+}
+
+function CopyScheduleDialog(props: CopyScheduleDialogProps) {
+    const { index, onClose } = props;
+    const [scheduleNames, setScheduleNames] = useState(AppStore.getScheduleNames());
+    const [selectedSchedule, setSelectedSchedule] = useState<number>(0);
+
+    const handleScheduleChange = useCallback((event: React.ChangeEvent<{ value: unknown }>) => {
+        setSelectedSchedule(event.target.value as number);
+    }, []);
+
+    const handleCopy = useCallback(() => {
+        if (selectedSchedule !== scheduleNames.length) {
+            copySchedule(selectedSchedule);
+        } else {
+            scheduleNames.forEach((_, scheduleIndex) => {
+                if (scheduleIndex !== index) {
+                    copySchedule(scheduleIndex);
+                }
+            });
+        }
+        onClose?.({}, 'escapeKeyDown');
+    }, [index, onClose, selectedSchedule, scheduleNames]);
+
+    const handleScheduleNamesChange = useCallback(() => {
+        setScheduleNames([...AppStore.getScheduleNames()]);
+    }, []);
+
+    useEffect(() => {
+        AppStore.on('scheduleNamesChange', handleScheduleNamesChange);
+
+        return () => {
+            AppStore.off('scheduleNamesChange', handleScheduleNamesChange);
+        };
+    }, [handleScheduleNamesChange]);
+
+    return (
+        <Dialog onClose={onClose} {...props}>
+            <DialogTitle>Copy To Schedule</DialogTitle>
+
+            <DialogContent>
+                <Box padding={1}>
+                    <Select fullWidth value={selectedSchedule} onChange={handleScheduleChange}>
+                        {scheduleNames.map((name, idx) => (
+                            <MenuItem key={idx} value={idx} disabled={index === idx}>
+                                {name}
+                            </MenuItem>
+                        ))}
+                        <MenuItem value={scheduleNames.length}>Copy to All Schedules</MenuItem>
+                    </Select>
+                </Box>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={onClose} color="inherit">
+                    Cancel
+                </Button>
+                <Button onClick={handleCopy} variant="contained" color="primary">
+                    Copy Schedule
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default CopyScheduleDialog;
