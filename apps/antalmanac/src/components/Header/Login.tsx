2f3bc79d8b09cf23f89f09a6966f043c46deab4b
Aponia <brianmv@uci.edu>
2023-12-07 08:42:37 -0800

    feat: working auth button in header

--- apps/antalmanac/src/components/Header/Login.tsx
+++ apps/antalmanac/src/components/Header/Login.tsx
@@ -1,85 +1,72 @@
-import * as React from 'react';
-import { Box, Button, Grid, Fade, Paper, Popper, Typography, type PopperPlacementType } from '@mui/material';
+import { useState, useRef } from 'react';
+import { Avatar, Box, Button, Fade, IconButton, Paper, Popper } from '@mui/material';
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { useCallback } from 'react';
-import trpc from '$lib/api/trpc';
+import { useSnackbar } from 'notistack';
+import { useAuthStore } from '$stores/auth';
 
 export function Login() {
-    const utils = trpc.useUtils();
+    const { enqueueSnackbar } = useSnackbar();
 
-    const query = trpc.auth.status.useQuery();
+    const ref = useRef();
 
-    const onSuccess = useCallback((credentialResponse: CredentialResponse) => {
-        console.log(credentialResponse);
+    const [open, setOpen] = useState(false);
 
-        document.cookie = `access_token=${credentialResponse.credential}; path=/`;
+    const { user, setUser, logout } = useAuthStore();
 
-        utils.auth.invalidate();
+    const handleClick = useCallback(() => {
+        setOpen((previousOpen) => !previousOpen);
     }, []);
+
+    const onSuccess = useCallback(
+        (credentialResponse: CredentialResponse) => {
+            setUser(credentialResponse);
+            enqueueSnackbar('Login Success', { variant: 'success' });
+            setOpen(false);
+        },
+        [setUser]
+    );
 
     const onError = useCallback(() => {
-        console.log('Login Failed');
+        enqueueSnackbar('Login Failed', { variant: 'error' });
+        setOpen(false);
     }, []);
 
-    return query.data?.email ? <div>LOGGED IN</div> : <GoogleLogin onSuccess={onSuccess} onError={onError} />;
-}
-
-export function PositionedPopper() {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
-    const [open, setOpen] = React.useState(false);
-    const [placement, setPlacement] = React.useState<PopperPlacementType>();
-
-    const handleClick = (newPlacement: PopperPlacementType) => (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-        setOpen((prev) => placement !== newPlacement || !prev);
-        setPlacement(newPlacement);
-    };
+    const handleLogout = useCallback(() => {
+        logout();
+        enqueueSnackbar('Logout Success', { variant: 'success' });
+        setOpen(false);
+    }, [logout]);
 
     return (
-        <Box sx={{ width: 500 }}>
-            <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
-                {({ TransitionProps }) => (
-                    <Fade {...TransitionProps} timeout={350}>
-                        <Paper>
-                            <Typography sx={{ p: 2 }}>The content of the Popper.</Typography>
-                        </Paper>
-                    </Fade>
+        <Box>
+            <Box ref={ref}>
+                {user?.email ? (
+                    <IconButton onClick={handleClick} sx={{ p: 0 }}>
+                        <Avatar alt={user.name} src={user.picture} />
+                    </IconButton>
+                ) : (
+                    <Button onClick={handleClick} variant="outlined" color="inherit">
+                        LOG IN
+                    </Button>
                 )}
-            </Popper>
-            <Grid container justifyContent="center">
-                <Grid item>
-                    <Button onClick={handleClick('top-start')}>top-start</Button>
-                    <Button onClick={handleClick('top')}>top</Button>
-                    <Button onClick={handleClick('top-end')}>top-end</Button>
-                </Grid>
-            </Grid>
-            <Grid container justifyContent="center">
-                <Grid item xs={6}>
-                    <Button onClick={handleClick('left-start')}>left-start</Button>
-                    <br />
-                    <Button onClick={handleClick('left')}>left</Button>
-                    <br />
-                    <Button onClick={handleClick('left-end')}>left-end</Button>
-                </Grid>
-                <Grid item container xs={6} alignItems="flex-end" direction="column">
-                    <Grid item>
-                        <Button onClick={handleClick('right-start')}>right-start</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button onClick={handleClick('right')}>right</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button onClick={handleClick('right-end')}>right-end</Button>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid container justifyContent="center">
-                <Grid item>
-                    <Button onClick={handleClick('bottom-start')}>bottom-start</Button>
-                    <Button onClick={handleClick('bottom')}>bottom</Button>
-                    <Button onClick={handleClick('bottom-end')}>bottom-end</Button>
-                </Grid>
-            </Grid>
+
+                <Popper open={open} anchorEl={ref.current} placement="bottom-end" transition>
+                    {({ TransitionProps }) => (
+                        <Fade {...TransitionProps} timeout={350}>
+                            <Paper sx={{ p: 4 }}>
+                                {user?.email != null ? (
+                                    <Button onClick={handleLogout} variant="outlined" color="inherit">
+                                        LOG OUT
+                                    </Button>
+                                ) : (
+                                    <GoogleLogin onSuccess={onSuccess} onError={onError} />
+                                )}
+                            </Paper>
+                        </Fade>
+                    )}
+                </Popper>
+            </Box>
         </Box>
     );
 }
--- apps/antalmanac/src/stores/auth.ts
+++ apps/antalmanac/src/stores/auth.ts
@@ -2,14 +2,32 @@ import { parse } from 'cookie';
 import { decodeJwt } from 'jose';
 import { create } from 'zustand';
 import { GoogleUserSchema } from '@packages/antalmanac-types';
+import { CredentialResponse } from '@react-oauth/google';
 
 export interface AuthStore {
     user?: typeof GoogleUserSchema.infer;
+    setUser: (credentialResponse: CredentialResponse) => void;
+    logout: () => void;
 }
 
-export const useAuthStore = create<AuthStore>(() => {
+export const useAuthStore = create<AuthStore>((set) => {
     return {
         user: getUser(),
+        setUser: (credentialResponse) => {
+            document.cookie = `access_token=${credentialResponse.credential}; path=/;`;
+
+            if (credentialResponse.credential) {
+                set({
+                    user: decodeJwt(credentialResponse.credential),
+                });
+            }
+        },
+        logout: () => {
+            document.cookie = 'access_token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+            set({
+                user: undefined,
+            });
+        },
     };
 });
 
