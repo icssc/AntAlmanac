7770bbd22729310d832990c120667bc9c593a1b4
Kevin Wu <kevinwu098@gmail.com>
2023-12-11 12:41:07 -0800

    feat: initial refactor of Import (WIP)

--- apps/antalmanac/src/lib/websoc.ts
+++ apps/antalmanac/src/lib/websoc.ts
@@ -58,6 +58,8 @@ class _WebSOC {
 
     async getCourseInfo(websoc_params: Record<string, string>) {
         const SOCObject = await this.query(websoc_params);
+
+        console.log(SOCObject);
         const courseInfo: { [sectionCode: string]: CourseInfo } = {};
         for (const school of SOCObject.schools) {
             for (const department of school.departments) {
