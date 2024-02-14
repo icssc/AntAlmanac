70c6efd99de2fc8ba9a8e3e97135c020b9830d7a
Aponia <brianmv@uci.edu>
2024-02-14 12:08:01 -0800

    refactor(types): use combined table schema per #890

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -4,15 +4,11 @@ import { type } from 'arktype';
 export const UserSchema = type({
     id: 'string',
     userData: ScheduleSaveStateSchema,
+
+    'googleId?': 'string',
+    'name?': 'string',
+    'email?': 'string',
+    'picture?': 'string',
 });
 
-export const AuthUserSchema = type({
-    id: 'string',
-    'userData?': ScheduleSaveStateSchema,
-    name: 'string',
-    email: 'string',
-    picture: 'string',
-});
 export type User = typeof UserSchema.infer;
-
-export type AuthUser = typeof AuthUserSchema.infer;
