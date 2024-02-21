92e67ccf56add109f2d54bea0ec8d15c9aa19217
Kevin Wu <kevinwu098@gmail.com>
2024-02-20 17:32:08 -0800

    feat: initial scaffold of localStorage abstraction

---
+++ apps/antalmanac/src/lib/localStorage.ts
@@ -0,0 +1,11 @@
+export enum LocalStorageKeys {
+    userId = 'userID' /* The case-difference is due to the original implementation */,
+}
+
+export function setLocalStorageItem(key: LocalStorageKeys, value: string) {
+    window.localStorage.setItem(key, value);
+}
+
+export function removeLocalStorageItem(key: LocalStorageKeys) {
+    window.localStorage.removeItem(key);
+}
