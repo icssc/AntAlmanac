599df312950dd9a6564000ea2141df1aa9fa65a2
Aponia <brianmv@uci.edu>
2024-02-29 23:01:32 -0800

    refactor: ddb client

--- apps/backend/src/db/ddb.ts
+++ apps/backend/src/db/ddb.ts
@@ -1,4 +1,5 @@
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import { type, Type } from 'arktype';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 
 import {
@@ -8,24 +9,30 @@ import {
     RepeatingCustomEventSchema,
     type LegacyUserData,
     type ScheduleSaveState,
-    type User,
 } from '@packages/antalmanac-types';
 
 import LegacyUserModel from '../models/User';
 import connectToMongoDB from '../db/mongodb';
 import env from '../env';
 
-class DDBClient<T extends Record<string, any>> {
+export const n = type({
+    hellO: 'string',
+});
+
+n.assert;
+
+class DDBClient<T extends Record<string, unknown>, TSchema extends Type<T> = Type<T>> {
+    private tableName: string;
+
+    private schema: TSchema;
+
     client: DynamoDB;
 
     documentClient: DynamoDBDocument;
 
-    private tableName: string;
-
-    private schema: any;
-
     static convertLegacySchedule(legacyUserData: LegacyUserData) {
         const scheduleSaveState: ScheduleSaveState = { schedules: [], scheduleIndex: 0 };
+
         for (const scheduleName of legacyUserData.scheduleNames) {
             scheduleSaveState.schedules.push({
                 scheduleName: scheduleName,
@@ -34,44 +41,50 @@ class DDBClient<T extends Record<string, any>> {
                 scheduleNote: '',
             });
         }
+
         for (const course of legacyUserData.addedCourses) {
             const { data, problems } = ShortCourseSchema(course);
+
             if (data === undefined) {
                 console.log(problems);
                 continue;
             }
+
             for (const scheduleIndex of course.scheduleIndices) {
                 scheduleSaveState.schedules[scheduleIndex].courses.push(data);
             }
         }
+
         for (const customEvent of legacyUserData.customEvents) {
             for (const scheduleIndex of customEvent.scheduleIndices) {
                 const { data } = RepeatingCustomEventSchema(customEvent);
+
                 if (data !== undefined) {
                     scheduleSaveState.schedules[scheduleIndex].customEvents.push(data);
                 }
             }
         }
+
         return scheduleSaveState;
     }
 
-    constructor(tableName: string, schema: any) {
+    constructor(tableName: string, schema: TSchema) {
+        this.tableName = tableName;
+        this.schema = schema;
         this.client = new DynamoDB({
             region: env.AWS_REGION,
         });
-
-        // Create DynamoDB DocumentClient
         this.documentClient = DynamoDBDocument.from(this.client, {
             marshallOptions: {
                 /**
-                 * Some JSON properties may exist and be undefined; DDB will throw an error unless this is true.
+                 * Some JSON properties may exist and be undefined.
+                 * DDB will throw an error unless this is true.
+                 *
                  * @example { "property": undefined }
                  */
                 removeUndefinedValues: true,
             },
         });
-        this.tableName = tableName;
-        this.schema = schema;
     }
 
     async get(id: string): Promise<T | undefined> {
@@ -83,15 +96,14 @@ class DDBClient<T extends Record<string, any>> {
         };
 
         const { Item } = await this.documentClient.get(params);
-        const { data, problems } = this.schema(Item);
-        return problems === undefined ? data : undefined;
+
+        const { data } = this.schema(Item);
+
+        return data as T | undefined;
     }
 
     async insertItem(item: T) {
-        await this.documentClient.put({
-            TableName: this.tableName,
-            Item: item,
-        });
+        await this.documentClient.put({ TableName: this.tableName, Item: item });
     }
 
     async updateSchedule(id: string, schedule: ScheduleSaveState) {
@@ -105,6 +117,7 @@ class DDBClient<T extends Record<string, any>> {
                 ':u': schedule,
             },
         };
+
         await this.documentClient.update(params);
     }
 
@@ -112,17 +125,17 @@ class DDBClient<T extends Record<string, any>> {
         await connectToMongoDB();
 
         const { data, problems } = LegacyUserSchema(await LegacyUserModel.findById(userId));
-        if (problems !== undefined) {
+
+        if (problems != null || data.userData == null) {
             return undefined;
         }
 
-        const legacyUserData = data?.userData;
-        return legacyUserData ? { id: userId, userData: DDBClient.convertLegacySchedule(legacyUserData) } : undefined;
+        return { id: userId, userData: DDBClient.convertLegacySchedule(data.userData) };
     }
 
-    async getUserData(userId: string) {
-        return (await ddbClient.get(userId))?.userData;
+    async getUserData(id: string) {
+        return (await ddbClient.get(id))?.userData;
     }
 }
 
-export const ddbClient = new DDBClient<User>(env.USERDATA_TABLE_NAME, UserSchema);
+export const ddbClient = new DDBClient(env.USERDATA_TABLE_NAME, UserSchema);
