92437b1f1c88909ff801715a7cc821fbcaa25075
Aponia <brianmv@uci.edu>
2024-03-06 07:56:53 -0800

    chore: update lint configs (#940)

--- apps/antalmanac/.eslintrc.cjs
+++ apps/antalmanac/.eslintrc.cjs
@@ -1,43 +1,27 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-  },
-  plugins: [
-    'react',
-    'react-hooks',
-    'import',
-    'jsx-a11y',
-    '@typescript-eslint/eslint-plugin',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  rules: {
-    'import/order': 'error',
-  },
-  env: {
-    browser: true,
-    es6: true,
-  },
-  globals: {
-    React: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    }
-  },
-  ignorePatterns: ['vite.config.ts'],
-}
+// @ts-check
 
-module.exports = config
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+    parserOptions: {
+        project: ['tsconfig.json'],
+    },
+    plugins: ['react', 'react-hooks', 'jsx-a11y'],
+    extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:jsx-a11y/recommended',
+    ],
+    globals: {
+        React: true,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
+
+module.exports = config;
