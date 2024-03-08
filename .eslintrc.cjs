73d70cbd155a2d79fcde46c933081f945f33a7bc
Aponia <brianmv@uci.edu>
2024-03-08 09:58:04 -0800

    chore: disable import/no-unresolved typescript-eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ const config = {
                 'newlines-between': 'always',
             },
         ],
+        'import/no-unresolved': 'off',
     },
     settings: {
         'import/resolver': {
