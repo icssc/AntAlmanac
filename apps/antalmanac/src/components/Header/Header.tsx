21ee1aeb042cd138162804f476fe37e08a9ffe4d
Dalton Adcock <dadcock@uci.edu>
2024-05-13 13:35:19 -0700

    initial idea

--- apps/antalmanac/src/components/Header/Header.tsx
+++ apps/antalmanac/src/components/Header/Header.tsx
@@ -7,8 +7,14 @@ import Import from './Import';
 import LoadSaveScheduleFunctionality from './LoadSaveFunctionality';
 import AppDrawer from './SettingsMenu';
 
-import Logo from '$assets/logo.svg';
-import MobileLogo from '$assets/mobile-logo.svg';
+import ChristmasLogo from '$assets/christmas-logo.png';
+import MobileChristmasLogo from '$assets/christmas-mobile-logo.png';
+import DefaultLogo from '$assets/logo.svg';
+import MobileDefaultLogo from '$assets/mobile-logo.svg';
+//import ThanksgivingLogo from '$assets/thanksgiving-logo.png';
+//import MobileThanksgivingLogo from '$assets/thanksgiving-mobile-logo.png';
+//import HalloweenLogo from '$assets/halloween-logo.png';
+//import MobileHalloweenLogo from '$assets/halloween-mobile-logo.png';
 
 const styles = {
     appBar: {
@@ -37,6 +43,40 @@ interface CustomAppBarProps {
     classes: ClassNameMap;
 }
 
+/*
+interface LogoBounds {
+    startDay: number; // inclusive
+    startMonth: number;
+    endDay: number; // exclusive
+    endMonth: number;
+}
+*/
+
+const withinBounds = (): boolean => {
+    //(bounds: LogoBounds): boolean => {
+    //const { startDay, startMonth, endDay, endMonth } = bounds;
+    //const currentDate = new Date();
+    //const currentMonth = currentDate.getMonth();
+    //const currentDay = currentDate.getDate();
+
+    // compare dates
+    return true; // not implemented obviously
+};
+
+//const Christmas: LogoBounds = { startDay: 1, startMonth: 11, endDay: 1, endMonth: 1 };
+// ...
+
+const getLogo = (isMobileScreen: boolean): string => {
+    if (withinBounds()) {
+        //Christmas)) {
+        return isMobileScreen ? MobileChristmasLogo : ChristmasLogo;
+    }
+    // ...
+    else {
+        return isMobileScreen ? MobileDefaultLogo : DefaultLogo;
+    }
+};
+
 const Header = ({ classes }: CustomAppBarProps) => {
     const isMobileScreen = useMediaQuery('(max-width:750px)');
 
@@ -45,7 +85,7 @@ const Header = ({ classes }: CustomAppBarProps) => {
             <Toolbar variant="dense" style={{ padding: '5px', display: 'flex', justifyContent: 'space-between' }}>
                 <img
                     height={32}
-                    src={isMobileScreen ? MobileLogo : Logo}
+                    src={getLogo(isMobileScreen)}
                     title={'Thanks Aejin for designing this seasonal logo!'}
                     alt="logo"
                 />
