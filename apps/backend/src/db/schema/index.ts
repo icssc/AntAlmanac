f92b8271367531f2dbbbf380e3ee4295b6566457
Aponia <brianmv@uci.edu>
2024-05-15 23:04:26 -0700

    fix(backend): db schema entrypoint

--- apps/backend/src/db/schema/index.ts
+++ apps/backend/src/db/schema/index.ts
@@ -1,16 +1,4 @@
-import { integer, pgEnum, pgTable, serial, uniqueIndex, varchar } from 'drizzle-orm/pg-core';
-
-export const user = pgTable(
-    'user',
-    {
-        id: serial('id').primaryKey(),
-        name: varchar('name', { length: 256 }),
-    },
-);
-
-export const cities = pgTable('cities', {
-    id: serial('id').primaryKey(),
-    name: varchar('name', { length: 256 }),
-    countryId: integer('country_id').references(() => countries.id),
-    popularity: popularityEnum('popularity'),
-});
+export * from './auth'
+export * from './course'
+export * from './schedule'
+export * from './subscription'
