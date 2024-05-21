97880493cfff42be8112ef1483d735306c076ed5
Dalton Adcock <dadcock@uci.edu>
2024-05-21 13:59:49 -0700

    logo credit

--- apps/antalmanac/src/components/Header/Logo.tsx
+++ apps/antalmanac/src/components/Header/Logo.tsx
@@ -47,51 +47,41 @@ interface Logo {
     endMonthIndex: number;
 }
 
-interface LogoProps {
-    isMobileScreen: boolean;
+interface Logos {
     logos: Logo[];
 }
 
+interface LogoImage extends Pick<Logo, 'mobileLogo' | 'desktopLogo'> {
+    imageCredit?: string;
+}
+
 const inBounds = (props: Logo): boolean => {
-    // get current date
     const currentDate = new Date();
-
-    // for testing...
-    // const currentDate = new Date(2024, 8, 30); // within default logo range
-    // const currentDate = new Date(2024, 9, 31); // last day of halloween logo (last day of october)
-    // const currentDate = new Date(2024, 10, 1); // first day of thanksgiving logo (first day of november, etc.)
-    // const currentDate = new Date(2024, 10, 30); // last day of thanksgiving logo
-    // const currentDate = new Date(2024, 0, 15); // should be christmas logo
-    // const currentDate = new Date(2024, 1, 1); // first day back in default range - dalton as of 5/20/24
-
-    // get bounds
     const currentYear = currentDate.getFullYear();
+
     const endYear = currentYear;
     const startYear = props.startMonthIndex > props.endMonthIndex ? endYear - 1 : endYear;
     const endDate = new Date(endYear, props.endMonthIndex, props.endDay);
     const startDate = new Date(startYear, props.startMonthIndex, props.startDay);
 
-    // check if current date is in bounds of special event/holiday/season
     return currentDate >= startDate && currentDate < endDate;
 };
 
-const getLogo = (props: LogoProps): string => {
+const getLogo = (props: Logos): LogoImage => {
     for (const logo of props.logos) {
         if (inBounds(logo)) {
-            // console.log('using the ' + logo.name + ' logo!');
-            return props.isMobileScreen ? logo.mobileLogo : logo.desktopLogo;
+            return {
+                mobileLogo: logo.mobileLogo,
+                desktopLogo: logo.desktopLogo,
+                imageCredit: 'Thanks Aejin for designing this seasonal logo!',
+            };
         }
     }
-    return props.isMobileScreen ? MobileDefaultLogo : DefaultLogo;
+    return { mobileLogo: MobileDefaultLogo, desktopLogo: DefaultLogo };
 };
 
 export default function Logo({ isMobileScreen }: { isMobileScreen: boolean }) {
-    return (
-        <img
-            height={32}
-            src={getLogo({ isMobileScreen: isMobileScreen, logos: logos })}
-            title={'Thanks Aejin for designing this seasonal logo!'}
-            alt="logo"
-        />
-    );
+    const { mobileLogo, desktopLogo, imageCredit } = getLogo({ logos });
+
+    return <img height={32} src={isMobileScreen ? mobileLogo : desktopLogo} title={imageCredit ?? ''} alt="logo" />;
 }
