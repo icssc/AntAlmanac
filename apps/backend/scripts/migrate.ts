f3caa71924546f6a9dcf9aebb9e8d548143be7f0
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-11-05 23:55:30 -0800

    feat(migration): properly copy users

--- apps/backend/scripts/migrate.ts
+++ apps/backend/scripts/migrate.ts
@@ -7,14 +7,14 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 import { ddbClient } from '../src/db/ddb.ts';
 import { db, client } from '../src/db/index.ts'; 
-import { users } from '../src/db/schema/index.ts';
+import { accounts, users } from '../src/db/schema/index.ts';
 
 /**
  * Migrates the current drizzle schema to the PostgreSQL database associated
  * with the drizzle client. Before migrating, run "pnpm run generate" to generate
  * the migration files, and make sure the PostgreSQL database exists beforehand.
  */
-async function migrateToPostgres() {
+async function migratePostgresDb() {
     await migrate(drizzle(client), {
         migrationsFolder: './drizzle'
     });
@@ -24,17 +24,40 @@ async function migrateToPostgres() {
  * Migrates user data from DynamoDB to the PostgreSQL database associated
  * with the drizzle client.
  */
-async function insertUsersToPostgres() {
-    const userIds = await ddbClient.getUserIds();
-    const usersToInsert = userIds.map((userId) => ({ id: userId }));
-    await db.insert(users).values(usersToInsert);
-    console.log(await db.query.users.findMany());
+async function copyUsersToPostgres() {
+    for await (const ddbBatch of ddbClient.getAllUserDataBatches()) {
+        // To insert 
+        const numUsers = ddbBatch.length;
+        const emptyArray = new Array(numUsers).fill({});
+
+        const newUsersIds = (
+            await db
+            .insert(users)
+            .values(emptyArray)
+            .returning({ userId: users.id})
+        ).map((user) => user.userId);
+        
+        // Make guest accounts for every user
+        db
+            .insert(accounts)
+            .values(newUsersIds.map(
+                (userId, index) => (
+                    { userId, providerAccountId: ddbBatch[index].name }
+                )
+            ))
+            // If someone changed their account information during the migration,
+            // don't overwrite it.
+            .onConflictDoNothing({ target: accounts.providerAccountId});
+
+        return newUsersIds;
+    }
 }
 
 async function main() {
     try {
-        await migrateToPostgres();
-        await insertUsersToPostgres();
+        await migratePostgresDb();
+        await copyUsersToPostgres();
+        // TODO: copy other tables
     } catch (error) {
         console.log(error);
     } finally {
