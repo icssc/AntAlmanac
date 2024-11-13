9b4f174b1eae85c2fe66d1bfec7def60cf645d60
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-11-12 18:41:07 -0800

    feat: remove hardcoded prereq tree types

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -8,6 +8,7 @@ const Environment = type({
     AWS_REGION: 'string',
     MAPBOX_ACCESS_TOKEN: 'string',
     'PR_NUM?': 'number',
+    STAGE: "'prod' | 'dev' | 'local'",
 });
 
 const env = Environment.assert({ ...process.env });
--- packages/anteater-api-types/src/courses.ts
+++ packages/anteater-api-types/src/courses.ts
@@ -1,34 +1,7 @@
-import { paths } from './generated/anteater-api-types';
+import { components, paths } from './generated/anteater-api-types';
 
-type _Course = paths['/v2/rest/courses/{id}']['get']['responses'][200]['content']['application/json']['data'];
+export type Course = paths['/v2/rest/courses/{id}']['get']['responses'][200]['content']['application/json']['data'];
 
-type CoursePrerequisite = {
-    prereqType: 'course';
-    coreq: false;
-    courseId: string;
-    minGrade?: string;
-};
+export type Prerequisite = components['schemas']['prereq'];
 
-type CourseCorequisite = {
-    prereqType: 'course';
-    coreq: true;
-    courseId: string;
-};
-
-type ExamPrerequisite = {
-    prereqType: 'exam';
-    examName: string;
-    minGrade?: string;
-};
-
-export type Prerequisite = CoursePrerequisite | CourseCorequisite | ExamPrerequisite;
-
-export type PrerequisiteTree = {
-    AND?: Array<Prerequisite | PrerequisiteTree>;
-    OR?: Array<Prerequisite | PrerequisiteTree>;
-    NOT?: Array<Prerequisite | PrerequisiteTree>;
-};
-
-export interface Course extends _Course {
-    prerequisiteTree: PrerequisiteTree;
-}
+export type PrerequisiteTree = components['schemas']['prereqTree'];
