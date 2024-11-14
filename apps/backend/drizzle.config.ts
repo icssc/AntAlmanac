c70423078fb0b4ca22d68ff23bde705b0387596f
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2024-11-13 21:54:12 -0800

    feat(drizzle): dotenv in config and studio

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,7 +1,15 @@
+import * as dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+dotenv.config();
+
+const { DB_URL } = process.env;
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: DB_URL ?? "",
+  }
 });
