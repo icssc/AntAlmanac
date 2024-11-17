6c6215c4d52897a6472272c92266c38417c50cdf
Alex Espejo <aespejo102@gmail.com>
2024-11-17 12:06:17 -0800

    created error page

--- apps/antalmanac/src/routes/Error.tsx
+++ apps/antalmanac/src/routes/Error.tsx
@@ -1,7 +1,25 @@
+import { Typography, Button, Stack, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => (
-    <div>
-        <h1>404</h1>
-        <p>The page you are looking for does not exist.</p>
-    </div>
+    <Box sx={{ padding: '1rem' }}>
+        <Typography variant="h3" component="h1">
+            Oops! Something went wrong 🥺 👉👈
+        </Typography>
+        <Stack spacing={2} sx={{ paddingY: '1rem' }}>
+            <Typography variant="h5" component="p">
+                This error may be caused by you having an out of date version of AntAlmanac.
+            </Typography>
+            <Typography variant="h5" component="p">
+                Try refreshing the page. If the error persists, please submit a{' '}
+                <Link to="https://forms.gle/k81f2aNdpdQYeKK8A">bug report</Link>
+            </Typography>
+        </Stack>
+        <Link to="/">
+            <Button variant="contained" size="large">
+                Reload
+            </Button>
+        </Link>
+    </Box>
 );
 export default ErrorPage;
