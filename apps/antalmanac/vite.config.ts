2192a258eb9ab09b7bbcd7f10ca195bce9bfc418
Dalton Adcock <dadcock@uci.edu>
2024-11-22 14:04:42 -0800

    custom sw static, maps broken

--- apps/antalmanac/vite.config.ts
+++ apps/antalmanac/vite.config.ts
@@ -2,9 +2,43 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            strategies: 'injectManifest',
+            srcDir: 'src',
+            filename: 'sw.ts',
+            devOptions: {
+                enabled: true,
+            },
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'], // may need mask-icon.svg
+            injectManifest: {
+                globPatterns: ['**/*.{html,js,css,png,svg,jpg,json}'], // Match files for pre-caching
+            },
+            manifest: {
+                name: 'AntAlmanac',
+                short_name: 'AntAlmanac',
+                description: 'A course exploration and scheduling tool for UCI Anteaters',
+                theme_color: '#305db7',
+                icons: [
+                    {
+                        src: 'logo-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'logo-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             $assets: resolve(__dirname, './src/assets'),
