31245665000142a82e4779591c04e1b2369e4a67
Kevin Wu <kevinwu098@gmail.com>
2024-11-24 09:23:54 -0800

    style: fix

--- apps/antalmanac/src/routes/ErrorPage.tsx
+++ apps/antalmanac/src/routes/ErrorPage.tsx
@@ -7,7 +7,7 @@ export const ErrorPage = () => {
     const location = useLocation();
 
     return (
-        <Box sx={{ height: '100dvh', overflowY: 'scroll' }}>
+        <Box sx={{ height: '100dvh', overflowY: 'auto' }}>
             <Stack
                 sx={{
                     display: 'flex',
@@ -16,45 +16,45 @@ export const ErrorPage = () => {
                     maxWidth: 800,
                     minHeight: '100dvh',
                     margin: 'auto',
+                    padding: 2,
+                    gap: 2,
                 }}
             >
-                <Stack sx={{ maxHeight: '100dvh', gap: 2, height: 'fit-content', padding: 2 }}>
-                    <Typography variant="h3" component="h1">
-                        Oops! Something went wrong.
+                <Typography variant="h3" component="h1">
+                    Oops! Something went wrong.
+                </Typography>
+                <Stack spacing={2}>
+                    <Typography variant="h5" component="p">
+                        This error may be caused by your browser having an out of date version of AntAlmanac.
                     </Typography>
-                    <Stack spacing={2}>
-                        <Typography variant="h5" component="p">
-                            This error may be caused by your browser having an out of date version of AntAlmanac.
-                        </Typography>
-                        <Typography variant="h5" component="p">
-                            Try refreshing the page. If the error persists, please submit a{' '}
-                            <Link to="https://forms.gle/k81f2aNdpdQYeKK8A">bug report</Link> with the provided error.
-                        </Typography>
-                    </Stack>
-                    <Link to="/">
-                        <Button variant="contained" size="large">
-                            Back to Home
-                        </Button>
-                    </Link>
-                    <Accordion defaultExpanded disableGutters sx={{ maxWidth: '100%' }}>
-                        <AccordionSummary expandIcon={<ExpandMore />}>
-                            <Typography component="p">View Error Message</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails
-                            sx={{
-                                display: 'flex',
-                                gap: 1,
-                                textAlign: 'left',
-                                flexWrap: 'wrap',
-                            }}
-                        >
-                            <Typography sx={{ fontWeight: '600' }}>Route: {location.pathname}</Typography>
-                            <Typography sx={{ wordBreak: 'break-word' }}>
-                                {error instanceof Error ? error.stack : 'No error stack provided.'}
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
+                    <Typography variant="h5" component="p">
+                        Try refreshing the page. If the error persists, please submit a{' '}
+                        <Link to="https://forms.gle/k81f2aNdpdQYeKK8A">bug report</Link> with the provided error.
+                    </Typography>
                 </Stack>
+                <Link to="/">
+                    <Button variant="contained" size="large">
+                        Back to Home
+                    </Button>
+                </Link>
+                <Accordion defaultExpanded disableGutters sx={{ maxWidth: '100%' }}>
+                    <AccordionSummary expandIcon={<ExpandMore />}>
+                        <Typography component="p">View Error Message</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                        sx={{
+                            display: 'flex',
+                            gap: 1,
+                            textAlign: 'left',
+                            flexWrap: 'wrap',
+                        }}
+                    >
+                        <Typography sx={{ fontWeight: '600' }}>Route: {location.pathname}</Typography>
+                        <Typography sx={{ wordBreak: 'break-word' }}>
+                            {error instanceof Error ? error.stack : 'No error stack provided.'}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
             </Stack>
         </Box>
     );
