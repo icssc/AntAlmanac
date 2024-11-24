228de55f01b89fdcdc754bbf634d0f2b77c10812
Kevin Wu <kevinwu098@gmail.com>
2024-11-24 08:05:41 -0800

    feat: update error page

--- apps/antalmanac/src/routes/ErrorPage.tsx
+++ apps/antalmanac/src/routes/ErrorPage.tsx
@@ -1,43 +1,61 @@
-import { Typography, Button, Stack } from '@mui/material';
-import { Link, useRouteError } from 'react-router-dom';
+import { ExpandMore } from '@mui/icons-material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Button, Stack } from '@mui/material';
+import { Link, useLocation, useRouteError } from 'react-router-dom';
 
 export const ErrorPage = () => {
     const error = useRouteError();
+    const location = useLocation();
 
     return (
         <Stack
-            spacing={3}
             sx={{
-                padding: '1rem',
                 display: 'flex',
                 justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                height: '100vh',
-                gap: 2,
+                textAlign: 'center',
+                maxWidth: 800,
+                minHeight: '100dvh',
+                margin: 'auto',
+                overflowY: 'scroll',
             }}
         >
-            <Typography variant="h3" component="h1">
-                Oops! Something went wrong.
-            </Typography>
-            <Stack spacing={2} sx={{ textAlign: 'center' }}>
-                <Typography variant="h5" component="p">
-                    This error may be caused by your browser having an out of date version of AntAlmanac.
+            <Stack sx={{ maxHeight: '100dvh', gap: 2, height: 'fit-content', padding: 2 }}>
+                <Typography variant="h3" component="h1">
+                    Oops! Something went wrong.
                 </Typography>
-                <Typography variant="h5" component="p">
-                    Try refreshing the page. If the error persists, please submit a{' '}
-                    <Link to="https://forms.gle/k81f2aNdpdQYeKK8A">bug report</Link> with the provided error.
-                </Typography>
+                <Stack spacing={2}>
+                    <Typography variant="h5" component="p">
+                        This error may be caused by your browser having an out of date version of AntAlmanac.
+                    </Typography>
+                    <Typography variant="h5" component="p">
+                        Try refreshing the page. If the error persists, please submit a{' '}
+                        <Link to="https://forms.gle/k81f2aNdpdQYeKK8A">bug report</Link> with the provided error.
+                    </Typography>
+                </Stack>
+                <Link to="/">
+                    <Button variant="contained" size="large">
+                        Back to Home
+                    </Button>
+                </Link>
+                <Accordion defaultExpanded disableGutters sx={{ maxWidth: '100%' }}>
+                    <AccordionSummary expandIcon={<ExpandMore />}>
+                        <Typography component="p">View Error Message</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails
+                        sx={{
+                            display: 'flex',
+                            gap: 1,
+                            textAlign: 'left',
+                            flexWrap: 'wrap',
+                            whiteSpace: 'normal',
+                            wordWrap: 'break-word',
+                        }}
+                    >
+                        <Typography sx={{ fontWeight: '600' }}>Route: {location.pathname}</Typography>
+
+                        {error instanceof Error ? error.stack : 'No error stack provided.'}
+                    </AccordionDetails>
+                </Accordion>
             </Stack>
-            <Link to="/">
-                <Button variant="contained" size="large">
-                    Back to Home
-                </Button>
-            </Link>
-            <details open>
-                <summary>View Error Message</summary>
-                <p>{error instanceof Error && <pre>{error.stack}</pre>}</p>
-            </details>
         </Stack>
     );
 };
