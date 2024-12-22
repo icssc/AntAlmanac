5f667ab1daf5ccf081e11ab3a6119884d1c7c20c
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2024-12-22 08:32:08 -0800

    Migrate feedback form to Asana (#1080)

--- apps/antalmanac/src/routes/Feedback.tsx
+++ apps/antalmanac/src/routes/Feedback.tsx
@@ -1,4 +1,6 @@
+export const FEEDBACK_LINK = 'https://form.asana.com/?k=fZ3SGnuGknDmzTYdocgIUg&d=1208267282546207';
+
 export default function Feedback() {
-    window.location.replace('https://forms.gle/k81f2aNdpdQYeKK8A');
+    window.location.replace(FEEDBACK_LINK);
     return null;
 }
