902ed9733381af883ff1f7908f547d12951a6944
Alex Espejo <aespejo102@gmail.com>
2024-12-29 19:47:37 -0800

    globally accessible quick search

--- apps/antalmanac/src/lib/helpers.ts
+++ apps/antalmanac/src/lib/helpers.ts
@@ -1,6 +1,9 @@
 import { MouseEvent } from 'react';
 
 import { openSnackbar } from '$actions/AppStoreActions';
+import depts from '$components/RightPane/CoursePane/SearchForm/DeptSearchBar/depts';
+import { useQuickSearchStore } from '$stores/QuickSearchStore';
+import { useTabStore } from '$stores/TabStore';
 
 export const warnMultipleTerms = (terms: Set<string>) => {
     openSnackbar(
@@ -16,6 +19,22 @@ export async function clickToCopy(event: MouseEvent<HTMLElement>, sectionCode: s
     event.stopPropagation();
     await navigator.clipboard.writeText(sectionCode);
     openSnackbar('success', 'WebsocSection code copied to clipboard');
+}
+
+export function quickSearchForClasses(title: string, term: string) {
+    const decompCourseInfo: string[] | undefined = title.match(/^(.*)\s(\S+)$/)?.slice(1);
+    const tabNum = useTabStore.getState().activeTab;
+
+    // tabNum == 1 locks this feature to only the Search page (even on mobile)
+    if (decompCourseInfo && tabNum == 1) {
+        const deptIdx: number = depts.findIndex((item) => item.deptValue === decompCourseInfo[0]);
+        useQuickSearchStore.getState().setValue({
+            term: term,
+            deptLabel: depts[deptIdx].deptLabel,
+            deptValue: decompCourseInfo[0],
+            courseNumber: decompCourseInfo[1],
+        });
+    }
 }
 
 export const FAKE_LOCATIONS = ['VRTL REMOTE', 'ON LINE', 'TBA'];
