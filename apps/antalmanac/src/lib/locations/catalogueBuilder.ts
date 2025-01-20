c829321229493cb9f083bfce4f22aafc66c68e53
Grace Yan <xgraceyan@gmail.com>
2025-01-19 16:28:36 -0800

    Removed axios dependency + made requested changes to catalogueBuilder

--- apps/antalmanac/src/lib/locations/catalogueBuilder.ts
+++ apps/antalmanac/src/lib/locations/catalogueBuilder.ts
@@ -1,7 +1,17 @@
 import fs from 'fs';
 
-import axios from 'axios';
-// https://map.uci.edu/?id=463#!s/?ct/8424,8309,8311,8392,8405,44392,44393,44394,44395,44396,44397,44398,44400,44401,44402,44538,44537,44399,8396,11907,8400,10486,11906,11889,8310,8312,8393,8394,8397,8398,8399,8404,8407,8408,11891,11892,11899,11900,11902,21318,8406,11908,11935
+interface LocationListData {
+    catId: number;
+    lat: number;
+    lng: number;
+    id: number;
+    name: string;
+}
+
+interface LocationDetailData {
+    mediaUrlTypes: string[];
+    mediaUrls: string[];
+}
 
 interface BuildingLocation {
     name: string;
@@ -10,6 +20,8 @@ interface BuildingLocation {
     imageURLs: string[];
 }
 
+// https://map.uci.edu/?id=463#!s/?ct/8424,8309,8311,8392,8405,44392,44393,44394,44395,44396,44397,44398,44400,44401,44402,44538,44537,44399,8396,11907,8400,10486,11906,11889,8310,8312,8393,8394,8397,8398,8399,8404,8407,8408,11891,11892,11899,11900,11902,21318,8406,11908,11935
+// Select locations from only specific categories (category IDs are sourced from link above)
 const CATEGORIES: Set<number> = new Set([
     8424, 8309, 8311, 8392, 8405, 44392, 44393, 44394, 44395, 44396, 44397, 44398, 44400, 44401, 44402, 44538, 44537,
     44399, 8396, 11907, 8400, 10486, 11906, 11889, 8310, 8312, 8393, 8394, 8397, 8398, 8399, 8404, 8407, 8408, 11891,
@@ -27,8 +39,8 @@ function sleep(ms: number): Promise<number> {
 
 async function fetchLocations() {
     // Hits locations API to get a list of every single location
-    let locations: any = await axios.get<BuildingLocation>(LOCATIONS_LIST_API);
-    locations = locations.data;
+    const locationsListResponse: Response = await fetch(LOCATIONS_LIST_API);
+    const locations: LocationListData[] = await locationsListResponse.json();
 
     // Go through all the locations
     for (const location of locations) {
@@ -40,16 +52,17 @@ async function fetchLocations() {
         // If one of the locations belongs to one of the categories above, and is not a duplicate
         if (CATEGORIES.has(location.catId) && !locationExists) {
             // Hits location data API to get details on location
-            const locationData = (await axios.get(LOCATIONS_DETAIL_API(location.id))).data;
+            const locationsDetailResponse: Response = await fetch(LOCATIONS_DETAIL_API(location.id));
+            const locationData: LocationDetailData = await locationsDetailResponse.json();
 
             const imgUrls: string[] = [];
 
             // Collects image URLs by checking if type is an image
             // { 'mediaUrlTypes': ['image', '...', 'image'], 'mediaUrls': ['xyz.jpg', '...', 'abc.png']}
-            if (locationData['mediaUrlTypes'] !== undefined) {
-                for (const [i, media] of locationData['mediaUrlTypes'].entries()) {
+            if (locationData.mediaUrlTypes !== undefined) {
+                for (const [i, media] of locationData.mediaUrlTypes.entries()) {
                     if (media === 'image') {
-                        imgUrls.push(locationData['mediaUrls'][i]);
+                        imgUrls.push(locationData.mediaUrls[i]);
                     }
                 }
             }
@@ -71,25 +84,31 @@ async function fetchLocations() {
     }
 }
 
-(async () => {
+async function main() {
     await fetchLocations();
-    fs.writeFileSync(
-        'buildingCatalogue.ts',
-        `export interface Building {
-    imageURLs: string[];
-    lat: number;
-    lng: number;
-    name: string;
-}\n\n` +
-            `const buildingCatalogue: Record<string, Building> = ${JSON.stringify(
-                locationsCatalogue,
-                null,
-                4
-            )};\nexport default buildingCatalogue;`
-    );
 
-    fs.writeFileSync(
-        'locations.ts',
-        `const locations: Record<string, number> = ${JSON.stringify(locationIds, null, 4)};\nexport default locations;`
-    );
-})();
+    const buildingInterfaceStr = `
+    export interface Building {
+        imageURLs: string[];
+        lat: number;
+        lng: number;
+        name: string;
+    }\n\n`;
+
+    const buildingCatalogueStr = `
+    const buildingCatalogue: Record<string, Building> = ${JSON.stringify(
+        locationsCatalogue,
+        null,
+        4
+    )};\nexport default buildingCatalogue;`;
+
+    const locationStr = `
+    const locations: Record<string, number> = ${JSON.stringify(locationIds, null, 4)};
+    \nexport default locations;`;
+
+    fs.writeFileSync('buildingCatalogue.ts', buildingInterfaceStr + buildingCatalogueStr);
+
+    fs.writeFileSync('locations.ts', locationStr);
+}
+
+main();
