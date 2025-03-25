d19b0f689bf88b4979143a392ba66a00e1c4c261
Grace Yan <xgraceyan@gmail.com>
2025-03-25 15:08:15 -0700

    Refactor courseRowPage details

--- apps/antalmanac/playwright/tests/test_pages/courseRowPage.tsx
+++ apps/antalmanac/playwright/tests/test_pages/courseRowPage.tsx
@@ -6,23 +6,27 @@ import { getEventFreq } from '../testTools';
 export class CourseRowPage {
     private courseRow: Locator;
     private courseRowInfo: Locator;
-    private courseTime: string;
-    private courseDayTime: string;
-    private courseLoc: string;
-    private courseCode: string;
-    private courseUnits: string;
-    private courseFreq: number;
+    private courseDetails: {
+        time: string;
+        dayTime: string;
+        location: string;
+        code: string;
+        units: string;
+        frequency: number;
+    };
 
     constructor(public readonly page: Page) {
         this.page = page;
         this.courseRow = this.page.getByTestId('class-table-row').nth(0);
         this.courseRowInfo = this.courseRow.locator('td');
-        this.courseTime = '';
-        this.courseDayTime = '';
-        this.courseLoc = '';
-        this.courseCode = '';
-        this.courseUnits = '';
-        this.courseFreq = 0;
+        this.courseDetails = {
+            time: '',
+            dayTime: '',
+            location: '',
+            code: '',
+            units: '',
+            frequency: 0,
+        };
     }
 
     async verifyCourseRowInfoCount() {
@@ -34,26 +38,26 @@ export class CourseRowPage {
     async initCourseRowFreqTime() {
         const dayTime = await this.courseRowInfo.nth(5).allInnerTexts();
         const timeInfo = dayTime[0].split(' ');
-        this.courseDayTime = dayTime[0]; // '[days] [time]'
-        this.courseTime = timeInfo.slice(1).join(' ');
-        this.courseFreq = getEventFreq(timeInfo[0]);
+        this.courseDetails.dayTime = dayTime[0]; // '[days] [time]'
+        this.courseDetails.time = timeInfo.slice(1).join(' ');
+        this.courseDetails.frequency = getEventFreq(timeInfo[0]);
     }
 
     async initCourseRowLoc() {
-        this.courseLoc = (await this.courseRowInfo.nth(6).allInnerTexts()).join('');
+        this.courseDetails.location = (await this.courseRowInfo.nth(6).allInnerTexts()).join('');
     }
 
     async initCourseRowCode() {
         const courseCodeContainer = await this.page.locator("div[aria-label='Click to copy course code']").first();
         await expect(courseCodeContainer).toBeVisible();
-        this.courseCode = (await courseCodeContainer.allInnerTexts()).join('');
+        this.courseDetails.code = (await courseCodeContainer.allInnerTexts()).join('');
     }
 
     async initCourseRowUnits() {
         const courseType = await this.courseRowInfo.nth(2);
         const courseUnitRow = await courseType.locator('div').nth(2);
         const courseUnits = (await courseUnitRow.allInnerTexts()).join('');
-        this.courseUnits = courseUnits.replace('Units: ', '');
+        this.courseDetails.units = courseUnits.replace('Units: ', '');
     }
 
     async initCourseRow() {
@@ -65,26 +69,26 @@ export class CourseRowPage {
     }
 
     getCourseTime() {
-        return this.courseTime;
+        return this.courseDetails.time;
     }
 
     getCourseDayTime() {
-        return this.courseDayTime;
+        return this.courseDetails.dayTime;
     }
 
     getCourseLoc() {
-        return this.courseLoc;
+        return this.courseDetails.location;
     }
 
     getCourseCode() {
-        return this.courseCode;
+        return this.courseDetails.code;
     }
 
     getCourseUnits() {
-        return this.courseUnits;
+        return this.courseDetails.units;
     }
 
     getCourseFreq() {
-        return this.courseFreq;
+        return this.courseDetails.frequency;
     }
 }
