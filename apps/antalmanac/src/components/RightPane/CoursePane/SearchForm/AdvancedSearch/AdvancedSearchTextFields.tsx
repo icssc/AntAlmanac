4f8c456c30c21b3ea68afdfe780aa4a7085a73cd
Kevin Wu <kevinwu098@gmail.com>
2025-04-21 15:11:48 -0700

    style: v5

--- apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AdvancedSearch/AdvancedSearchTextFields.tsx
+++ apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AdvancedSearch/AdvancedSearchTextFields.tsx
@@ -1,6 +1,15 @@
-import { TextField, Box, FormControl, InputLabel, Select, Switch, FormControlLabel } from '@material-ui/core';
-import { MenuItem } from '@mui/material';
-import { useState, useEffect, useCallback } from 'react';
+import {
+    MenuItem,
+    TextField,
+    Box,
+    FormControl,
+    InputLabel,
+    Select,
+    Switch,
+    FormControlLabel,
+    SelectChangeEvent,
+} from '@mui/material';
+import { useState, useEffect, useCallback, ChangeEvent } from 'react';
 
 import {
     EXCLUDE_RESTRICTION_CODES_OPTIONS,
@@ -46,17 +55,18 @@ export function AdvancedSearchTextFields() {
     const handleChange =
         (name: string) =>
         (
-            event: React.ChangeEvent<
-                HTMLInputElement | HTMLTextAreaElement | { name?: string | undefined; value: unknown }
-            >
+            event:
+                | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+                | SelectChangeEvent<string | string[]>
+                | { target: { checked: boolean } }
         ) => {
             const stateObj = { url: 'url' };
             const url = new URL(window.location.href);
             const urlParam = new URLSearchParams(url.search);
-            const value = event.target.value as string | string[];
 
             if (name === 'online') {
-                if (event.target instanceof HTMLInputElement && event.target.checked) {
+                const checked = (event as { target: { checked: boolean } }).target.checked;
+                if (checked) {
                     setBuilding('ON');
                     setRoom('LINE');
                     RightPaneStore.updateFormValue('building', 'ON');
@@ -71,57 +81,61 @@ export function AdvancedSearchTextFields() {
                     urlParam.delete('building');
                     urlParam.delete('room');
                 }
+                return;
+            }
+
+            const value = (
+                event as SelectChangeEvent<string | string[]> | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ).target.value;
+            const stringValue = Array.isArray(value) ? value.join('') : value;
+
+            switch (name) {
+                case 'instructor':
+                    setInstructor(stringValue);
+                    break;
+                case 'units':
+                    setUnits(stringValue);
+                    break;
+                case 'endTime':
+                    setEndTime(stringValue);
+                    break;
+                case 'startTime':
+                    setStartTime(stringValue);
+                    break;
+                case 'coursesFull':
+                    setCoursesFull(stringValue);
+                    break;
+                case 'building':
+                    setBuilding(stringValue);
+                    break;
+                case 'room':
+                    setRoom(stringValue);
+                    break;
+                case 'division':
+                    setDivision(stringValue);
+                    break;
+                case 'excludeRestrictionCodes':
+                    setExcludeRestrictionCodes(stringValue);
+                    break;
+                case 'days': {
+                    setDays(stringValue);
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            if (stringValue !== '') {
+                urlParam.set(name, String(stringValue));
             } else {
-                const stringValue = Array.isArray(value) ? value.join('') : value;
-
-                switch (name) {
-                    case 'instructor':
-                        setInstructor(stringValue);
-                        break;
-                    case 'units':
-                        setUnits(stringValue);
-                        break;
-                    case 'endTime':
-                        setEndTime(stringValue);
-                        break;
-                    case 'startTime':
-                        setStartTime(stringValue);
-                        break;
-                    case 'coursesFull':
-                        setCoursesFull(stringValue);
-                        break;
-                    case 'building':
-                        setBuilding(stringValue);
-                        break;
-                    case 'room':
-                        setRoom(stringValue);
-                        break;
-                    case 'division':
-                        setDivision(stringValue);
-                        break;
-                    case 'excludeRestrictionCodes':
-                        setExcludeRestrictionCodes(stringValue);
-                        break;
-                    case 'days': {
-                        setDays(stringValue);
-                        break;
-                    }
-                    default:
-                        break;
-                }
-
-                if (stringValue !== '') {
-                    urlParam.set(name, String(stringValue));
-                } else {
-                    urlParam.delete(name);
-                }
-
-                RightPaneStore.updateFormValue(name, stringValue);
+                urlParam.delete(name);
             }
 
             const param = urlParam.toString();
             const newUrl = `${param.trim() ? '?' : ''}${param}`;
             history.replaceState(stateObj, 'url', '/' + newUrl);
+
+            RightPaneStore.updateFormValue(name, stringValue);
         };
 
     // List of times from 2:00am-11:00pm
