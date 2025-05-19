eff0da5ee2748dc63ea32c871bbe56d5333a05a5
Jay Wu <jayqw@uci.edu>
2025-05-19 02:57:47 -0700

    fix: add alias

--- apps/backend/scripts/build.mjs
+++ apps/backend/scripts/build.mjs
@@ -1,15 +1,26 @@
-import { build } from 'esbuild'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { build } from 'esbuild';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function main() {
-  await build({
-    bundle: true,
-    minify: true,
-    platform: 'node',
-    outdir: 'dist',
-    entryPoints: {
-      lambda: 'src/lambda.ts',
-    }
-  })
+    await build({
+        bundle: true,
+        minify: true,
+        platform: 'node',
+        outdir: 'dist',
+        entryPoints: {
+            lambda: 'src/lambda.ts',
+        },
+        alias: {
+            $aa: join(__dirname, '../../antalmanac'),
+            $db: join(__dirname, '../src/db'),
+            $generated: join(__dirname, '../src/generated'),
+            $lib: join(__dirname, '../src/lib'),
+            $models: join(__dirname, '../src/models'),
+        },
+    });
 }
 
-main()
+main();
