138b56be5c388fc03531bb46cc5d6bf34e52ea11
Kevin Wu <kevinwu098@gmail.com>
2025-05-18 18:23:38 -0700

    fix: misc

--- apps/antalmanac/src/components/Calendar/CourseCalendarEvent.tsx
+++ apps/antalmanac/src/components/Calendar/CourseCalendarEvent.tsx
@@ -136,7 +136,7 @@ export const CourseCalendarEvent = ({ selectedEvent, scheduleNames, closePopover
                     }}
                 >
                     <Tooltip title="Quick Search">
-                        <Button size="small" onClick={handleQuickSearch} color="secondary">
+                        <Button size="small" onClick={handleQuickSearch}>
                             <Search fontSize="small" style={{ marginRight: 5 }} />
                             <span style={{ fontSize: '0.9rem', fontWeight: 500 }}>{`${title} ${sectionType}`}</span>
                         </Button>
