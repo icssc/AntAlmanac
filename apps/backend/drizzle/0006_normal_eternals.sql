63c9cbdee87e31aaa2c2f273951fe89063ace9e2
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-05-20 15:22:39 -0700

    chore(drizzle): generate new migration

---
+++ apps/backend/drizzle/0006_normal_eternals.sql
@@ -0,0 +1,12 @@
+ALTER TABLE "subscriptions" RENAME COLUMN "status" TO "lastUpdated";--> statement-breakpoint
+ALTER TABLE "subscriptions" DROP CONSTRAINT "subscriptions_userId_sectionCode_pk";--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "sectionCode" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_userId_sectionCode_year_quarter_pk" PRIMARY KEY("userId","sectionCode","year","quarter");--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "year" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "quarter" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "lastCodes" text DEFAULT '';--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "openStatus" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "waitlistStatus" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "fullStatus" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "subscriptions" ADD COLUMN "restrictionStatus" boolean DEFAULT false;--> statement-breakpoint
+DROP TYPE "public"."subscription_target_status";
