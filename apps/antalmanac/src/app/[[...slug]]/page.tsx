e1e63d24ff22603788bc4f05d80afa1a4ba411ba
Kevin Wu <kevinwu098@gmail.com>
2025-07-30 22:01:53 -0700

    feat: generateStaticParams

--- apps/antalmanac/src/app/[[...slug]]/page.tsx
+++ apps/antalmanac/src/app/[[...slug]]/page.tsx
@@ -1,7 +1,7 @@
 import { ClientOnly } from './client';
 
 export function generateStaticParams() {
-    return [{ slug: [''] }];
+    return [{ slug: [] }, { slug: ['added'] }, { slug: ['map'] }];
 }
 
 export default function Page() {
