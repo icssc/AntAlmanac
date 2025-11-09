78b17354aaed1f3d76de6fc418989eca408ae576
vicksey <vickyzhang@hotmail.ca>
2025-11-09 12:51:54 -0800

    auto instrumentation library

--- apps/antalmanac/src/lib/telemetry/browser.ts
+++ apps/antalmanac/src/lib/telemetry/browser.ts
@@ -1,9 +1,9 @@
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import { ZoneContextManager } from '@opentelemetry/context-zone';
 
 const exporter = new OTLPTraceExporter({
   url: 'http://localhost:4318/v1/traces',
@@ -20,5 +20,12 @@ provider.register({
 
 // Registering instrumentations
 registerInstrumentations({
-  instrumentations: [new DocumentLoadInstrumentation()],
+  instrumentations: [getWebAutoInstrumentations({
+    '@opentelemetry/instrumentation-fetch': {
+        propagateTraceHeaderCorsUrls: /.*/, // add AAPI
+      },
+      '@opentelemetry/instrumentation-xml-http-request': { enabled: true },
+      '@opentelemetry/instrumentation-user-interaction': { enabled: true },
+      '@opentelemetry/instrumentation-document-load': { enabled: true },
+  })],
 });
