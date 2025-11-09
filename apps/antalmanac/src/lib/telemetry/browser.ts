1a81311666e85b74561a1696f421ba9e24e1fbb5
vicksey <vickyzhang@hotmail.ca>
2025-11-09 10:17:35 -0800

    init browser otel

---
+++ apps/antalmanac/src/lib/telemetry/browser.ts
@@ -0,0 +1,24 @@
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+
+const exporter = new OTLPTraceExporter({
+  url: 'http://localhost:8081/v1/traces',
+});
+
+const provider = new WebTracerProvider({
+  spanProcessors: [new BatchSpanProcessor(exporter)],
+});
+
+provider.register({
+  // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
+  contextManager: new ZoneContextManager(),
+});
+
+// Registering instrumentations
+registerInstrumentations({
+  instrumentations: [new DocumentLoadInstrumentation()],
+});
