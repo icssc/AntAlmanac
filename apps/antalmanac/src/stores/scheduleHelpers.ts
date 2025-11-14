680a6864e7b4da36fb8a000ee8f11d903f9a7bd9
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2025-11-13 17:11:13 -0800

    add more colors to color variants

--- apps/antalmanac/src/stores/scheduleHelpers.ts
+++ apps/antalmanac/src/stores/scheduleHelpers.ts
@@ -18,13 +18,13 @@ import {
 import { ScheduleCourse } from '@packages/antalmanac-types';
 
 const colorVariants: Record<string, string[]> = {
-    blue: [blue[300], blue[200], blue[100]],
-    pink: [pink[300], pink[200], pink[100]],
-    purple: [purple[300], purple[200], purple[100]],
-    green: [green[300], green[200], green[100]],
-    amber: [amber[300], amber[200], amber[100]],
-    deepPurple: [deepPurple[300], deepPurple[200], deepPurple[100]],
-    deepOrange: [deepOrange[300], deepOrange[200], deepOrange[100]],
+    blue: [blue[300], blue[200], blue[100], blue[400], blue[500]],
+    pink: [pink[300], pink[200], pink[100], pink[400], pink[500]],
+    purple: [purple[300], purple[200], purple[100], purple[400], purple[500]],
+    green: [green[300], green[200], green[100], green[400], green[500]],
+    amber: [amber[300], amber[200], amber[100], amber[400], amber[500]],
+    deepPurple: [deepPurple[300], deepPurple[200], deepPurple[100], deepPurple[400], deepPurple[500]],
+    deepOrange: [deepOrange[300], deepOrange[200], deepOrange[100], deepOrange[400], deepOrange[500]],
 };
 
 export const colorPickerPresetColors = [
