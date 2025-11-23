40b50d1c389ed6de1d5eb2d92977062f3ba367ea
Isaac Nguyen <58119262+IsaacNguyen@users.noreply.github.com>
2025-11-23 14:11:33 -0800

    Added AANTS Backend Routes (#1340)

--- apps/backend/src/lib/rds.ts
+++ apps/backend/src/lib/rds.ts
@@ -1,4 +1,4 @@
-import { ShortCourse, ShortCourseSchedule, User, RepeatingCustomEvent } from '@packages/antalmanac-types';
+import { ShortCourse, ShortCourseSchedule, User, RepeatingCustomEvent, Notification } from '@packages/antalmanac-types';
 import { and, eq, ExtractTablesWithRelations, gt } from 'drizzle-orm';
 import { PgTransaction, PgQueryResultHKT } from 'drizzle-orm/pg-core';
 import type { Database } from '$db/index';
@@ -15,6 +15,7 @@ import {
     sessions,
     Account,
     Session,
+    subscriptions,
 } from '$db/schema';
 import * as schema from '$db/schema';
 
@@ -259,6 +260,8 @@ export class RDS {
      * deduplicating by section code and term.
      * */
     private static async upsertCourses(tx: Transaction, scheduleId: string, courses: ShortCourse[]) {
+        await tx.delete(coursesInSchedule).where(eq(coursesInSchedule.scheduleId, scheduleId));
+
         if (courses.length === 0) {
             return;
         }
@@ -607,5 +610,120 @@ export class RDS {
         } catch (error) {
             return false;
         }
+    }
+
+    /**
+     * Retrieves notifications associated with a specified user
+     *
+     * @param db - The database or transaction object to use for the operation.
+     * @param userId - The ID of the user for whom we're retrieving notifications.
+     * @returns A promise that resolves to the notifications associated with a userId, or an empty array if not found.
+     */
+    static async retrieveNotifications(db: DatabaseOrTransaction, userId: string) {
+        return db.transaction((tx) =>
+            tx.select().from(subscriptions).where(eq(subscriptions.userId, userId))
+        );
+    }
+
+    /**
+     * Upserts notification for a specified user
+     *
+     * @param db - The database or transaction object to use for the operation.
+     * @param userId - The ID of the user for whom we're upserting a notification.
+     * @param notification - The notification object to upsert.
+     * @returns A promise that upserts the notification associated with a userId.
+     */
+    static async upsertNotification(db: DatabaseOrTransaction, userId: string, notification: Notification) {
+        return db.transaction((tx) =>
+            tx
+                .insert(subscriptions)
+                .values({
+                    userId,
+                    sectionCode: Number(notification.sectionCode),
+                    year: notification.term.split(' ')[0],
+                    quarter: notification.term.split(' ')[1],
+                    openStatus: notification.notificationStatus.openStatus,
+                    waitlistStatus: notification.notificationStatus.waitlistStatus,
+                    fullStatus: notification.notificationStatus.fullStatus,
+                    restrictionStatus: notification.notificationStatus.restrictionStatus,
+                    lastUpdated: notification.lastUpdated,
+                    lastCodes: notification.lastCodes,
+                })
+                .onConflictDoUpdate({
+                    target: [
+                        subscriptions.userId,
+                        subscriptions.year,
+                        subscriptions.quarter,
+                        subscriptions.sectionCode,
+                    ],
+                    set: {
+                        openStatus: notification.notificationStatus.openStatus,
+                        waitlistStatus: notification.notificationStatus.waitlistStatus,
+                        fullStatus: notification.notificationStatus.fullStatus,
+                        restrictionStatus: notification.notificationStatus.restrictionStatus,
+                        lastUpdated: notification.lastUpdated,
+                        lastCodes: notification.lastCodes,
+                    },
+                })
+        );
+    }
+
+    /**
+     * Updates lastUpdated and lastCodes of ALL notifications with a shared sectionCode, year, and quarter
+     *
+     * @param db - The database or transaction object to use for the operation.
+     * @param notification - The notification object type we are updating.
+     * @returns A promise that updates ALL notifications with a shared sectionCode, year, and quarter.
+     */
+    static async updateAllNotifications(db: DatabaseOrTransaction, notification: Notification) {
+        return db.transaction((tx) =>
+            tx
+                .update(subscriptions)
+                .set({
+                    lastUpdated: notification.lastUpdated,
+                    lastCodes: notification.lastCodes,
+                })
+                .where(
+                    and(
+                        eq(subscriptions.sectionCode, Number(notification.sectionCode)),
+                        eq(subscriptions.year, notification.term.split(' ')[0]),
+                        eq(subscriptions.quarter, notification.term.split(' ')[1])
+                    )
+                )
+        );
+    }
+
+    /**
+     * Deletes a notification for a specified user
+     *
+     * @param db - The database or transaction object to use for the operation.
+     * @param notification - The notification object type we are deleting.
+     * @param userId - The ID of the user for whom we're deleting a notification.
+     * @returns A promise that deletes a user's notification.
+     */
+    static async deleteNotification(db: DatabaseOrTransaction, notification: Notification, userId: string) {
+        return db.transaction((tx) =>
+            tx
+                .delete(subscriptions)
+                .where(
+                    and(
+                        eq(subscriptions.userId, userId),
+                        eq(subscriptions.sectionCode, Number(notification.sectionCode)),
+                        eq(subscriptions.year, notification.term.split(' ')[0]),
+                        eq(subscriptions.quarter, notification.term.split(' ')[1])
+                    )
+                )
+        );
+    }
+
+    /**
+     * Deletes ALL notifications for a specified user
+     *
+     * @param db - The database or transaction object to use for the operation.
+     * @param userId - The ID of the user for whom we're deleting all notifications.
+     * @returns A promise that deletes all of a user's notifications.
+     */
+    static async deleteAllNotifications(db: DatabaseOrTransaction, userId: string) {
+        return db.transaction((tx) => tx.delete(subscriptions).where(eq(subscriptions.userId, userId)));
     }
 }
