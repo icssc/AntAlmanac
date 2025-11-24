4c7ba113ff144baf09b6b8d24fdb2b1408d0df3f
Grace Yan <xgraceyan@gmail.com>
2025-11-23 21:20:07 -0800

    Basic manifest setup

--- apps/antalmanac/next.config.mjs
+++ apps/antalmanac/next.config.mjs
@@ -1,4 +1,12 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    //swSrc: 'pwa/sw.js',
+    disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
