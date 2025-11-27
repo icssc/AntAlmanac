16c3e9a00c1a77dc52e14dccb4a1e8cb07e49b4b
Isaac Nguyen <58119262+IsaacNguyen@users.noreply.github.com>
2025-11-26 21:32:14 -0800

    AANTS CDK Changes (#1341)

--- apps/cdk/src/app/backend-staging.ts
+++ apps/cdk/src/app/backend-staging.ts
@@ -1,20 +1,23 @@
 import { App } from 'aws-cdk-lib';
 
-import { BackendStack } from '../stacks/backend';
 import { waitForStackIdle } from '../lib/wait-for-stack-idle';
+import { AantsStack } from '../stacks/aants';
+import { BackendStack } from '../stacks/backend';
 
 async function main() {
     if (!process.env['PR_NUM']) {
         throw new Error('Staging deployment missing PR_NUM environment variable');
     }
 
-    const stackName = `antalmanac-backend-staging-${process.env['PR_NUM']}`;
+    const backendStackName = `antalmanac-backend-staging-${process.env['PR_NUM']}`;
+    const aantsStackName = `antalmanac-aants-staging-${process.env['PR_NUM']}`;
 
-    await waitForStackIdle(stackName);
+    await Promise.all([waitForStackIdle(backendStackName), waitForStackIdle(aantsStackName)]);
 
     const app = new App({ autoSynth: true });
 
-    new BackendStack(app, stackName, { env: { region: 'us-east-1' } });
+    new BackendStack(app, backendStackName, { env: { region: 'us-east-1' } });
+    new AantsStack(app, aantsStackName, { env: { region: 'us-east-1' } });
 }
 
 main();
