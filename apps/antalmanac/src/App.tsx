02be59012cda9809aae7e3f4b9e89343221088c6
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-11-28 15:01:25 -0800

    Fix: Mobile UI Start (#1339)

--- apps/antalmanac/src/App.tsx
+++ apps/antalmanac/src/App.tsx
@@ -120,7 +120,7 @@ export default function App() {
                             }),
                         }}
                     >
-                        <SnackbarProvider>
+                        <SnackbarProvider classes={{ containerRoot: 'notification-snackbar-container' }}>
                             <RouterProvider router={ROUTER} />
                         </SnackbarProvider>
                     </TourProvider>
