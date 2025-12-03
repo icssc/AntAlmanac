4c32af0aa0cf74ecf3f298812ab1a54ef0441e4f
Grace Yan <xgraceyan@gmail.com>
2025-12-03 01:44:17 -0800

    Update next.config.mjs

--- apps/antalmanac/next.config.mjs
+++ apps/antalmanac/next.config.mjs
@@ -7,6 +7,5 @@ export default withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    //swSrc: 'pwa/sw.js',
     disable: process.env.NODE_ENV === 'development',
 })(nextConfig);
