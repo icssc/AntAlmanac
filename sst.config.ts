ebb09a753f7b65f7e3e8ee51615f2a5a04a27b44
Andrew Wang <awwang3@uci.edu>
2025-12-07 02:14:38 -0800

    chore: update ci

--- sst.config.ts
+++ sst.config.ts
@@ -34,7 +34,7 @@ export default $config({
                 ANTEATER_API_KEY: process.env.ANTEATER_API_KEY,
                 GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
                 GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-                GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
+                GOOGLE_REDIRECT_URI: `https://${domain}/auth"`,
                 NEXT_PUBLIC_BASE_URL: domain,
             },
         });
