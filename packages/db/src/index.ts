2563d5d3e7fbc29bcf7cc88c935218040a1c05e3
Kevin Wu <kevinwu098@gmail.com>
2025-12-07 02:21:37 -0800

    fix: db url

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,4 +3,4 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 
 import * as schema from './schema/index';
 
-export const db = drizzle(process.env.DATABASE_URL as string, { schema });
+export const db = drizzle(process.env.DB_URL as string, { schema });
