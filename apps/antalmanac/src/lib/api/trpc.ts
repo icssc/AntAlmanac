4179c271f17f2a3f6eab6611203490b3d18d1751
Andrew Wang <awwang3@uci.edu>
2025-12-07 01:48:25 -0800

    chore: update ci

--- apps/antalmanac/src/lib/api/trpc.ts
+++ apps/antalmanac/src/lib/api/trpc.ts
@@ -4,13 +4,7 @@ import superjson from 'superjson';
 import { AppRouter } from '$src/backend/routers';
 
 function getEndpoint() {
-    if (process.env.NEXT_PUBLIC_ENDPOINT) {
-        return `https://${process.env.NEXT_PUBLIC_ENDPOINT}.api.antalmanac.com`;
-    }
-    if (process.env.NEXT_PUBLIC_LOCAL_SERVER) {
-        return `http://localhost:3000`;
-    }
-    return process.env.NODE_ENV === 'development' ? `https://dev.api.antalmanac.com` : `https://api.antalmanac.com`;
+    return process.env.NEXT_PUBLIC_BASE_URL ?? `http://localhost:3000`;
 }
 
 const trpc = createTRPCProxyClient<AppRouter>({
--- sst.config.ts
+++ sst.config.ts
@@ -35,6 +35,7 @@ export default $config({
                 GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
                 GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
                 GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
+                NEXT_PUBLIC_BASE_URL: domain,
             },
         });
     },
