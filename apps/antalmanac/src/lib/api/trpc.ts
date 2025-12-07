d502d5ce571ffd0baae12e3f186abbf045b4f779
Andrew Wang <awwang3@uci.edu>
2025-12-07 01:53:42 -0800

    chore: update ci

--- apps/antalmanac/src/lib/api/trpc.ts
+++ apps/antalmanac/src/lib/api/trpc.ts
@@ -3,14 +3,10 @@ import superjson from 'superjson';
 
 import { AppRouter } from '$src/backend/routers';
 
-function getEndpoint() {
-    return process.env.NEXT_PUBLIC_BASE_URL ?? `http://localhost:3000`;
-}
-
 const trpc = createTRPCProxyClient<AppRouter>({
     links: [
         httpBatchLink({
-            url: getEndpoint() + '/api/trpc',
+            url: '/api/trpc',
         }),
     ],
     transformer: superjson,
