37deeceed12dab865d4518a77a045d4004ae63c4
bruhkookie <calebwongtech@gmail.com>
2025-12-06 22:08:16 -0800

    get rid of unnecessary comments

--- apps/antalmanac/src/components/RightPane/AddedCourses/getMissingSections.ts
+++ apps/antalmanac/src/components/RightPane/AddedCourses/getMissingSections.ts
@@ -1,11 +1,8 @@
 import { CourseWithTerm } from './AddedCoursePane';
 
 export const getMissingSections = (userCourses: CourseWithTerm): string[] => {
-    //Get required types from stored section types
     const requiredTypes = userCourses.sectionTypes;
-    //Get the section types the user has added
     const userTypes = new Set(userCourses.sections.map((section) => section.sectionType));
-    //Compare types the user added with the required types
     const missingTypes = [...requiredTypes].filter((type) => !userTypes.has(type));
     const missingSections = missingTypes.map((section) => {
         switch (section) {
