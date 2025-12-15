b5aa03e17a76c15f1557331bd3df341a85e62ca9
bruhkookie <calebwongtech@gmail.com>
2025-12-15 15:18:24 -0800

    add guard to prevent crashes if user course data is not yet enriched

--- apps/antalmanac/src/components/RightPane/AddedCourses/getMissingSections.ts
+++ apps/antalmanac/src/components/RightPane/AddedCourses/getMissingSections.ts
@@ -1,7 +1,18 @@
+import { WebsocSectionType } from '@packages/antalmanac-types';
+
 import { CourseWithTerm } from '$components/RightPane/AddedCourses/AddedCoursePane';
 
 export const getMissingSections = (userCourses: CourseWithTerm): string[] => {
-    const requiredTypes = userCourses.sectionTypes;
+    //This will cause an error if a user has data that is not yet enriched because they fetched it before the feature was released
+    const requiredTypes = userCourses.sectionTypes ?? new Set<WebsocSectionType>();
+
+    // If no required types, no sections can be missing
+    if (requiredTypes.size === 0) {
+        console.log('required types for ' + userCourses.courseTitle + ': ', requiredTypes);
+        console.log('protected!');
+        return [];
+    }
+
     const userTypes = new Set(userCourses.sections.map((section) => section.sectionType));
     const missingTypes = [...requiredTypes].filter((type) => !userTypes.has(type));
     const missingSections = missingTypes.map((section) => {
--- packages/types/src/websoc.ts
+++ packages/types/src/websoc.ts
@@ -8,7 +8,7 @@ export type AASection = WebsocSection & AASectionExtendedProperties;
 
 type AACourseExtendedProperties = {
     sections: AASection[];
-    sectionTypes: Set<WebsocSectionType>;
+    sectionTypes?: Set<WebsocSectionType>;
 };
 
 export type AACourse = Omit<WebsocCourse, 'sections'> & AACourseExtendedProperties;
