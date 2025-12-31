ea2395cede46fbdf79d47b83154ee20702b686a2
Alex Espejo <aespejo102@gmail.com>
2025-12-30 18:02:36 -0800

    revert test commit

--- apps/antalmanac/src/components/Header/Signin.tsx
+++ apps/antalmanac/src/components/Header/Signin.tsx
@@ -307,7 +307,7 @@ export const Signin = () => {
                     onClick={handleLogin}
                     size="large"
                 >
-                    Sign in with Google test lint
+                    Sign in with Google
                 </LoadingButton>
             </AlertDialog>
         </div>
