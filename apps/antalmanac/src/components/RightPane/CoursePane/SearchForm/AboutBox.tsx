328f42c696a6209162ccf4bdb0ad70adcab0bad8
Grace Yan <xgraceyan@gmail.com>
2026-01-03 15:25:34 -0800

    Convert to collapse

--- apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AboutBox.tsx
+++ apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AboutBox.tsx
@@ -1,5 +1,14 @@
-import { Close } from '@mui/icons-material';
-import { Paper, Typography, Box, IconButton, Card, CardMedia, CardContent, Stack } from '@mui/material';
+import { ExpandMore } from '@mui/icons-material';
+import {
+    Typography,
+    Card,
+    CardMedia,
+    CardContent,
+    Stack,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+} from '@mui/material';
 import { grey } from '@mui/material/colors';
 import Link from 'next/link';
 import { useCallback } from 'react';
@@ -8,22 +17,21 @@ import { useShallow } from 'zustand/react/shallow';
 import FeedbackButton from '$components/buttons/Feedback';
 import GitHubButton from '$components/buttons/GitHub';
 import ProjectsButton from '$components/buttons/Projects';
-import { setLocalStorageAboutBoxDismissalTime } from '$lib/localStorage';
+import { setLocalStorageAboutBoxCollapseTime, setLocalStorageExpandAboutBox } from '$lib/localStorage';
 import { useHelpMenuStore } from '$stores/HelpMenuStore';
 import { useThemeStore } from '$stores/SettingsStore';
 
-// TODO: Replace helpbox images
 const images = [
     {
-        src: '/helpbox1.png',
+        src: '/about-box/aboutbox1.png',
         alt: '1. Enter a department or course name to search',
     },
     {
-        src: '/helpbox2.png',
+        src: '/about-box/aboutbox2.png',
         alt: '2. Add courses to your schedule',
     },
     {
-        src: '/helpbox3.png',
+        src: '/about-box/aboutbox3.png',
         alt: '3. Save your schedule and access from anywhere!',
     },
 ];
@@ -31,64 +39,74 @@ const images = [
 export function AboutBox() {
     const isDark = useThemeStore((store) => store.isDark);
 
-    const [showAboutBox, setShowAboutBox] = useHelpMenuStore(
-        useShallow((store) => [store.showAboutBox, store.setShowAboutBox])
+    const [expandAboutBox, toggleExpandAboutBox] = useHelpMenuStore(
+        useShallow((store) => [store.expandAboutBox, store.toggleExpandAboutBox])
     );
 
-    const dismissAboutBox = useCallback(() => {
-        setLocalStorageAboutBoxDismissalTime(Date.now().toString());
+    const collapseAboutBox = useCallback(() => {
+        setLocalStorageAboutBoxCollapseTime(Date.now().toString());
     }, []);
 
     const handleClick = useCallback(() => {
-        setShowAboutBox(false);
-        dismissAboutBox();
-    }, [dismissAboutBox, setShowAboutBox]);
-
-    if (!showAboutBox) {
-        return null;
-    }
+        if (expandAboutBox) {
+            collapseAboutBox();
+        }
+        setLocalStorageExpandAboutBox(!expandAboutBox ? 'true' : 'false');
+        toggleExpandAboutBox();
+    }, [expandAboutBox, collapseAboutBox, toggleExpandAboutBox]);
 
     return (
-        <Paper variant="outlined" sx={{ padding: 2, marginBottom: '10px', marginRight: '5px' }}>
-            <Stack gap={2}>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <Typography variant="h5" fontWeight="bold">
-                        About AntAlmanac Scheduler
-                    </Typography>
-                    <IconButton aria-label="close" size="large" color="inherit" onClick={handleClick}>
-                        <Close fontSize="inherit" />
-                    </IconButton>
-                </Box>
-                <Stack direction="row" gap={2}>
-                    {images.map((image) => (
-                        <Card sx={{ border: '1px solid #ccc' }} elevation={0} key={image.src}>
-                            <CardMedia
-                                component="img"
-                                image={image.src}
-                                alt={image.alt}
-                                style={{ width: '100%', height: 'auto' }}
-                            />
-                            <CardContent>
-                                <Typography variant="body1" fontWeight="bold">
-                                    {image.alt}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </Stack>
-                <Stack gap={1}>
-                    <Typography variant="body1">
-                        Need help <i>discovering</i> courses? Check out{' '}
-                        <Link href={'/planner'}>AntAlmanac Planner</Link>
-                    </Typography>
+        <Accordion
+            variant="outlined"
+            expanded={expandAboutBox}
+            onChange={handleClick}
+            sx={{
+                padding: 1,
+                paddingRight: 4, // FIX ME: Magic Number padding for the Help Menu
+                marginBottom: '10px',
+                marginRight: '5px',
+            }}
+        >
+            <AccordionSummary expandIcon={<ExpandMore />}>
+                <Typography component="span" variant="h5" fontWeight={500}>
+                    Using AntAlmanac Scheduler
+                </Typography>
+            </AccordionSummary>
 
-                    <Stack direction="row" color={isDark ? 'white' : grey[600]}>
-                        <ProjectsButton />
-                        <GitHubButton />
-                        <FeedbackButton />
+            <AccordionDetails>
+                <Stack gap={2}>
+                    <Stack direction="row" gap={2}>
+                        {images.map((image) => (
+                            <Card key={image.src} elevation={0} sx={{ border: '1px solid #ccc' }}>
+                                <CardMedia
+                                    component="img"
+                                    image={image.src}
+                                    alt={image.alt}
+                                    sx={{ width: '100%', height: 'auto' }}
+                                />
+                                <CardContent>
+                                    <Typography variant="body1" fontWeight="bold">
+                                        {image.alt}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </Stack>
+
+                    <Stack gap={1}>
+                        <Typography variant="body1">
+                            Need help <i>discovering</i> courses? Check out{' '}
+                            <Link href="/planner">AntAlmanac Planner</Link>
+                        </Typography>
+
+                        <Stack direction="row" color={isDark ? 'white' : grey[600]}>
+                            <ProjectsButton />
+                            <GitHubButton />
+                            <FeedbackButton />
+                        </Stack>
                     </Stack>
                 </Stack>
-            </Stack>
-        </Paper>
+            </AccordionDetails>
+        </Accordion>
     );
 }
