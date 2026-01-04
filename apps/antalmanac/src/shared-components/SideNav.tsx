1a71ea0c614a1303ada7218436f19c095090126d
Grace Yan <xgraceyan@gmail.com>
2026-01-03 16:46:15 -0800

    Added side nav

---
+++ apps/antalmanac/src/shared-components/SideNav.tsx
@@ -0,0 +1,50 @@
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import RouteIcon from '@mui/icons-material/Route';
+import { Paper, Stack, SxProps, Typography, useTheme } from '@mui/material';
+import Link from 'next/link';
+
+import { useIsMobile } from '$hooks/useIsMobile';
+import { useThemeStore } from '$stores/SettingsStore';
+
+const SideNav = () => {
+    const isMobile = useIsMobile();
+    const isDark = useThemeStore((store) => store.isDark);
+    const theme = useTheme();
+
+    if (isMobile) {
+        return null;
+    }
+
+    const paperStyleOverrides: SxProps = {
+        height: '100%',
+        width: 64,
+        zIndex: 300,
+        top: 0,
+        left: 0,
+        borderRight: `1px solid ${theme.palette.divider}`,
+        borderRadius: 0,
+        flexShrink: 0,
+    };
+
+    return (
+        <Paper elevation={0} sx={paperStyleOverrides}>
+            <Stack direction="column" alignItems="center" sx={{ gap: '16px', paddingTop: '8px' }}>
+                <Link href="https://antalmanac.com" style={{ textDecoration: 'none', color: 'primary' }}>
+                    <Stack direction="column" alignItems="center">
+                        <EventNoteIcon fontSize="medium" />
+                        <Typography fontSize={11}>Scheduler</Typography>
+                    </Stack>
+                </Link>
+
+                <Link href="/" style={{ textDecoration: 'none' }}>
+                    <Stack direction="column" alignItems="center" sx={{ color: isDark ? 'white' : 'black' }}>
+                        <RouteIcon fontSize="medium" />
+                        <Typography fontSize={11}>Planner</Typography>
+                    </Stack>
+                </Link>
+            </Stack>
+        </Paper>
+    );
+};
+
+export default SideNav;
