dac3719971b18501f3e8ae1313da9c50598dc4a7
bruhkookie <calebwongtech@gmail.com>
2026-01-05 18:41:54 -0800

    get rid of optional sectionType

--- packages/types/src/websoc.ts
+++ packages/types/src/websoc.ts
@@ -8,7 +8,7 @@ export type AASection = WebsocSection & AASectionExtendedProperties;
 
 type AACourseExtendedProperties = {
     sections: AASection[];
-    sectionTypes?: WebsocSectionType[];
+    sectionTypes: WebsocSectionType[];
 };
 
 export type AACourse = Omit<WebsocCourse, 'sections'> & AACourseExtendedProperties;
