4f79a1a33d5676837e0c475b7eb2959e605590aa
Kevin Wu <kevinwu098@gmail.com>
2026-01-22 00:25:36 -0600

    feat: staging shared (#1423)

--- apps/antalmanac/src/globals.ts
+++ apps/antalmanac/src/globals.ts
@@ -4,4 +4,4 @@ export const BLUE = '#305db7';
 
 export const FEEDBACK_LINK = 'https://form.asana.com/?k=fZ3SGnuGknDmzTYdocgIUg&d=1208267282546207';
 
-export const PLANNER_LINK = 'https://peterportal.org';
+export const PLANNER_LINK = '/planner';
