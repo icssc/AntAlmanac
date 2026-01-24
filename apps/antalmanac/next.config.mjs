f84a3300f4aa6907eca570d97fd99f6ab06a7b07
Dominic Seung <dominic@seung.dev>
2026-01-23 22:51:17 -0800

    ci: enforce typecheck/eslint

--- apps/antalmanac/next.config.mjs
+++ apps/antalmanac/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // TODO: Remove these ignores and fix the underlying issues
-    typescript: {
-        ignoreBuildErrors: true,
-    },
     serverExternalPackages: ['@node-rs/argon2'],
 };
 
