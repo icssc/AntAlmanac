5315a1d287c2e038d648c7f07d9b737cda14dc94
Dominic Seung <dominic@seung.dev>
2026-02-07 16:51:43 -0800

    only generate needed term data

--- apps/antalmanac/scripts/get-search-data.ts
+++ apps/antalmanac/scripts/get-search-data.ts
@@ -1,4 +1,4 @@
-import { mkdir, writeFile, stat } from 'node:fs/promises';
+import { access, mkdir, writeFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -12,7 +12,6 @@ import 'dotenv/config';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const MAX_COURSES = 10_000;
-const VALID_CACHE_TIME_DAYS = 14;
 const DELAY_MS = 500; // avoid rate limits from AAPI
 
 const ALIASES: Record<string, string | undefined> = {
@@ -25,21 +24,6 @@ const ALIASES: Record<string, string | undefined> = {
 async function main() {
     const apiKey = process.env.ANTEATER_API_KEY;
     if (!apiKey) throw new Error('ANTEATER_API_KEY is required');
-
-    try {
-        const cacheFolderStatistics = await stat(join(__dirname, '../src/generated/searchData.ts'));
-
-        const lastModifiedDate = cacheFolderStatistics.mtime;
-        const currentDate = new Date();
-        const validCacheMs = VALID_CACHE_TIME_DAYS * 24 * 60 * 60 * 1000;
-
-        if (process.env.STAGE == 'local' && currentDate.getTime() - lastModifiedDate.getTime() < validCacheMs) {
-            console.log('Using existing search cache, last updated ' + lastModifiedDate.toLocaleString() + '.');
-            return;
-        }
-    } catch {
-        console.log('Cache is empty or unreachable, rebuilding from scratch...');
-    }
 
     console.log('Generating cache for fuzzy search.');
     console.log('Fetching courses from Anteater API...');
@@ -96,20 +80,37 @@ async function main() {
     await writeFile(
         join(__dirname, '../src/generated/searchData.ts'),
         `
-    import type { CourseSearchResult, DepartmentSearchResult } from "@packages/antalmanac-types";
-    export const departments: Array<DepartmentSearchResult & { id: string }> = ${JSON.stringify(
-        Array.from(deptMap.values())
-    )};
+import type { CourseSearchResult, DepartmentSearchResult } from "@packages/antalmanac-types";
+export const departments: Array<DepartmentSearchResult & { id: string }> = ${JSON.stringify(
+            Array.from(deptMap.values())
+        )};
     export const courses: Array<CourseSearchResult & { id: string }> = ${JSON.stringify(
         Array.from(courseMap.values())
     )};
-    `
+`
     );
+
+    const currentTerm = termData[0];
     let count = 0;
     const termPromises = termData.map(async (term, index) => {
         try {
             const [year, quarter] = term.shortName.split(' ');
             const parsedTerm = `${quarter}_${year}`;
+            const fileName = join(__dirname, `../src/generated/terms/${parsedTerm}.json`);
+
+            try {
+                await access(fileName);
+
+                if (currentTerm.longName != term.longName) {
+                    console.log(`Skipping ${term.shortName}, cache already exists.`);
+                    return 0;
+                } else {
+                    console.log('Updating current term data...');
+                }
+            } catch {
+                console.log(`${term.shortName} doesn't exist in cache, rebuilding.`);
+            }
+
             const query = QUERY_TEMPLATE.replace('$$YEAR$$', year).replace('$$QUARTER$$', quarter);
 
             // TODO (@kevin): remove delay once AAPI resolves OOM issues
@@ -126,7 +127,6 @@ async function main() {
                 } from Anteater API.`
             );
 
-            const fileName = join(__dirname, `../src/generated/terms/${parsedTerm}.json`);
             await writeFile(fileName, JSON.stringify(parsedSectionData, null, 2));
             return Object.keys(parsedSectionData).length;
         } catch (error) {
