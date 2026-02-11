62a9345016f62254e592b5f13cf886cb09f41eef
Choollol <67297627+Choollol@users.noreply.github.com>
2026-02-10 16:11:12 -0800

    Fix SIgnIn button load logic to be the same as before

--- apps/antalmanac/src/components/Header/Signin.tsx
+++ apps/antalmanac/src/components/Header/Signin.tsx
@@ -111,14 +111,11 @@ export const Signin = () => {
             if (!validSession) {
                 setOpenalert(true);
                 setAlertMessage(ALERT_MESSAGES.SESSION_EXPIRED);
-            } else if (sessionToken) {
-                updateSession(sessionToken);
-                if (!skipScheduleLoad) {
-                    await loadScheduleWithSessionToken();
-                }
-            } else if (sessionToken === '' && userID && userID !== '') {
-                await validateImportedUser(userID);
-                if (!skipScheduleLoad) {
+            } else if (!skipScheduleLoad) {
+                if (sessionToken && (await loadScheduleWithSessionToken())) {
+                    updateSession(sessionToken);
+                } else if (sessionToken === '' && userID && userID !== '') {
+                    await validateImportedUser(userID);
                     await loadSchedule(userID, rememberMe, 'GUEST');
                 }
             }
