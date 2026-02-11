df4afc52ab1db10bcc383c42e19f110905a459bd
vicksey <vickyzhang@hotmail.ca>
2026-02-11 14:17:09 -0800

    Revert "add: prevent url griefing"
    This reverts commit 9176c3684beb4130f7913d3851ab92004e89c7a1.

--- apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AdvancedSearch/AdvancedSearchTextFields.tsx
+++ apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/AdvancedSearch/AdvancedSearchTextFields.tsx
@@ -103,16 +103,6 @@ export function AdvancedSearchTextFields() {
         };
     }, [resetField]);
 
-    useEffect(() => {
-        if (excludeRoadmapCourses && roadmaps.length > 0) {
-            const roadmapExists = roadmaps.some((r) => r.id.toString() === excludeRoadmapCourses);
-            if (!roadmapExists) {
-                setExcludeRoadmapCourses('');
-                updateValue('excludeRoadmapCourses', '');
-            }
-        }
-    }, [excludeRoadmapCourses, roadmaps]);
-
     const updateValue = (name: AdvancedSearchParam, stringValue: string) => {
         const stateObj = { url: 'url' };
         const url = new URL(window.location.href);
