ac4d248e060ac047cc0ccb3ffd0ee12ad038590c
vicksey <vickyzhang@hotmail.ca>
2026-02-18 14:36:24 -0800

    refactor tutorial and unused components

--- apps/antalmanac/src/components/buttons/TutorialButton.tsx
+++ apps/antalmanac/src/components/buttons/TutorialButton.tsx
@@ -1,24 +1,50 @@
 import { PlayLesson } from '@mui/icons-material';
 import { Button } from '@mui/material';
-import { useCallback } from 'react';
+import { useTour } from '@reactour/tour';
+import { useCallback, useEffect } from 'react';
+import { useShallow } from 'zustand/react/shallow';
 
-import { TutorialAction } from '$components/HelpMenu/actions/TutorialAction';
+import { stepsFactory, tourShouldRun } from '$lib/TutorialHelpers';
+import { removeSampleClasses } from '$lib/tourExampleGeneration';
+import { useCoursePaneStore } from '$stores/CoursePaneStore';
 
 interface TutorialButtonProps {
     onMenuClose?: () => void;
 }
 
 export const TutorialButton = ({ onMenuClose }: TutorialButtonProps) => {
-    const action = TutorialAction();
+    const { setCurrentStep, setIsOpen, setSteps, isOpen } = useTour();
+    const [displaySearch, disableManualSearch] = useCoursePaneStore(
+        useShallow((state) => [state.displaySearch, state.disableManualSearch])
+    );
+
+    const startTutorial = useCallback(() => {
+        displaySearch();
+        disableManualSearch();
+        setCurrentStep(0);
+        setIsOpen(true);
+    }, [displaySearch, disableManualSearch, setCurrentStep, setIsOpen]);
+
+    useEffect(() => setIsOpen(tourShouldRun), [setIsOpen]);
+
+    useEffect(() => {
+        return () => {
+            removeSampleClasses();
+        };
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (setSteps == null || setCurrentStep == null) {
+            return;
+        }
+
+        setSteps(stepsFactory(setCurrentStep));
+    }, [setCurrentStep, setSteps]);
 
     const handleClick = useCallback(() => {
         onMenuClose?.();
-        action?.onClick();
-    }, [action, onMenuClose]);
-
-    if (!action) {
-        return null;
-    }
+        startTutorial();
+    }, [startTutorial, onMenuClose]);
 
     return (
         <Button onClick={handleClick} color="inherit" startIcon={<PlayLesson />} size="large" variant="text">
