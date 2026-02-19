87e4eb829835366cb7683c4cc2ec300c23c316e4
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2026-02-19 15:33:53 -0800

    fix: secondarycolor hook

--- apps/antalmanac/src/hooks/useSecondaryColor.ts
+++ apps/antalmanac/src/hooks/useSecondaryColor.ts
@@ -1,11 +1,11 @@
 import { useMemo } from 'react';
 
-import { BLUE } from '$src/globals';
+import { BLUE, LIGHT_BLUE } from '$src/globals';
 import { useThemeStore } from '$stores/SettingsStore';
 
 export function useSecondaryColor() {
     const isDark = useThemeStore((store) => store.isDark);
-    const color = useMemo(() => (isDark ? '#90B3F9' : BLUE), [isDark]);
+    const color = useMemo(() => (isDark ? LIGHT_BLUE : BLUE), [isDark]);
 
     return color;
 }
