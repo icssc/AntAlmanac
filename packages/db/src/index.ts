8cb83b94257c778cb0ff3f1ca4c6f8279a2fe87b
Kevin Wu <kevinwu098@gmail.com>
2026-02-24 09:33:13 -0800

    feat: drop max connections

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -6,7 +6,7 @@ import * as schema from './schema/index';
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    max: 5,
+    max: 3,
 });
 
 export const db = drizzle(pool, { schema });
