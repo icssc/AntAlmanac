98e1097e6f82bc12fe8bf902e6ce624594346716
Alex Espejo <73915224+alexespejo@users.noreply.github.com>
2026-02-26 12:56:35 -0800

    Restore GraphQl in get-data (#1523)

--- apps/antalmanac/src/backend/lib/term-section-codes.ts
+++ apps/antalmanac/src/backend/lib/term-section-codes.ts
@@ -15,33 +15,8 @@ export interface SectionCodesGraphQLResponse {
     };
 }
 
-export interface SectionCodesRESTResponse {
-    data: {
-        schools: WebsocSchool[];
-    };
-}
-
-export function parseSectionCodesREST(response: SectionCodesRESTResponse): Record<string, SectionSearchResult> {
-    const results: Record<string, SectionSearchResult> = {};
-    response.data.schools.forEach((school: WebsocSchool) => {
-        school.departments.forEach((department: WebsocDepartment) => {
-            department.courses.forEach((course: WebsocCourse) => {
-                course.sections.forEach((section: WebsocSection) => {
-                    const sectionCode = section.sectionCode;
-                    results[sectionCode] = {
-                        type: 'SECTION',
-                        department: department.deptCode,
-                        courseNumber: course.courseNumber,
-                        sectionCode: section.sectionCode,
-                        sectionNum: section.sectionNum,
-                        sectionType: section.sectionType,
-                    };
-                });
-            });
-        });
-    });
-
-    return results;
+export interface SectionCodesResponse {
+    schools: WebsocSchool[];
 }
 
 export function parseSectionCodes(response: SectionCodesGraphQLResponse): Record<string, SectionSearchResult> {
