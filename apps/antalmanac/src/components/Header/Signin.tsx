b7b292cedc54bfe263fe07fa12b0fae71a1851f4
Kevin Wu <kevinwu098@gmail.com>
2026-02-27 03:01:54 -0600

    fix: aants initialization (#1535)

--- apps/antalmanac/src/components/Header/Signin.tsx
+++ apps/antalmanac/src/components/Header/Signin.tsx
@@ -27,6 +27,7 @@ import { ProfileMenuButtons } from '$components/Header/ProfileMenuButtons';
 import { SettingsMenu } from '$components/Header/Settings/SettingsMenu';
 import trpc from '$lib/api/trpc';
 import { getLocalStorageSessionId, getLocalStorageUserId, setLocalStorageFromLoading } from '$lib/localStorage';
+import { useNotificationStore } from '$stores/NotificationStore';
 import { scheduleComponentsToggleStore } from '$stores/ScheduleComponentsToggleStore';
 import { useSessionStore } from '$stores/SessionStore';
 import { useThemeStore } from '$stores/SettingsStore';
@@ -181,6 +182,8 @@ export const Signin = () => {
 
             if (savedUserID != null || sessionID !== null) {
                 void loadScheduleAndSetLoadingAuth(savedUserID ?? '', true);
+            } else {
+                useNotificationStore.getState().loadNotifications();
             }
         }
     }, [loadScheduleAndSetLoadingAuth]);
