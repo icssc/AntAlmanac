fb6f638032f478594c44617a5e2bae63b946908c
Anthony Jenanian <55033931+anthonyj33@users.noreply.github.com>
2026-03-04 13:53:31 -0800

    Completion Hint for (Grouped)CourseRequirement (#995)
    * "Pick n" for requirements
    
    Clarifies number of required courses for GroupedCourseRequirement
    
    * Show num completed out of reqd
    
    * CompletionHint for CourseRequirement and GroupedCourseRequirement
    
    Supports courses and units
    (x/y) for courses
    (x/y units) for units

--- site/src/app/roadmap/catalog/ProgramRequirementsList.tsx
+++ site/src/app/roadmap/catalog/ProgramRequirementsList.tsx
@@ -204,13 +204,23 @@ const CourseRequirement: FC<CourseRequirementProps> = ({ data, takenCourseIDs, s
       <Collapse in={open} unmountOnExit>
         {showLabel && (
           <p className="requirement-label">
-            <b>Complete {label} of the following:</b>
+            <b>
+              Complete {label} of the following{CompletionHint(data, takenCourseIDs)}
+            </b>
           </p>
         )}
         <CourseList courses={data.courses} takenCourseIDs={takenCourseIDs} />
       </Collapse>
     </div>
   );
+};
+
+const CompletionHint = (data: ProgramRequirement<'Course' | 'Unit'>, takenCourseIDs: CompletedCourseSet) => {
+  const showCourseCount = data.courses.length > 0 && 'courseCount' in data;
+  const showUnitCount = 'unitCount' in data && data.unitCount > 0;
+  const completedCount = useCompletionCheck(takenCourseIDs, data).completed;
+  if (showCourseCount) return ` • (${completedCount}/${data.courseCount})`;
+  if (showUnitCount) return ` • (${completedCount}/${data.unitCount} units)`;
 };
 
 interface GroupedCourseRequirementProps {
@@ -225,7 +235,10 @@ const GroupedCourseRequirement: FC<GroupedCourseRequirementProps> = ({ data, tak
     <>
       <div className={className}>
         <p className="requirement-label">
-          <b>{data.label}</b>
+          <b>
+            {data.label}
+            {CompletionHint(data, takenCourseIDs)}
+          </b>
         </p>
         <CourseList courses={data.courses} takenCourseIDs={takenCourseIDs} />
       </div>
