f2fc40feb1b19d36b2c89da28aa8f41847382c14
Kevin Wu <kevinwu098@gmail.com>
2026-03-20 04:10:43 -0500

    feat: remove unsaved actions

--- apps/antalmanac/src/actions/ActionTypesStore.ts
+++ apps/antalmanac/src/actions/ActionTypesStore.ts
@@ -4,17 +4,10 @@ import { CustomEventId, RepeatingCustomEvent, ScheduleCourse } from '@packages/a
 
 import { autoSaveSchedule } from '$actions/AppStoreActions';
 import trpc from '$lib/api/trpc';
-import {
-    getLocalStorageAutoSave,
-    getLocalStorageUnsavedActions,
-    removeLocalStorageUnsavedActions,
-    setLocalStorageUnsavedActions,
-} from '$lib/localStorage';
+import { getLocalStorageAutoSave, removeLocalStorageUnsavedActions } from '$lib/localStorage';
 import AppStore from '$stores/AppStore';
 import { scheduleComponentsToggleStore } from '$stores/ScheduleComponentsToggleStore';
 import { useSessionStore } from '$stores/SessionStore';
-
-const MAX_UNSAVED_ACTIONS = 1000;
 
 export interface UndoRedoAction {
     type: 'undoRedoAction';
@@ -113,20 +106,12 @@ export type ActionType =
     | ChangeCourseColorAction
     | UndoRedoAction;
 
-function parseUnsavedActionsString(unsavedActionsString: string): ActionType[] {
-    try {
-        return JSON.parse(unsavedActionsString);
-    } catch {
-        return [];
-    }
-}
-
 class ActionTypesStore extends EventEmitter {
     constructor() {
         super();
     }
 
-    async autoSaveSchedule(action: ActionType) {
+    async autoSaveSchedule(_action: ActionType) {
         const sessionStore = useSessionStore.getState();
         const autoSave = typeof Storage !== 'undefined' && getLocalStorageAutoSave() == 'true';
 
@@ -148,81 +133,11 @@ class ActionTypesStore extends EventEmitter {
                 AppStore.unsavedChanges = false;
                 this.emit('autoSaveEnd');
             }
-        } else {
-            const unsavedActionsString = getLocalStorageUnsavedActions();
-            if (unsavedActionsString == null) {
-                const unsavedActions = [action];
-                setLocalStorageUnsavedActions(JSON.stringify(unsavedActions));
-            } else {
-                let unsavedActions = parseUnsavedActionsString(unsavedActionsString);
-                if (unsavedActions.length > MAX_UNSAVED_ACTIONS) {
-                    unsavedActions = unsavedActions.slice(100);
-                }
-                unsavedActions.push(action);
-                setLocalStorageUnsavedActions(JSON.stringify(unsavedActions));
-            }
         }
     }
 
     async loadScheduleFromUnsavedActions() {
-        const unsavedActionsString = getLocalStorageUnsavedActions();
-        if (unsavedActionsString == null) {
-            return;
-        }
-        if (!confirm('You have unsaved changes. Would you like to load them?')) {
-            removeLocalStorageUnsavedActions();
-            return;
-        }
-
-        const actions = parseUnsavedActionsString(unsavedActionsString);
-
-        actions.forEach((action) => {
-            switch (action.type) {
-                case 'addCourse':
-                    AppStore.schedule.addCourse(action.course, action.scheduleIndex);
-                    break;
-                case 'deleteCourse':
-                    AppStore.schedule.deleteCourse(action.sectionCode, action.term, action.scheduleIndex);
-                    break;
-                case 'addCustomEvent':
-                    AppStore.schedule.addCustomEvent(action.customEvent, action.scheduleIndices);
-                    break;
-                case 'deleteCustomEvent':
-                    AppStore.schedule.deleteCustomEvent(action.customEventId, action.scheduleIndices);
-                    break;
-                case 'editCustomEvent':
-                    AppStore.schedule.editCustomEvent(action.editedCustomEvent, action.newScheduleIndices);
-                    break;
-                case 'changeCustomEventColor':
-                    AppStore.schedule.changeCustomEventColor(action.customEventId, action.newColor);
-                    break;
-                case 'changeCourseColor':
-                    AppStore.schedule.changeCourseColor(action.sectionCode, action.term, action.newColor);
-                    break;
-                case 'clearSchedule':
-                    AppStore.schedule.clearCurrentSchedule();
-                    break;
-                case 'addSchedule':
-                    AppStore.schedule.addNewSchedule(action.newScheduleName);
-                    break;
-                case 'renameSchedule':
-                    AppStore.schedule.renameSchedule(action.scheduleIndex, action.newScheduleName);
-                    break;
-                case 'copySchedule':
-                    AppStore.schedule.copySchedule(action.scheduleIndex, action.newScheduleName);
-                    break;
-                case 'deleteSchedule':
-                    AppStore.schedule.deleteSchedule(action.scheduleIndex);
-                    break;
-                case 'reorderSchedule':
-                    AppStore.schedule.reorderSchedule(action.from, action.to);
-                    break;
-                case 'undoRedoAction':
-                    break;
-                default:
-                    break;
-            }
-        });
+        removeLocalStorageUnsavedActions();
     }
 }
 
