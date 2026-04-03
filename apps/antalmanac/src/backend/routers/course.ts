1eda0f4462a541c5c0d8127da765358892c7dc59
Choollol <67297627+Choollol@users.noreply.github.com>
2026-04-02 19:44:26 -0700

    Use fetchAnteaterAPIData in course.ts

--- apps/antalmanac/src/backend/routers/course.ts
+++ apps/antalmanac/src/backend/routers/course.ts
@@ -1,17 +1,16 @@
-import type { Course } from '@packages/antalmanac-types';
+import type { CourseByIdAPIResult } from '@packages/antalmanac-types';
 import { z } from 'zod';
 
 import { procedure, router } from '../trpc';
 
+import { fetchAnteaterAPIData } from '$src/backend/lib/helpers';
+
 const courseRouter = router({
     get: procedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-        return await fetch(`https://anteaterapi.com/v2/rest/courses/${encodeURIComponent(input.id)}`, {
-            headers: {
-                ...(process.env.ANTEATER_API_KEY && { Authorization: `Bearer ${process.env.ANTEATER_API_KEY}` }),
-            },
-        })
-            .then((data) => data.json())
-            .then((data) => (data.ok ? (data.data as Course) : null));
+        const data = await fetchAnteaterAPIData<CourseByIdAPIResult>(
+            `https://anteaterapi.com/v2/rest/courses/${encodeURIComponent(input.id)}`
+        );
+        return data.ok ? data.data : null;
     }),
 });
 
--- packages/anteater-api-types/src/courses.ts
+++ packages/anteater-api-types/src/courses.ts
@@ -1,6 +1,9 @@
 import { components, paths } from './generated/anteater-api-types';
 
-export type Course = paths['/v2/rest/courses/{id}']['get']['responses'][200]['content']['application/json']['data'];
+export type CourseByIdAPIResult =
+    paths['/v2/rest/courses/{id}']['get']['responses'][200]['content']['application/json'];
+
+export type Course = CourseByIdAPIResult['data'];
 
 export type Prerequisite = components['schemas']['prereq'];
 
