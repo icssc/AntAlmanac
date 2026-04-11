3a67c02ece1d4a1fe00397e31deda821ace6108b
Choollol <67297627+Choollol@users.noreply.github.com>
2026-04-10 21:32:46 -0700

    Add nextCookies plugin

--- apps/antalmanac/src/lib/auth/authClient.ts
+++ apps/antalmanac/src/lib/auth/authClient.ts
@@ -1,6 +1,7 @@
 import { genericOAuthClient } from 'better-auth/client/plugins';
+import { nextCookies } from 'better-auth/next-js';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-    plugins: [genericOAuthClient()],
+    plugins: [genericOAuthClient(), nextCookies()],
 });
