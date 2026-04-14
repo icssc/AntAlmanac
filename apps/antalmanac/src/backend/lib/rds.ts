39a3b8cb170a41d3860f2e281f044417a02e5e84
Kevin Wu <kevinwu098@gmail.com>
2026-04-14 17:51:18 +0900

    fix: use transaction object (tx) instead of root db client in upsertUserData (#1609)

--- apps/antalmanac/src/backend/lib/rds.ts
+++ apps/antalmanac/src/backend/lib/rds.ts
@@ -232,7 +232,7 @@ export class RDS {
     ): Promise<{ userId: string; scheduleIdMap: Record<string, string> }> {
         return db.transaction(async (tx) => {
             const account = await this.registerUserAccount(
-                db,
+                tx,
                 'OIDC',
                 userData.id,
                 userData.name,
