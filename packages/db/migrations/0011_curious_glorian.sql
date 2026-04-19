5b0bc6fae8d9dc6bf1311d73e526385f9d61968d
Choollol <67297627+Choollol@users.noreply.github.com>
2026-04-18 22:08:57 -0700

    Add temp if not exists

--- packages/db/migrations/0011_curious_glorian.sql
+++ packages/db/migrations/0011_curious_glorian.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "verifications" (
+CREATE TABLE IF NOT EXISTS "verifications" (
 	"id" text PRIMARY KEY NOT NULL,
 	"identifier" text NOT NULL,
 	"value" text NOT NULL,
@@ -7,20 +7,20 @@ CREATE TABLE "verifications" (
 	"updated_at" timestamp (6) with time zone NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "id" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "provider_id" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "access_token" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "refresh_token" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "access_token_expires_at" timestamp;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "refresh_token_expires_at" timestamp;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "id_token" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "scope" text;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "accounts" ADD COLUMN "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP);--> statement-breakpoint
-ALTER TABLE "sessions" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "sessions" ADD COLUMN "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP);--> statement-breakpoint
-ALTER TABLE "sessions" ADD COLUMN "ip_address" text;--> statement-breakpoint
-ALTER TABLE "sessions" ADD COLUMN "user_agent" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email_verified" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-CREATE INDEX "session_userId_idx" ON "sessions" USING btree ("user_id");
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "id" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "provider_id" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "access_token" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "refresh_token" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "access_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "refresh_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "id_token" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "scope" text;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
+ALTER TABLE "accounts" ADD COLUMN IF NOT EXISTS "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP);--> statement-breakpoint
+ALTER TABLE "sessions" ADD COLUMN IF NOT EXISTS "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
+ALTER TABLE "sessions" ADD COLUMN IF NOT EXISTS "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP);--> statement-breakpoint
+ALTER TABLE "sessions" ADD COLUMN IF NOT EXISTS "ip_address" text;--> statement-breakpoint
+ALTER TABLE "sessions" ADD COLUMN IF NOT EXISTS "user_agent" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "email_verified" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "session_userId_idx" ON "sessions" USING btree ("user_id");
