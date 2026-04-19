f38cf9c4098d167fa8b8d8fea86bcb4f9a57eff9
Grace Yan <xgraceyan@gmail.com>
2026-04-18 23:27:06 -0700

    fix: posthog init fix???

--- apps/antalmanac/src/providers/PostHog.tsx
+++ apps/antalmanac/src/providers/PostHog.tsx
@@ -1,22 +1,28 @@
 import { PostHog } from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
+import { useEffect } from 'react';
 
 export const postHog = new PostHog();
+
 interface Props {
     children?: React.ReactNode;
 }
 
 export default function AppPostHogProvider(props: Props) {
-    if (process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_KEY) {
-        postHog.init(process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_KEY, {
-            api_host: process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_HOST,
-            capture_pageview: false,
-            autocapture: false,
-        });
+    useEffect(() => {
+        if (process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_KEY) {
+            postHog.init(process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_KEY, {
+                api_host: process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_HOST,
+                capture_pageview: false,
+                autocapture: false,
+            });
+        } else {
+            console.warn('PostHog not initialized: Missing API key');
+        }
+    }, []);
 
+    if (process.env.NEXT_PUBLIC_PUBLIC_POSTHOG_KEY) {
         return <PostHogProvider client={postHog}>{props.children}</PostHogProvider>;
-    } else {
-        console.warn('PostHog not initialized: Missing API key');
-        return <>{props.children}</>;
     }
+    return <>{props.children}</>;
 }
