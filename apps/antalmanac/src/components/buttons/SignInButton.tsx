fe5b3d538a193b7c28e9f351a57b7c60c20227a1
Choollol <67297627+Choollol@users.noreply.github.com>
2026-04-19 13:43:56 -0700

    Add loading state to SignInButton

--- apps/antalmanac/src/components/buttons/SignInButton.tsx
+++ apps/antalmanac/src/components/buttons/SignInButton.tsx
@@ -1,21 +1,30 @@
 import { loginUser } from '$actions/AppStoreActions';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Button } from '@mui/material';
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 
 interface Props {
     fullWidth?: ComponentProps<typeof Button>['fullWidth'];
 }
 
 const SignInButton = ({ fullWidth }: Props) => {
+    const [isSigningIn, setIsSigningIn] = useState(false);
+
+    const handleClick = async () => {
+        setIsSigningIn(true);
+        await loginUser();
+        setIsSigningIn(false);
+    };
+
     return (
         <Button
-            onClick={() => loginUser()}
+            onClick={handleClick}
             startIcon={<GoogleIcon />}
             color="primary"
             variant="contained"
             size="large"
             fullWidth={fullWidth}
+            loading={isSigningIn}
         >
             Sign in with Google
         </Button>
