98d0cbbf2b4d5232b5d959433149af41aebb0f43
Kevin Wu <kevinwu098@gmail.com>
2026-04-27 20:38:35 -0700

    refactor: remove lazy wrapper

--- apps/antalmanac/src/components/RightPane/SectionTable/SectionTable.tsx
+++ apps/antalmanac/src/components/RightPane/SectionTable/SectionTable.tsx
@@ -3,15 +3,15 @@ import { CourseInfoButton } from '$components/RightPane/SectionTable/CourseInfo/
 import { CourseInfoSearchButton } from '$components/RightPane/SectionTable/CourseInfo/CourseInfoSearchButton';
 import { EnrollmentColumnHeader } from '$components/RightPane/SectionTable/EnrollmentColumnHeader';
 import { EnrollmentHistoryPopup } from '$components/RightPane/SectionTable/EnrollmentHistoryPopup';
-import type { SectionTableProps } from '$components/RightPane/SectionTable/SectionTable.types';
 import { SectionTableBody } from '$components/RightPane/SectionTable/SectionTableBody/SectionTableBody';
 import { useIsMobile } from '$hooks/useIsMobile';
-import analyticsEnum from '$lib/analytics/analytics';
+import analyticsEnum, { AnalyticsCategory } from '$lib/analytics/analytics';
 import { SECTION_TABLE_COLUMNS, type SectionTableColumn, useColumnStore } from '$stores/ColumnStore';
 import { useTimeFormatStore } from '$stores/SettingsStore';
 import { useTabStore } from '$stores/TabStore';
 import { Route, ShowChart as ShowChartIcon } from '@mui/icons-material';
 import { Alert, Box, Paper, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { AACourse } from '@packages/antalmanac-types';
 import { useMemo } from 'react';
 
 const TOTAL_NUM_COLUMNS = SECTION_TABLE_COLUMNS.length;
@@ -34,6 +34,16 @@ const tableHeaderColumns: Record<Exclude<SectionTableColumn, 'action'>, TableHea
     syllabus: { label: 'Syllabus', weight: 5 },
 };
 const tableHeaderColumnEntries = Object.entries(tableHeaderColumns);
+
+interface SectionTableProps {
+    courseDetails: AACourse;
+    term: string;
+    allowHighlight: boolean;
+    scheduleNames: string[];
+    analyticsCategory: AnalyticsCategory;
+    updatedAt?: string;
+    missingSections?: string[];
+}
 
 function SectionTable(props: SectionTableProps) {
     const { courseDetails, term, allowHighlight, scheduleNames, analyticsCategory, missingSections = [] } = props;
