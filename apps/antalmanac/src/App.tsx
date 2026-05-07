0c9476952003eb3b03bb973070dbd8e8884e2614
Dominic Seung <dominic@seung.dev>
2026-05-06 21:28:51 -0700

    fix: open tutorial on page load, not menu open

--- apps/antalmanac/src/App.tsx
+++ apps/antalmanac/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { undoDelete, redoDelete } from '$actions/AppStoreActions';
 import { AutoSignIn } from '$components/AutoSignIn';
+import { TutorialInitializer } from '$components/TutorialInitializer';
 import PosthogPageviewTracker from '$lib/analytics/PostHogPageviewTracker';
 import AppPostHogProvider from '$providers/PostHog';
 import AppQueryProvider from '$providers/Query';
@@ -144,6 +145,7 @@ export default function App() {
                                 }),
                             }}
                         >
+                            <TutorialInitializer />
                             <RouterProvider router={ROUTER} />
                         </TourProvider>
                     </AppQueryProvider>
