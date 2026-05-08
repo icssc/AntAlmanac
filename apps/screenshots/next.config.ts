505efa8212d0404314f366b585aaa9dea3b05540
Kevin Wu <kevinwu098@gmail.com>
2026-05-07 18:06:56 -0700

    feat: integration

--- apps/screenshots/next.config.ts
+++ apps/screenshots/next.config.ts
@@ -1,8 +1,5 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
