cd6d51ee1de0f0683a5953d499866e3d4e4a5938
Kevin Wu <kevinwu098@gmail.com>
2026-05-09 21:23:56 -0700

    feat: migration

---
+++ packages/db/migrations/0015_daily_starfox.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "reviewDismissals" DROP CONSTRAINT "unique_dismissal";--> statement-breakpoint
+ALTER TABLE "instructorReviews" DROP CONSTRAINT "unique_review";--> statement-breakpoint
+ALTER TABLE "reviewDismissals" ADD COLUMN "term" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "reviewDismissals" ADD CONSTRAINT "unique_dismissal" UNIQUE("user_id","professor_id","course_id","term");--> statement-breakpoint
+ALTER TABLE "instructorReviews" ADD CONSTRAINT "unique_review" UNIQUE("user_id","professor_id","course_id","quarter");
