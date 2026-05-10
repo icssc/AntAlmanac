d1c4091f82839826ff5a8963ae916a7a4245b07d
Kevin Wu <kevinwu098@gmail.com>
2026-05-09 21:37:46 -0700

    fix: migration

--- packages/db/migrations/0015_daily_starfox.sql
+++ packages/db/migrations/0015_daily_starfox.sql
@@ -1,5 +1,6 @@
 ALTER TABLE "reviewDismissals" DROP CONSTRAINT "unique_dismissal";--> statement-breakpoint
 ALTER TABLE "instructorReviews" DROP CONSTRAINT "unique_review";--> statement-breakpoint
-ALTER TABLE "reviewDismissals" ADD COLUMN "term" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "reviewDismissals" ADD COLUMN "term" text NOT NULL DEFAULT '__legacy__';--> statement-breakpoint
+ALTER TABLE "reviewDismissals" ALTER COLUMN "term" DROP DEFAULT;--> statement-breakpoint
 ALTER TABLE "reviewDismissals" ADD CONSTRAINT "unique_dismissal" UNIQUE("user_id","professor_id","course_id","term");--> statement-breakpoint
 ALTER TABLE "instructorReviews" ADD CONSTRAINT "unique_review" UNIQUE("user_id","professor_id","course_id","quarter");
