05b665c2c1ae9354ca3794b1dafea59f8ca44666
Choollol <67297627+Choollol@users.noreply.github.com>
2026-05-15 12:36:28 -0700

    Use local mergeSx utility (#1786)

--- apps/antalmanac/src/components/NotificationSnackbar.tsx
+++ apps/antalmanac/src/components/NotificationSnackbar.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useIsMobile } from '$hooks/useIsMobile';
+import { mergeSx } from '$lib/helpers';
 import { useSnackbarStore } from '$stores/SnackbarStore';
 import { Alert, Snackbar, SnackbarCloseReason } from '@mui/material';
-import { mergeSx } from '@mui/x-date-pickers/internals';
 
 export const NotificationSnackbar = () => {
     const { open, snackbarClosed, message, severity, durationSeconds, position, style } = useSnackbarStore();
