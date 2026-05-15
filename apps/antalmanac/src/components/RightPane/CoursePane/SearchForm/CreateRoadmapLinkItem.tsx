34c3251f3d61b3a7afeb700d0aea741099557888
Kevin Wu <kevinwu098@gmail.com>
2026-05-15 00:07:05 -0700

    Merge main into kwu/type-safety and resolve CourseRenderPane conflicts (#1787)

---
+++ apps/antalmanac/src/components/RightPane/CoursePane/SearchForm/CreateRoadmapLinkItem.tsx
@@ -0,0 +1,30 @@
+import { LIGHT_BLUE, PLANNER_LINK } from '$src/globals';
+import { useThemeStore } from '$stores/SettingsStore';
+import { Link, MenuItem } from '@mui/material';
+
+interface CreateRoadmapLinkItemProps {
+    verticalPadding?: number | string;
+}
+
+export const CreateRoadmapLinkItem = ({ verticalPadding }: CreateRoadmapLinkItemProps) => {
+    const isDark = useThemeStore((state) => state.isDark);
+
+    return (
+        <MenuItem sx={{ padding: 0 }}>
+            <Link
+                href={PLANNER_LINK}
+                target="_blank"
+                sx={{
+                    padding: 2,
+                    paddingTop: verticalPadding,
+                    paddingBottom: verticalPadding,
+                    width: '100%',
+                    height: '100%',
+                    ...(isDark && { color: LIGHT_BLUE }),
+                }}
+            >
+                Create a roadmap!
+            </Link>
+        </MenuItem>
+    );
+};
