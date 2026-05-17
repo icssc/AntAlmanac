768e4c8329a686a66d062ad448861c7b83df76a1
Choollol <67297627+Choollol@users.noreply.github.com>
2026-05-17 14:26:00 -0700

    Move aggregateUserData definition closer to usage

--- apps/antalmanac/src/backend/lib/rds.ts
+++ apps/antalmanac/src/backend/lib/rds.ts
@@ -374,6 +374,90 @@ export class RDS {
     }
 
     /**
+     * Aggregates the user's schedule data from the results of two queries.
+     */
+    private static aggregateUserData(
+        sectionResults: { schedules: Schedule; coursesInSchedule: CourseInSchedule | null }[],
+        customEventResults: { schedules: Schedule; customEvents: CustomEvent | null }[]
+    ): (ShortCourseSchedule & { id: string; index: number })[] {
+        // Map from schedule ID to schedule data
+        const schedulesMapping: Record<string, ShortCourseSchedule & { id: string; index: number }> = {};
+
+        const courseIndexes: Record<Schedule['id'], Record<ShortCourse['sectionCode'], CourseInSchedule['index']>> = {};
+
+        // Add courses to schedules
+        sectionResults.forEach(({ schedules: schedule, coursesInSchedule: course }) => {
+            const scheduleId = schedule.id;
+
+            const scheduleAggregate = schedulesMapping[scheduleId] || {
+                id: scheduleId,
+                scheduleName: schedule.name,
+                scheduleNote: schedule.notes,
+                courses: [],
+                customEvents: [],
+                index: schedule.index,
+            };
+
+            if (course) {
+                const sectionCode = course.sectionCode.toString();
+                scheduleAggregate.courses.push({
+                    sectionCode: sectionCode,
+                    term: course.term,
+                    color: course.color,
+                });
+
+                if (course.index !== null) {
+                    if (!courseIndexes[scheduleId]) {
+                        courseIndexes[scheduleId] = {};
+                    }
+                    courseIndexes[scheduleId][sectionCode] = course.index;
+                }
+            }
+
+            schedulesMapping[scheduleId] = scheduleAggregate;
+        });
+
+        for (const [scheduleId, indexes] of Object.entries(courseIndexes)) {
+            schedulesMapping[scheduleId].courses.sort((a, b) => {
+                const aIndex = indexes[a.sectionCode];
+                const bIndex = indexes[b.sectionCode];
+                if (typeof aIndex !== 'number' || typeof bIndex !== 'number') {
+                    return 0;
+                }
+                return aIndex - bIndex;
+            });
+        }
+
+        // Add custom events to schedules
+        customEventResults.forEach(({ schedules: schedule, customEvents: customEvent }) => {
+            const scheduleId = schedule.id;
+            const scheduleAggregate = schedulesMapping[scheduleId] || {
+                scheduleName: schedule.name,
+                scheduleNote: schedule.notes,
+                courses: [],
+                customEvents: [],
+            };
+
+            if (customEvent) {
+                scheduleAggregate.customEvents.push({
+                    customEventID: customEvent.id,
+                    title: customEvent.title,
+                    start: customEvent.start,
+                    end: customEvent.end,
+                    days: customEvent.days.split('').map((day) => day === '1'),
+                    color: customEvent.color ?? '#551a8b',
+                    building: customEvent.building ?? undefined,
+                });
+            }
+
+            schedulesMapping[scheduleId] = scheduleAggregate;
+        });
+
+        // Sort schedules by index
+        return Object.values(schedulesMapping).sort((a, b) => a.index - b.index);
+    }
+
+    /**
      * Retrieves a schedule by its ID. All schedules are publicly accessible via their ID.
      *
      * @param db - The database or transaction object to use for the query.
@@ -474,90 +558,6 @@ export class RDS {
                 },
             };
         });
-    }
-
-    /**
-     * Aggregates the user's schedule data from the results of two queries.
-     */
-    private static aggregateUserData(
-        sectionResults: { schedules: Schedule; coursesInSchedule: CourseInSchedule | null }[],
-        customEventResults: { schedules: Schedule; customEvents: CustomEvent | null }[]
-    ): (ShortCourseSchedule & { id: string; index: number })[] {
-        // Map from schedule ID to schedule data
-        const schedulesMapping: Record<string, ShortCourseSchedule & { id: string; index: number }> = {};
-
-        const courseIndexes: Record<Schedule['id'], Record<ShortCourse['sectionCode'], CourseInSchedule['index']>> = {};
-
-        // Add courses to schedules
-        sectionResults.forEach(({ schedules: schedule, coursesInSchedule: course }) => {
-            const scheduleId = schedule.id;
-
-            const scheduleAggregate = schedulesMapping[scheduleId] || {
-                id: scheduleId,
-                scheduleName: schedule.name,
-                scheduleNote: schedule.notes,
-                courses: [],
-                customEvents: [],
-                index: schedule.index,
-            };
-
-            if (course) {
-                const sectionCode = course.sectionCode.toString();
-                scheduleAggregate.courses.push({
-                    sectionCode: sectionCode,
-                    term: course.term,
-                    color: course.color,
-                });
-
-                if (course.index !== null) {
-                    if (!courseIndexes[scheduleId]) {
-                        courseIndexes[scheduleId] = {};
-                    }
-                    courseIndexes[scheduleId][sectionCode] = course.index;
-                }
-            }
-
-            schedulesMapping[scheduleId] = scheduleAggregate;
-        });
-
-        for (const [scheduleId, indexes] of Object.entries(courseIndexes)) {
-            schedulesMapping[scheduleId].courses.sort((a, b) => {
-                const aIndex = indexes[a.sectionCode];
-                const bIndex = indexes[b.sectionCode];
-                if (typeof aIndex !== 'number' || typeof bIndex !== 'number') {
-                    return 0;
-                }
-                return aIndex - bIndex;
-            });
-        }
-
-        // Add custom events to schedules
-        customEventResults.forEach(({ schedules: schedule, customEvents: customEvent }) => {
-            const scheduleId = schedule.id;
-            const scheduleAggregate = schedulesMapping[scheduleId] || {
-                scheduleName: schedule.name,
-                scheduleNote: schedule.notes,
-                courses: [],
-                customEvents: [],
-            };
-
-            if (customEvent) {
-                scheduleAggregate.customEvents.push({
-                    customEventID: customEvent.id,
-                    title: customEvent.title,
-                    start: customEvent.start,
-                    end: customEvent.end,
-                    days: customEvent.days.split('').map((day) => day === '1'),
-                    color: customEvent.color ?? '#551a8b',
-                    building: customEvent.building ?? undefined,
-                });
-            }
-
-            schedulesMapping[scheduleId] = scheduleAggregate;
-        });
-
-        // Sort schedules by index
-        return Object.values(schedulesMapping).sort((a, b) => a.index - b.index);
     }
 
     /**
