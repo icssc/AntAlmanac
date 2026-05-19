1d18fac44c51dd300d03587d15174729ab30cb1f
Kevin Wu <kevinwu098@gmail.com>
2026-05-18 23:00:56 -0700

    fix: misc

--- apps/antalmanac/src/hooks/useQuickSearch.tsx
+++ apps/antalmanac/src/hooks/useQuickSearch.tsx
@@ -1,33 +1,30 @@
 import {
     defaultCourseSearchFormValues,
-    serializeCourseSearchParams,
+    useCourseSearchUrlState,
 } from '$components/RightPane/CoursePane/SearchForm/searchParams';
 import { AATerm } from '$lib/term';
 import { useCoursePaneStore } from '$stores/CoursePaneStore';
 import { useTabStore } from '$stores/TabStore';
 import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export function useQuickSearch() {
     const displaySections = useCoursePaneStore((s) => s.displaySections);
     const forceUpdate = useCoursePaneStore((s) => s.forceUpdate);
     const setActiveTab = useTabStore((s) => s.setActiveTab);
-    const navigate = useNavigate();
+    const { setFields } = useCourseSearchUrlState();
 
     return useCallback(
         (deptValue: string, courseNumber: string, term: AATerm) => {
-            const href = serializeCourseSearchParams('/', {
+            void setFields({
                 ...defaultCourseSearchFormValues,
                 term,
-                deptValue: deptValue,
-                courseNumber: courseNumber,
+                deptValue,
+                courseNumber,
             });
-
-            navigate(href, { replace: false });
             setActiveTab('search');
             displaySections();
             forceUpdate();
         },
-        [displaySections, forceUpdate, navigate, setActiveTab]
+        [displaySections, forceUpdate, setActiveTab, setFields]
     );
 }
