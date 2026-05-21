b3f013eaf33415c34bc9878120124fc8ba67f2de
Kevin Wu <kevinwu098@gmail.com>
2026-05-21 05:44:14 -0700

    fix: quick search

--- apps/antalmanac/src/hooks/useQuickSearch.tsx
+++ apps/antalmanac/src/hooks/useQuickSearch.tsx
@@ -1,24 +1,28 @@
-import { defaultFormData, useCourseSearchUrlState } from '$components/RightPane/CoursePane/SearchForm/searchParams';
+import { defaultFormData, serializeCourseSearchParams } from '$components/RightPane/CoursePane/SearchForm/searchParams';
 import { AATerm } from '$lib/term';
 import { useTabStore } from '$stores/TabStore';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function useQuickSearch() {
+    const navigate = useNavigate();
     const setActiveTab = useTabStore((s) => s.setActiveTab);
-    const { setFields, setSearchMode, showResults } = useCourseSearchUrlState();
 
     return useCallback(
         (deptValue: string, courseNumber: string, term: AATerm) => {
-            void setSearchMode('quick');
-            void setFields({
+            const courseSearch = serializeCourseSearchParams({
                 ...defaultFormData,
                 term,
                 deptValue,
                 courseNumber,
             });
+            const searchParams = new URLSearchParams(courseSearch);
+            searchParams.set('search', 'quick');
+            searchParams.set('view', 'results');
+
+            navigate({ pathname: '/', search: searchParams.toString() });
             setActiveTab('search');
-            void showResults();
         },
-        [setActiveTab, setFields, setSearchMode, showResults]
+        [navigate, setActiveTab]
     );
 }
