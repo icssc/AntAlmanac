43256f3f91b5ea1c94adc5b44b76b501e240e58a
Choollol <67297627+Choollol@users.noreply.github.com>
2026-05-23 21:55:59 -0700

    fix: return URL validation to fix encoding bug (#1854)

---
+++ apps/antalmanac/tests/authUtils.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'vitest';
+
+import { getSafeAuthRedirectPath } from '../src/lib/auth/authUtils';
+
+const VALID_URL = '/importRoadmap=1234&term=2026+Spring';
+const ALLOWED_ORIGIN = 'https://www.antalmanac.com';
+
+describe('getSafeAuthRedirectPath', () => {
+    test('Returns valid URL', () => {
+        expect(getSafeAuthRedirectPath(VALID_URL, ALLOWED_ORIGIN, ALLOWED_ORIGIN)).toStrictEqual(
+            ALLOWED_ORIGIN + VALID_URL
+        );
+    });
+
+    test('Returns root if URL origin is not allowed', () => {
+        expect(getSafeAuthRedirectPath('https://www.google.com', ALLOWED_ORIGIN, ALLOWED_ORIGIN)).toStrictEqual('/');
+    });
+});
