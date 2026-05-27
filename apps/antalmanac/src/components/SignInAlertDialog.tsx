8a355ee5e2a55b296dcd922b0d07d161f998eb5b
caleb wong <calebwongtech@gmail.com>
2026-05-27 14:47:36 -0700

    feat: adjust sign in alert dialogue (#1869)

--- apps/antalmanac/src/components/SignInAlertDialog.tsx
+++ apps/antalmanac/src/components/SignInAlertDialog.tsx
@@ -13,7 +13,7 @@ interface Props {
 export const SignInAlertDialog = ({ open, title, severity = 'info', onClose }: Props) => {
     return (
         <AlertDialog open={open} onClose={onClose} title={title} severity={severity}>
-            <DialogContentText>Sign in to load your schedule</DialogContentText>
+            <DialogContentText sx={{ mb: 1 }}>Sign in to load your schedule</DialogContentText>
             <Stack spacing={1}>
                 <SignInButtons />
             </Stack>
