2fe1044193c85485ef7c78c39d3371aa79317b65
Kevin Wu <kevinwu098@gmail.com>
2026-06-05 16:23:52 -0700

    fix: env

--- apps/antalmanac/src/env.ts
+++ apps/antalmanac/src/env.ts
@@ -1,12 +1,5 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import { createEnv } from '@t3-oss/env-nextjs';
-import { config } from 'dotenv';
 import { z } from 'zod';
-
-const appRoot = resolve(dirname(fileURLToPath(import.meta.url)), '..');
-config({ path: resolve(appRoot, '.env') });
 
 export const env = createEnv({
     server: {
