1152f316a3a24549229a874e245750635a874e0f
Kevin Wu <kevinwu098@gmail.com>
2026-06-28 04:15:12 -0700

    fix: minWidth

--- apps/antalmanac/src/app/(main)/client.tsx
+++ apps/antalmanac/src/app/(main)/client.tsx
@@ -63,7 +63,7 @@ function Home() {
             defaultLayout={DEFAULT_LAYOUT}
             style={{ flexGrow: 1, marginTop: 4 }}
         >
-            <Panel id={CALENDAR_PANEL_ID} minSize="400px" style={{ overflow: 'hidden' }}>
+            <Panel id={CALENDAR_PANEL_ID} minSize="40%" style={{ overflow: 'hidden' }}>
                 <Stack direction="column" height="100%">
                     {showCalendarPane && <ScheduleCalendar />}
                 </Stack>
@@ -92,7 +92,7 @@ function Home() {
                 ⋮
             </Separator>
 
-            <Panel id={SCHEDULE_PANEL_ID} minSize="400px">
+            <Panel id={SCHEDULE_PANEL_ID} minSize="40%">
                 <Stack direction="column" height="100%">
                     <ScheduleManagement />
                 </Stack>
